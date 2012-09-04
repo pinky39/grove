f361869247a615d9daaecdef6d419039c5a9b5c1
pinky <pinky@lab.org>
2012-09-04 19:56:49 +0200

    UI Fix: mana pool colors count was not correct; mana from Thran Turbine was not counted.

--- source/Grove/Core/Details/Mana/ManaPool.cs
+++ source/Grove/Core/Details/Mana/ManaPool.cs
@@ -1,13 +1,14 @@
 ﻿namespace Grove.Core.Details.Mana
 {
+  using System;
   using System.Linq;
   using Infrastructure;
 
   [Copyable]
   public class ManaPool : IManaSource
   {
-    private readonly ManaBag _unrestricted;    
     private readonly ManaBag _restrictedAbilities;
+    private readonly ManaBag _unrestricted;
 
     private ManaPool() {}
 
@@ -17,21 +18,21 @@
       _restrictedAbilities = new ManaBag(changeTracker);
     }
 
-    public int WhiteCount { get { return _unrestricted.Count(x => x.IsSingleColor(ManaColors.White)); } }
-    public int BlueCount { get { return _unrestricted.Count(x => x.IsSingleColor(ManaColors.Blue)); } }
-    public int BlackCount { get { return _unrestricted.Count(x => x.IsSingleColor(ManaColors.Black)); } }
-    public int RedCount { get { return _unrestricted.Count(x => x.IsSingleColor(ManaColors.Red)); } }
-    public int GreenCount { get { return _unrestricted.Count(x => x.IsSingleColor(ManaColors.Green)); } }
-    public int MultiCount { get { return _unrestricted.Count(x => x.IsMultiColor); } }
-    public int ColorlessCount { get { return _unrestricted.Count(x => x.IsColorless); } }
-    public bool IsEmpty { get { return _unrestricted.IsEmpty; } }
+    public int WhiteCount { get { return CountTotal(ManaColors.White); } }
+
+    public int BlueCount { get { return CountTotal(ManaColors.Blue); } }
+    public int BlackCount { get { return CountTotal(ManaColors.Black); } }
+    public int RedCount { get { return CountTotal(ManaColors.Red); } }
+    public int GreenCount { get { return CountTotal(ManaColors.Green); } }
+    public int MultiCount { get { return CountTotal(x => x.IsMultiColor); } }
+    public int ColorlessCount { get { return CountTotal(x => x.IsColorless); } }    
 
     public int Priority { get { return 0; } }
     object IManaSource.Resource { get { return this; } }
 
     [Updates("WhiteCount", "BlueCount", "BlackCount", "RedCount", "GreenCount", "MultiCount")]
     public virtual void Consume(IManaAmount amount, ManaUsage usage)
-    {                  
+    {
       // since the check will be made if pool contains
       // appropriate mana, we can speed things up a
       // bit if the whole pool should be consumed
@@ -60,12 +61,23 @@
       return new AggregateManaAmount(_unrestricted.GetAmount(), _restrictedAbilities.GetAmount());
     }
 
+    private int CountTotal(ManaColors color)
+    {
+      return _unrestricted.Count(x => x.IsSingleColor(color)) +
+        _restrictedAbilities.Count(x => x.IsSingleColor(color));
+    }
+
+    private int CountTotal(Func<ManaUnit, bool> selector)
+    {
+      return _unrestricted.Count(selector) + _restrictedAbilities.Count(selector);
+    }
+
     [Updates("WhiteCount", "BlueCount", "BlackCount", "RedCount", "GreenCount", "MultiCount")]
     public virtual void Add(IManaAmount amount)
     {
       _unrestricted.Add(amount);
     }
-    
+
     [Updates("WhiteCount", "BlueCount", "BlackCount", "RedCount", "GreenCount", "MultiCount")]
     public virtual void AddAbilities(IManaAmount amount)
     {
