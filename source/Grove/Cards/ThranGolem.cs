63801105dc79233f0c192f8a58c6473244beeb6c
pinky <pinky@lab.org>
2013-12-05 21:02:45 +0100

    New card: Thran Golem.

---
+++ source/Grove/Cards/ThranGolem.cs
@@ -0,0 +1,65 @@
+﻿namespace Grove.Cards
+{
+  using System.Collections.Generic;
+  using Gameplay.Abilities;
+  using Gameplay.Effects;
+  using Gameplay.Messages;
+  using Gameplay.Misc;
+  using Gameplay.Modifiers;
+  using Gameplay.Triggers;
+
+  public class ThranGolem : CardTemplateSource
+  {
+    private static Lifetime UntilAttached()
+    {
+      return new AttachmentLifetime(self =>
+        self.Modifier.SourceEffect.TriggerMessage<AttachmentAttached>().Attachment);
+    }
+
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Thran Golem")
+        .ManaCost("{5}")
+        .Type("Artifact Creature Golem")
+        .Text("As long as Thran Golem is enchanted, it gets +2/+2 and has flying, first strike, and trample.")
+        .FlavorText("Karn felt more secure about his value to Urza when he realized he didn't need regular trimming.")
+        .Power(3)
+        .Toughness(3)
+        .TriggeredAbility(p =>
+          {
+            p.Trigger(new OnAttachmentAttached(c => c.Is().Aura));
+            p.Effect = () =>
+              {
+                return new ApplyModifiersToSelf(
+                  () =>
+                    {
+                      var modifier = new AddPowerAndToughness(2, 2);
+                      modifier.AddLifetime(UntilAttached());
+                      return modifier;
+                    },
+                  () =>
+                    {
+                      var modifier = new AddStaticAbility(Static.Flying);
+                      modifier.AddLifetime(UntilAttached());
+                      return modifier;
+                    },
+                  () =>
+                    {
+                      var modifier = new AddStaticAbility(Static.FirstStrike);
+                      modifier.AddLifetime(UntilAttached());
+                      return modifier;
+                    },
+                  () =>
+                    {
+                      var modifier = new AddStaticAbility(Static.Trample);
+                      modifier.AddLifetime(UntilAttached());
+                      return modifier;
+                    });
+              };
+
+            p.UsesStack = false;
+          });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/ThranGolem.cs
@@ -0,0 +1,22 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class ThranGolem
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void EnchantGolem()
+      {
+        Hand(P1, "Rancor");
+        Battlefield(P1, "Thran Golem", "Forest");
+        Battlefield(P2, "Wall of Blossoms");
+        RunGame(1);
+
+        Equal(13, P2.Life);
+      }
+    }
+  }
+}
