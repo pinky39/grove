c975181c78d77fe1d5a5ce7d275bedd597348abc
Sandmann123 <norfirill@gmail.com>
2014-09-21 13:53:08 +0800

    Added a card: Resolute Archangel

---
+++ source/Grove/CardsLibrary/ResoluteArchangel.cs
@@ -0,0 +1,36 @@
+﻿namespace Grove.CardsLibrary
+{
+    using System.Collections.Generic;
+    using AI.TargetingRules;
+    using Effects;
+    using Triggers;
+
+    public class ResoluteArchangel : CardTemplateSource
+    {
+        public override IEnumerable<CardTemplate> GetCards()
+        {
+            yield return Card
+              .Named("Resolute Archangel")
+              .ManaCost("{5}{W}{W}")
+              .Type("Creature - Angel")
+              .Text("{Flying}{EOL}When Resolute Archangel enters the battlefield, if your life total is less than your starting life total, it becomes equal to your starting life total.")
+              .FlavorText("Cut it down, bury it in snow, put it to the torch. The rose will still bloom again.")
+              .Power(4)
+              .Toughness(4)
+              .SimpleAbilities(Static.Flying)
+              .TriggeredAbility(p =>
+              {
+                  p.Text = "When Resolute Archangel enters the battlefield, if your life total is less than your starting life total, it becomes equal to your starting life total.";
+
+                  p.Trigger(new OnZoneChanged(
+                    to: Zone.Battlefield));
+
+                  p.Trigger(new OnLifepointsLeft(ability => ability.OwningCard.Controller.Life < 20));
+
+                  p.Effect = () => new YourLifeBecomesEqual(20);
+                  
+                  p.TargetingRule(new EffectYou());
+              });
+        }
+    }
+}
---
+++ tests/Grove.Tests/Cards/ResoluteArchangel.cs
@@ -0,0 +1,30 @@
+﻿namespace Grove.Tests.Cards
+{
+    using System.Linq;
+    using Infrastructure;
+    using Xunit;
+
+    public class ResoluteArchangel
+    {
+        public class Predefined : PredefinedAiScenario
+        {
+            [Fact]
+            public void SetLife()
+            {
+                var angel = C("Resolute Archangel");
+
+                P1.Life = 10;
+                Hand(P1, angel);
+
+                Exec(
+                  At(Step.FirstMain)
+                    .Cast(angel)
+                    .Verify(() =>
+                    {
+                        Equal(1, P1.Battlefield.Creatures.Count());
+                        Equal(20, P1.Life);
+                    }));              
+            }
+        }
+    }
+}
