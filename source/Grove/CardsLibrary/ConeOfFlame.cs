7c78121153281d9905defdbd17e85f4473e768e1
Sandmann123 <norfirill@gmail.com>
2014-09-25 10:08:36 +0800

    Added new card: Cone of Flame

---
+++ source/Grove/CardsLibrary/ConeOfFlame.cs
@@ -0,0 +1,42 @@
+﻿namespace Grove.CardsLibrary
+{
+    using System.Collections.Generic;
+    using AI.TargetingRules;
+    using AI.TimingRules;
+    using Effects;
+
+    public class ConeOfFlame : CardTemplateSource
+    {
+        public override IEnumerable<CardTemplate> GetCards()
+        {
+            yield return Card
+                .Named("Cone of Flame")
+                .ManaCost("{3}{R}{R}")
+                .Type("Sorcery")
+                .Text("Cone of Flame deals 1 damage to target creature or player, 2 damage to another target creature or player, and 3 damage to a third target creature or player.")
+                .Cast(p =>
+                {
+                    p.Effect = () => new DealIncrementalDamagesToTargets(1, damageIncrement: 1); 
+
+                    p.TargetSelector.AddEffect(trg =>
+                    {
+                        trg.Is.CreatureOrPlayer().On.Battlefield();
+                        trg.Message = "Select a target creature or player that is dealt 1 damage.";
+                    });
+                    p.TargetSelector.AddEffect(trg =>
+                    {
+                        trg.Is.CreatureOrPlayer().On.Battlefield();
+                        trg.Message = "Select a second target creature or player that is dealt 2 damage.";
+                    });
+                    p.TargetSelector.AddEffect(trg =>
+                    {
+                        trg.Is.CreatureOrPlayer().On.Battlefield();
+                        trg.Message = "Select a third target creature or player that is dealt 3 damage.";
+                    });
+
+                    p.TargetingRule(new EffectDealDamage());
+                    p.TimingRule(new WhenOpponentControllsPermanents(c => c.Is().Creature && c.Toughness < 4, minCount: 2));
+                });
+        }
+    }
+}
---
+++ source/Grove/Core/Effects/DealIncrementalDamagesToTargets.cs
@@ -0,0 +1,58 @@
+﻿namespace Grove.Effects
+{
+    using System.Linq;
+    using AI;
+
+    public class DealIncrementalDamagesToTargets : Effect
+    {
+        private readonly DynParam<int> _amount;
+
+        private readonly int _damageIncrement;
+
+        private DealIncrementalDamagesToTargets()
+        {
+        }
+
+        public DealIncrementalDamagesToTargets(DynParam<int> startAmount, int damageIncrement = 0)
+        {
+            _amount = startAmount;
+            _damageIncrement = damageIncrement;
+
+            RegisterDynamicParameters(startAmount);
+            SetTags(EffectTag.DealDamage);
+        }
+
+        public override int CalculatePlayerDamage(Player player)
+        {
+            return Targets.Effect.Any(x => x == player) ? _amount.Value + _damageIncrement * Targets.Effect.IndexOf(player) : 0;
+        }
+
+        public override int CalculateCreatureDamage(Card creature)
+        {
+            return Targets.Effect.Any(x => x == creature) ? _amount.Value + _damageIncrement * Targets.Effect.IndexOf(creature) : 0;
+        }
+
+        protected override void ResolveEffect()
+        {
+            var damage = _amount.Value;
+
+            foreach (var target in Targets)
+            {
+                if (ValidEffectTargets.Contains(target))
+                {
+                    Source.OwningCard.DealDamageTo(
+                        damage,
+                        (IDamageable)target,
+                        isCombat: false);
+                }
+
+                damage += _damageIncrement;
+            }
+        }
+
+        public override string ToString()
+        {
+            return GetType().Name;
+        }
+    }
+}
