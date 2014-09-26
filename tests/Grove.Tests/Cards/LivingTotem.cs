b60e166643f2be8cf142e0dff49b592716a861ba
Sandmann123 <norfirill@gmail.com>
2014-09-26 20:49:16 +0800

    Added new card: Living Totem

---
+++ tests/Grove.Tests/Cards/LivingTotem.cs
@@ -0,0 +1,25 @@
+﻿namespace Grove.Tests.Cards
+{
+    using Infrastructure;
+    using Xunit;
+
+    public class LivingTotem
+    {
+        public class Ai : AiScenario
+        {
+            [Fact]
+            public void CastLivingTotem()
+            {
+                Hand(P1, "Living Totem");
+                Battlefield(P1, "Forest", "Forest", "Forest", "Wall of Frost", "Grizzly Bears");
+
+                P2.Life = 4;
+
+                RunGame(1);
+
+                Equal(0, P1.Hand.Count);
+                Equal(1, P2.Life);
+            }
+        }
+    }
+}
