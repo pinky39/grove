a3a672c47943f497e34985fc6fa38f928039e18c
Sandmann123 <norfirill@gmail.com>
2014-09-26 20:52:18 +0800

    Added new card: Seraph of the Masses

---
+++ source/Grove/CardsLibrary/SeraphOfTheMasses.cs
@@ -0,0 +1,30 @@
+﻿namespace Grove.CardsLibrary
+{
+    using System.Collections.Generic;
+    using Modifiers;
+
+    public class SeraphOfTheMasses : CardTemplateSource
+    {
+        public override IEnumerable<CardTemplate> GetCards()
+        {
+            yield return Card
+                .Named("Seraph of the Masses")
+                .ManaCost("{5}{W}{W}")
+                .Type("Creature — Angel")
+                .Text("{Convoke}{I}(Your creatures can help cast this spell. Each creature you tap while casting this spell pays for {1} or one mana of that creature's color.){/I}{EOL}{Flying}{EOL}Seraph of the Masses's power and toughness are each equal to the number of creatures you control.")
+                .Power(0)
+                .Toughness(0)
+                .Convoke()
+                .SimpleAbilities(Static.Flying)
+                .StaticAbility(p =>
+                {
+                    p.Modifier(() => new ModifyPowerToughnessForEachPermanent(
+                      power: 1,
+                      toughness: 1,
+                      filter: (c, _) => c.Is().Creature,
+                      modifier: () => new IntegerSetter()));
+                    p.EnabledInAllZones = true;
+                });
+        }
+    }
+}
---
+++ tests/Grove.Tests/Cards/SeraphOfTheMasses.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Grove.Tests.Cards
+{
+    using Infrastructure;
+    using Xunit;
+
+    public class SeraphOfTheMasses
+    {
+        public class Ai : AiScenario
+        {
+            [Fact]
+            public void CastSeraphOfTheMasses()
+            {
+                Hand(P1, "Seraph of the Masses");
+                Battlefield(P1, "Plains", "Plains", "Mountain", "Mountain", "Mountain", "Mountain", "Grizzly Bears");
+                
+                Battlefield(P2, "Coral Barrier");
+
+                RunGame(1);
+
+                Equal(8, P1.Battlefield.Count);
+            }
+        }
+    }
+}
