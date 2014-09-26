60c592c6624be1af6a869680b236089c83aeff80
Sandmann123 <norfirill@gmail.com>
2014-09-26 21:07:46 +0800

    Added new card: Feral Incarnation

---
+++ source/Grove/CardsLibrary/FeralIncarnation.cs
@@ -0,0 +1,33 @@
+﻿namespace Grove.CardsLibrary
+{
+    using System.Collections.Generic;
+    using Effects;
+
+    public class FeralIncarnation : CardTemplateSource
+    {
+        public override IEnumerable<CardTemplate> GetCards()
+        {
+            yield return Card
+                .Named("Feral Incarnation")
+                .ManaCost("{8}{G}")
+                .Type("Sorcery")
+                .Text(
+                    "{Convoke}{I}(Your creatures can help cast this spell. Each creature you tap while casting this spell pays for {1} or one mana of that creature's color.){/I}{EOL}Put three 3/3 green Beast creature tokens onto the battlefield.")
+                .FlavorText("Nature is itself wild—in all its forms.")
+                .Convoke()
+                .Cast(p =>
+                {
+                    p.Text = "Put three 3/3 green Beast creature tokens onto the battlefield.";
+
+                    p.Effect = () => new CreateTokens(
+                        count: 3,
+                        token: Card
+                            .Named("Beast")
+                            .Power(3)
+                            .Toughness(3)
+                            .Type("Creature - Token Beast")
+                            .Colors(CardColor.Green));
+                });
+        }
+    }
+}
---
+++ tests/Grove.Tests/Cards/FeralIncarnation.cs
@@ -0,0 +1,23 @@
+﻿namespace Grove.Tests.Cards
+{
+    using System.Linq;
+    using Infrastructure;
+    using Xunit;
+
+    public class FeralIncarnation
+    {
+        public class Ai : AiScenario
+        {
+            [Fact]
+            public void CastFeralIncarnation()
+            {
+                Hand(P1, "Feral Incarnation");
+                Battlefield(P1, "Forest", "Mountain", "Mountain", "Mountain", "Mountain", "Forest", "Mountain", "Mountain", "Mountain", "Mountain");
+
+                RunGame(1);
+
+                Equal(3, P1.Battlefield.Creatures.Count());
+            }
+        }
+    }
+}
