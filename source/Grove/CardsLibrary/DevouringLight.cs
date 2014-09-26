0d61d52a1fc9133d913b0ac66c75966f21ca27cc
Sandmann123 <norfirill@gmail.com>
2014-09-26 21:04:01 +0800

    Added new card: Devouring Light

---
+++ source/Grove/CardsLibrary/DevouringLight.cs
@@ -0,0 +1,36 @@
+﻿namespace Grove.CardsLibrary
+{
+    using System.Collections.Generic;
+    using AI.TargetingRules;
+    using AI.TimingRules;
+    using Effects;
+
+    public class DevouringLight : CardTemplateSource
+    {
+        public override IEnumerable<CardTemplate> GetCards()
+        {
+            yield return Card
+                .Named("Devouring Light")
+                .ManaCost("{1}{W}{W}")
+                .Type("Instant")
+                .Text("{Convoke}{I}(Your creatures can help cast this spell. Each creature you tap while casting this spell pays for {1} or one mana of that creature's color.){/I}{EOL}Exile target attacking or blocking creature.")
+                .FlavorText("\"Even your shadow is too foul to tolerate.\"")
+                .Convoke()
+                .Cast(p =>
+                {
+                    p.Text = "Exile target attacking or blocking creature.";
+
+                    p.Effect = () => new ExileTargets();
+
+                    p.TargetSelector.AddEffect(trg =>
+                    {
+                        trg.Is.Card(c => c.Is().Creature && (c.IsAttacker || c.IsBlocker)).On.Battlefield();
+                        trg.Message = "Select target attacking or blocking creature.";
+                    });
+
+                    p.TimingRule(new Any(new AfterOpponentDeclaresAttackers(), new AfterOpponentDeclaresBlockers(), new AfterYouDeclareBlockers()));
+                    p.TargetingRule(new EffectExileBattlefield());
+                });
+        }
+    }
+}
---
+++ tests/Grove.Tests/Cards/DevouringLight.cs
@@ -0,0 +1,27 @@
+﻿namespace Grove.Tests.Cards
+{
+    using Infrastructure;
+    using Xunit;
+
+    public class DevouringLight
+    {
+        public class Ai : AiScenario
+        {
+            [Fact]
+            public void CastDevouringLight()
+            {
+                Battlefield(P1, "Grizzly Bears", "Grizzly Bears");
+
+                P2.Life = 2;
+                Battlefield(P2, "Grizzly Bears", "Plains", "Plains");
+                Hand(P2, "Devouring Light");
+
+                RunGame(1);
+
+                Equal(2, P2.Life);
+                Equal(0, P2.Hand.Count);
+                Equal(2, P2.Battlefield.Count);
+            }
+        }
+    }
+}
