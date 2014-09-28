192a3166d177814be700141b8bf5aa745acce781
Sandmann123 <norfirill@gmail.com>
2014-09-28 12:03:38 +0800

    Fixed Carnivorous Moss-Beast: changed timing rule. Tuned Midnight Guard

--- source/Grove/CardsLibrary/MidnightGuard.cs
+++ source/Grove/CardsLibrary/MidnightGuard.cs
@@ -1,33 +1,34 @@
 ﻿namespace Grove.CardsLibrary
 {
-    using System.Collections.Generic;
-    using Effects;
-    using Triggers;
+  using System.Collections.Generic;
+  using Effects;
+  using Triggers;
 
-    public class MidnightGuard : CardTemplateSource
+  public class MidnightGuard : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
     {
-        public override IEnumerable<CardTemplate> GetCards()
-        {
-            yield return Card
-                .Named("Midnight Guard")
-                .ManaCost("{2}{W}")
-                .Type("Creature — Human Soldier")
-                .Text("Whenever another creature enters the battlefield, untap Midnight Guard.")
-                .FlavorText("\"When you're on watch, no noise is harmless and no shadow can be ignored.\"{EOL}—Olgard of the Skiltfolk")
-                .Power(2)
-                .Toughness(3)
-                .TriggeredAbility(p =>
-                {
-                    p.Text = "Whenever another creature enters the battlefield, untap Midnight Guard.";
+      yield return Card
+          .Named("Midnight Guard")
+          .ManaCost("{2}{W}")
+          .Type("Creature — Human Soldier")
+          .Text("Whenever another creature enters the battlefield, untap Midnight Guard.")
+          .FlavorText("\"When you're on watch, no noise is harmless and no shadow can be ignored.\"{EOL}—Olgard of the Skiltfolk")
+          .Power(2)
+          .Toughness(3)
+          .TriggeredAbility(p =>
+          {
+            p.Text = "Whenever another creature enters the battlefield, untap Midnight Guard.";
 
-                    p.Trigger(new OnZoneChanged(
-                        to: Zone.Battlefield,
-                        filter: (card, ability, game) => card.Is().Creature && card != ability.OwningCard));
+            p.Trigger(new OnZoneChanged(
+                to: Zone.Battlefield,
+                filter: (card, ability, game) => card.Is().Creature && card != ability.OwningCard));
 
-                    p.Effect = () => new UntapOwner();
+            p.Effect = () => new UntapOwner();
 
-                    p.TriggerOnlyIfOwningCardIsInPlay = true;
-                });
-        }
+            p.TriggerOnlyIfOwningCardIsInPlay = true;
+            p.UsesStack = false;
+          });
     }
+  }
 }
