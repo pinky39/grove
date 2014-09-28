7ff4b2e21fc82ff1ca3ba1b7ac690585077664c3
pinky <pinky@lab.org>
2014-09-28 19:10:01 +0200

    Fixed Invasive Species.
    * Test did not work because ai played the land after it was returned to
      hand. Fixed it.
    * EffectBounce Targeting rule is meant for bouncing opponents creatures a
      better alternative is just to rank by score (lowest to greatest).

--- source/Grove/CardsLibrary/InvasiveSpecies.cs
+++ source/Grove/CardsLibrary/InvasiveSpecies.cs
@@ -1,36 +1,39 @@
 ﻿namespace Grove.CardsLibrary
 {
-    using System.Collections.Generic;
-    using AI.TargetingRules;
-    using AI.TimingRules;
-    using Effects;
-    using Triggers;
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+  using Triggers;
 
-    public class InvasiveSpecies : CardTemplateSource
+  public class InvasiveSpecies : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
     {
-        public override IEnumerable<CardTemplate> GetCards()
-        {
-            yield return Card
-              .Named("Invasive Species")
-              .ManaCost("{2}{G}")
-              .Type("Creature - Insect")
-              .Text("When Invasive Species enters the battlefield, return another permanent you control to its owner's hand.")
-              .FlavorText("It's easier to relocate a village that lies in their path than to turn the bugs aside.")
-              .Power(3)
-              .Toughness(3)
-              .TriggeredAbility(p =>
-              {
-                  p.Text = "When Invasive Species enters the battlefield, return another permanent you control to its owner's hand.";
+      yield return Card
+        .Named("Invasive Species")
+        .ManaCost("{2}{G}")
+        .Type("Creature - Insect")
+        .Text("When Invasive Species enters the battlefield, return another permanent you control to its owner's hand.")
+        .FlavorText("It's easier to relocate a village that lies in their path than to turn the bugs aside.")
+        .Power(3)
+        .Toughness(3)
+        .TriggeredAbility(p =>
+          {
+            p.Text =
+              "When Invasive Species enters the battlefield, return another permanent you control to its owner's hand.";
 
-                  p.Trigger(new OnZoneChanged(to: Zone.Battlefield));
-                  
-                  p.Effect = () => new ReturnToHand();
-                  
-                  p.TargetSelector.AddEffect(trg => trg.Is.Card(controlledBy: ControlledBy.SpellOwner, canTargetSelf: false).On.Battlefield());
+            p.Trigger(new OnZoneChanged(to: Zone.Battlefield));
 
-                  p.TargetingRule(new EffectBounce());
-                  p.TimingRule(new OnSecondMain());
-              });
-        }
+            p.Effect = () => new ReturnToHand();
+
+            p.TargetSelector.AddEffect(
+              trg => trg
+                .Is.Card(controlledBy: ControlledBy.SpellOwner, canTargetSelf: false)
+                .On.Battlefield());
+
+            p.TargetingRule(new EffectOrCostRankBy(x => x.Score));            
+          });
     }
+  }
 }
--- tests/Grove.Tests/Cards/InvasiveSpecies.cs
+++ tests/Grove.Tests/Cards/InvasiveSpecies.cs
@@ -1,25 +1,25 @@
 ﻿namespace Grove.Tests.Cards
 {
-    using Infrastructure;
-    using Xunit;
+  using Infrastructure;
+  using Xunit;
 
-    public class InvasiveSpecies
+  public class InvasiveSpecies
+  {
+    public class Ai : AiScenario
     {
-        public class Ai : AiScenario
-        {
-            [Fact(Skip="In real game it works. But it is failed in the test.")]
-            public void ReturnPermament()
-            {
-              var species = C("Invasive Species");
+      [Fact]
+      public void ReturnPermament()
+      {
+        var species = C("Invasive Species");
 
-              Hand(P1, species);
-              Battlefield(P1, "Forest", "Forest", "Forest");
+        Hand(P1, species);
+        Battlefield(P1, "Birds of Paradise", "Birds of Paradise", "Birds of Paradise");
 
-              RunGame(1);
+        RunGame(1);
 
-              Equal(Zone.Battlefield, C(species).Zone);
-              Equal(3, P1.Battlefield.Count);
-            }
-        }
+        Equal(Zone.Battlefield, C(species).Zone);
+        Equal(3, P1.Battlefield.Count);
+      }
     }
+  }
 }
