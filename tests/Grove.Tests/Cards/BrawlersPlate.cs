f7e3e8aba8ac19a18d38cb4dc1994bc23c614d55
pinky <pinky@lab.org>
2014-09-28 20:05:34 +0200

    Changed tabs to 2.

--- tests/Grove.Tests/Cards/BrawlersPlate.cs
+++ tests/Grove.Tests/Cards/BrawlersPlate.cs
@@ -1,25 +1,25 @@
 ﻿namespace Grove.Tests.Cards
 {
-    using Infrastructure;
-    using Xunit;
+  using Infrastructure;
+  using Xunit;
 
-    public class BrawlersPlate
+  public class BrawlersPlate
+  {
+    public class Ai : AiScenario
     {
-        public class Ai : AiScenario
-        {
-            [Fact]
-            public void EquipWithBrawlersPlate()
-            {
-                Battlefield(P1, "Grizzly Bears", "Brawler's Plate", "Mountain", "Mountain", "Mountain", "Mountain");
+      [Fact]
+      public void EquipWithBrawlersPlate()
+      {
+        Battlefield(P1, "Grizzly Bears", "Brawler's Plate", "Mountain", "Mountain", "Mountain", "Mountain");
 
-                P2.Life = 4;
-                Battlefield(P2, "Grizzly Bears");
+        P2.Life = 4;
+        Battlefield(P2, "Grizzly Bears");
 
-                RunGame(1);
+        RunGame(1);
 
-                Equal(2, P2.Life);
-                Equal(1, P2.Graveyard.Count);
-            }
-        }
+        Equal(2, P2.Life);
+        Equal(1, P2.Graveyard.Count);
+      }
     }
+  }
 }
