ed61dd33b14f9366963d2f6e518a5907947f357d
pinky <pinky@lab.org>
2014-09-29 21:04:09 +0200

    Fixed Crippling Blight.
    * added parameter blockOnly = true.

--- tests/Grove.Tests/Cards/CripplingBlight.cs
+++ tests/Grove.Tests/Cards/CripplingBlight.cs
@@ -1,26 +1,25 @@
 ﻿namespace Grove.Tests.Cards
 {
-    using System.Linq;
-    using Infrastructure;
-    using Xunit;
+  using Infrastructure;
+  using Xunit;
 
-    public class CripplingBlight
+  public class CripplingBlight
+  {
+    public class Predefined : AiScenario
     {
-        public class Predefined : AiScenario
-        {
-            [Fact]
-            public void CannotBlock()
-            {
-                Hand(P1, "Crippling Blight");
-                Battlefield(P1, "Grizzly Bears", "Swamp");
+      [Fact]
+      public void CannotBlock()
+      {
+        Hand(P1, "Crippling Blight");
+        Battlefield(P1, "Grizzly Bears", "Swamp");
 
-                P2.Life = 2;
-                Battlefield(P2, "Grizzly Bears");
+        P2.Life = 2;
+        Battlefield(P2, "Grizzly Bears");
 
-                RunGame(1);
+        RunGame(1);
 
-                Equal(0, P2.Life);
-            }
-        }
+        Equal(0, P2.Life);
+      }
     }
+  }
 }
