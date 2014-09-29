d365247fb07d39c2b330db5632631d99763ab1bb
pinky <pinky@lab.org>
2014-09-29 21:09:06 +0200

    Changed tabs to 2.

--- source/Grove/CardsLibrary/DivineFavor.cs
+++ source/Grove/CardsLibrary/DivineFavor.cs
@@ -1,38 +1,40 @@
 ﻿namespace Grove.CardsLibrary
 {
-    using System.Collections.Generic;
-    using AI;
-    using AI.TargetingRules;
-    using AI.TimingRules;
-    using Effects;
-    using Modifiers;
-    using Triggers;
+  using System.Collections.Generic;
+  using AI;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+  using Modifiers;
+  using Triggers;
 
-    public class DivineFavor : CardTemplateSource
+  public class DivineFavor : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
     {
-        public override IEnumerable<CardTemplate> GetCards()
-        {
-            yield return Card
-                .Named("Divine Favor")
-                .ManaCost("{1}{W}")
-                .Type("Enchantment - Aura")
-                .Text("Enchant creature{EOL}When Divine Favor enters the battlefield, you gain 3 life.{EOL}Enchanted creature gets +1/+3.")
-                .FlavorText("With an armory of light, even the squire may champion her people.")
-                .Cast(p =>
-                {
-                    p.Effect = () => new Attach(
-                        () => new AddPowerAndToughness(1, 3)).SetTags(EffectTag.IncreasePower, EffectTag.IncreasePower);
+      yield return Card
+        .Named("Divine Favor")
+        .ManaCost("{1}{W}")
+        .Type("Enchantment - Aura")
+        .Text(
+          "Enchant creature{EOL}When Divine Favor enters the battlefield, you gain 3 life.{EOL}Enchanted creature gets +1/+3.")
+        .FlavorText("With an armory of light, even the squire may champion her people.")
+        .Cast(p =>
+          {
+            p.Effect = () => new Attach(
+              () => new AddPowerAndToughness(1, 3))
+              .SetTags(EffectTag.IncreasePower, EffectTag.IncreasePower);
 
-                    p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
-                    p.TimingRule(new OnFirstMain());
-                    p.TargetingRule(new EffectCombatEnchantment());
-                })
-                .TriggeredAbility(p =>
-                {
-                    p.Text = "When Divine Favor enters the battlefield, you gain 3 life.";
-                    p.Trigger(new OnZoneChanged(to: Zone.Battlefield));
-                    p.Effect = () => new YouGainLife(3);
-                });
-        }
+            p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
+            p.TimingRule(new OnFirstMain());
+            p.TargetingRule(new EffectCombatEnchantment());
+          })
+        .TriggeredAbility(p =>
+          {
+            p.Text = "When Divine Favor enters the battlefield, you gain 3 life.";
+            p.Trigger(new OnZoneChanged(to: Zone.Battlefield));
+            p.Effect = () => new YouGainLife(3);
+          });
     }
+  }
 }
--- tests/Grove.Tests/Cards/DivineFavor.cs
+++ tests/Grove.Tests/Cards/DivineFavor.cs
@@ -1,25 +1,25 @@
 ﻿namespace Grove.Tests.Cards
 {
-    using Infrastructure;
-    using Xunit;
+  using Infrastructure;
+  using Xunit;
 
-    public class DivineFavor
+  public class DivineFavor
+  {
+    public class Ai : AiScenario
     {
-        public class Ai : AiScenario
-        {
-            [Fact]
-            public void GetLifeAndCounter13()
-            {
-                Hand(P1, "Divine Favor");
-                Battlefield(P1, "Grizzly Bears", "Swamp", "Plains");
+      [Fact]
+      public void GetLifeAndCounter13()
+      {
+        Hand(P1, "Divine Favor");
+        Battlefield(P1, "Grizzly Bears", "Swamp", "Plains");
 
-                P2.Life = 3;
+        P2.Life = 3;
 
-                RunGame(1);
+        RunGame(1);
 
-                Equal(0, P2.Life);
-                Equal(23, P1.Life);
-            }
-        }
+        Equal(0, P2.Life);
+        Equal(23, P1.Life);
+      }
     }
+  }
 }
