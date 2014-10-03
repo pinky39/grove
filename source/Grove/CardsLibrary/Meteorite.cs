9678c17b0be709a56c9d768e3aacc957b8479177
pinky <pinky@lab.org>
2014-10-03 22:40:30 +0200

    Tabs 2.

--- source/Grove/CardsLibrary/Meteorite.cs
+++ source/Grove/CardsLibrary/Meteorite.cs
@@ -1,36 +1,34 @@
 ﻿namespace Grove.CardsLibrary
 {
-    using System.Collections.Generic;
-    using AI.TargetingRules;
-    using Effects;
-    using Triggers;
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using Effects;
+  using Triggers;
 
-    public class Meteorite : CardTemplateSource
+  public class Meteorite : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
     {
-        public override IEnumerable<CardTemplate> GetCards()
-        {
-            yield return Card
-              .Named("Meteorite")
-              .ManaCost("{5}")
-              .Type("Artifact")
-              .Text("When Meteorite enters the battlefield, it deals 2 damage to target creature or player.{EOL}{T}: Add one mana of any color to your mana pool.")
-              .FlavorText("\"And if I'm lying,\" he began...")
-              .TriggeredAbility(p =>
-              {
-                  p.Text = "When Meteorite enters the battlefield, it deals 2 damage to target creature or player";
-
-                  p.Trigger(new OnZoneChanged(to: Zone.Battlefield));
-
-                  p.Effect = () => new DealDamageToTargets(2);
-                  p.TargetSelector.AddEffect(trg => trg.Is.CreatureOrPlayer().On.Battlefield());
-
-                  p.TargetingRule(new EffectDealDamage(2));
-              })
-              .ManaAbility(p =>
-              {
-                  p.Text = "{T}: Add one mana of any color to your mana pool.";
-                  p.ManaAmount(Mana.Any);
-              });
-        }
+      yield return Card
+        .Named("Meteorite")
+        .ManaCost("{5}")
+        .Type("Artifact")
+        .Text(
+          "When Meteorite enters the battlefield, it deals 2 damage to target creature or player.{EOL}{T}: Add one mana of any color to your mana pool.")
+        .FlavorText("\"And if I'm lying,\" he began...")
+        .TriggeredAbility(p =>
+          {
+            p.Text = "When Meteorite enters the battlefield, it deals 2 damage to target creature or player";
+            p.Trigger(new OnZoneChanged(to: Zone.Battlefield));
+            p.Effect = () => new DealDamageToTargets(2);
+            p.TargetSelector.AddEffect(trg => trg.Is.CreatureOrPlayer().On.Battlefield());
+            p.TargetingRule(new EffectDealDamage(2));
+          })
+        .ManaAbility(p =>
+          {
+            p.Text = "{T}: Add one mana of any color to your mana pool.";
+            p.ManaAmount(Mana.Any);
+          });
     }
+  }
 }
