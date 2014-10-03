0aa268e3934f1c7c3501168d1c248ae294a5f421
Sandmann123 <norfirill@gmail.com>
2014-10-03 15:15:48 +0800

    Added new card: Heliod's Pilgrim

---
+++ tests/Grove.Tests/Cards/HeliodsPilgrim.cs
@@ -0,0 +1,24 @@
+﻿namespace Grove.Tests.Cards
+{
+    using System.Linq;
+    using Infrastructure;
+    using Xunit;
+
+    public class HeliodsPilgrim
+    {
+        public class Ai : AiScenario
+        {
+            [Fact]
+            public void CastHeliodsPilgrimSearchAura()
+            {
+                Library(P1, "Plains","Divine Favor", "Plains");
+                Battlefield(P1, "Plains", "Plains", "Forest");
+                Hand(P1, "Heliod's Pilgrim");
+
+                RunGame(1);
+
+                True(P1.Hand.Any(c => c.Is().Aura));
+            }
+        }
+    }
+}
