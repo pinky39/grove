6b5503ff1e6c098c2bbe19aa06f3797c2bbb6f71
Sandmann123 <norfirill@gmail.com>
2014-10-04 22:11:00 +0800

    Added new card: Kiora's Follower.

---
+++ source/Grove/CardsLibrary/KiorasFollower.cs
@@ -0,0 +1,34 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using Costs;
+  using Effects;
+
+  public class KiorasFollower : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+          .Named("Kiora's Follower")
+          .ManaCost("{G}{U}")
+          .Type("Creature — Merfolk")
+          .Text("{T}: Untap another target permanent.")
+          .FlavorText("\"She may call herself Kiora but I believe she is Thassa, the embodiment of the sea and empress of the depths.\"")
+          .Power(2)
+          .Toughness(2)
+          .ActivatedAbility(p =>
+          {
+            p.Text = "{T}: Untap another target permanent.";
+
+            p.Cost = new Tap();
+
+            p.Effect = () => new UntapTargetPermanents();
+
+            p.TargetSelector.AddEffect(trg => trg.Is.Card(c => c.IsTapped, canTargetSelf: false).On.Battlefield());
+
+            p.TargetingRule(new EffectUntapPermanent());
+          });
+    }
+  }
+}
