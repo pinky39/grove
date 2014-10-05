98df61d5b6563d89f95b4cbf0cbfa7ffc47f78b5
Sandmann123 <norfirill@gmail.com>
2014-10-05 23:37:00 +0800

    Added new card: Paragon of New Dawns

---
+++ source/Grove/CardsLibrary/ParagonOfNewDawns.cs
@@ -0,0 +1,46 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Costs;
+  using Effects;
+  using Modifiers;
+
+  public class ParagonOfNewDawns : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Paragon of New Dawns")
+        .ManaCost("{3}{W}")
+        .Type("Creature — Human Soldier")
+        .Text("Other white creatures you control get +1/+1.{EOL}{W},{T}: Another target white creature you control gains vigilance until end of turn.{I}(Attacking doesn't cause it to tap.){/I}")
+        .Power(2)
+        .Toughness(2)
+        .Cast(p =>
+        {
+          p.Effect = () => new PutIntoPlay().SetTags(EffectTag.IncreasePower, EffectTag.IncreaseToughness);
+        })
+        .ContinuousEffect(p =>
+        {
+          p.Modifier = () => new AddPowerAndToughness(1, 1);
+          p.CardFilter = (c, e) => c.Controller == e.Source.Controller && c.Is().Creature && c.HasColor(CardColor.White) && c != e.Source;
+        })
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{W},{T}: Another target white creature you control gains vigilance until end of turn.";
+          p.Cost = new AggregateCost(new PayMana("{W}".Parse(), ManaUsage.Abilities), new Tap());
+          p.Effect = () => new ApplyModifiersToTargets(() => new AddStaticAbility(Static.Vigilance){ UntilEot = true });
+
+          p.TargetSelector.AddEffect(trg => trg
+              .Is.Card(c => c.Is().Creature && c.HasColor(CardColor.White), controlledBy: ControlledBy.SpellOwner, canTargetSelf: false)
+              .On.Battlefield());
+
+          p.TimingRule(new OnFirstMain());
+          p.TargetingRule(new EffectOrCostRankBy(c => -c.Score, controlledBy: ControlledBy.SpellOwner));
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/ParagonOfNewDawns.cs
@@ -0,0 +1,37 @@
+﻿namespace Grove.Tests.Cards
+{
+  using System.Linq;
+  using Infrastructure;
+  using Xunit;
+
+  public class ParagonOfNewDawns
+  {
+    public class Predefined : PredefinedScenario
+    {
+      [Fact]
+      public void PumpYourCreatures()
+      {
+        var soldier = C("Paragon Of New Dawns");
+        var bear = C("Grizzly Bears");
+        var cat1 = C("Oreskos Swiftclaw");
+        var cat2 = C("Oreskos Swiftclaw");
+
+        Hand(P1, soldier);
+        Battlefield(P1, bear, cat1);
+        Battlefield(P2, cat2);
+
+        Exec(
+          At(Step.FirstMain)
+            .Cast(soldier)
+            .Verify(() =>
+            {
+              Equal(2, C(bear).Power);
+              Equal(2, C(soldier).Power);
+              Equal(4, C(cat1).Power);
+              Equal(3, C(cat2).Power);              
+            })
+          );
+      }
+    }
+  }
+}
