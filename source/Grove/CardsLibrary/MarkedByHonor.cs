b569e5c0d302de026329c223eec333efba408a08
Sandmann123 <norfirill@gmail.com>
2014-10-05 20:33:55 +0800

    Added new card: Marked  By  Honor

---
+++ source/Grove/CardsLibrary/MarkedByHonor.cs
@@ -0,0 +1,33 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+  using Modifiers;
+
+  public class MarkedByHonor : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Marked by Honor")
+        .ManaCost("{3}{W}")
+        .Type("Enchantment - Aura")
+        .Text("Enchant creature{EOL}Enchanted creature gets +2/+2 and has vigilance.{I}(Attacking doesn't cause it to tap.){/I}")
+        .FlavorText("Stand your post for duty. Stand your ground for honor.")
+        .Cast(p =>
+        {
+          p.Effect = () => new Attach(
+            () => new AddStaticAbility(Static.Vigilance),
+            () => new AddPowerAndToughness(2, 2))
+            .SetTags(EffectTag.IncreasePower, EffectTag.IncreasePower);
+
+          p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
+          p.TimingRule(new OnFirstMain());
+          p.TargetingRule(new EffectCombatEnchantment());
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/MarkedByHonor.cs
@@ -0,0 +1,26 @@
+﻿namespace Grove.Tests.Cards
+{
+  using System.Linq;
+  using Infrastructure;
+  using Xunit;
+
+  public class MarkedByHonor
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void GiveVigilanceAnd22()
+      {
+        Hand(P1, "Marked By Honor");
+        Battlefield(P1, "Grizzly Bears", "Plains", "Plains", "Plains", "Plains");
+
+        P2.Life = 4;
+
+        RunGame(1);
+
+        Equal(0, P2.Life);
+        True(P1.Battlefield.Creatures.All(c => !c.IsTapped));
+      }
+    }
+  }
+}
