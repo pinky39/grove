1be42dfa1b7d2676910782f2d2eba483af5ae2c0
Sandmann123 <norfirill@gmail.com>
2014-10-06 21:07:32 +0800

    Added new card: Spectra Ward

---
+++ source/Grove/CardsLibrary/SpectraWard.cs
@@ -0,0 +1,32 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+  using Modifiers;
+
+  public class SpectraWard : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Spectra Ward")
+        .ManaCost("{3}{W}{W}")
+        .Type("Enchantment - Aura")
+        .Text("Enchant creature{EOL}Enchanted creature gets +2/+2 and has protection from all colors. This effect doesn't remove Auras. {I}(It can't be blocked, targeted, or dealt damage by anything that's white, blue, black, red, or green.){/I}")
+        .Cast(p =>
+        {
+          p.Effect = () => new Attach(
+            () => new AddPowerAndToughness(2, 2),
+            () => new AddProtectionFromColors(L(CardColor.Black, CardColor.Blue, CardColor.Green, CardColor.Red, CardColor.White)))
+            .SetTags(EffectTag.IncreasePower, EffectTag.IncreasePower);
+
+          p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
+          p.TimingRule(new OnFirstMain());
+          p.TargetingRule(new EffectCombatEnchantment());
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/SpectraWard.cs
@@ -0,0 +1,25 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class SpectraWard
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void GiveProtectionAnd22()
+      {
+        Hand(P1, "Spectra Ward");
+        Battlefield(P1, "Grizzly Bears", "Swamp", "Plains", "Swamp", "Plains", "Swamp", "Plains");
+
+        P2.Life = 4;
+        Battlefield(P2, "Grizzly Bears");
+
+        RunGame(1);
+
+        Equal(0, P2.Life);
+      }
+    }
+  }
+}
