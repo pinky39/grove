f35d9270fee3363f19febd5ed98dfa62ebbdb2ba
pinky <pinky@lab.org>
2014-10-06 23:07:07 +0200

    Improved Research Assistant.
    * Added a timing rule to improve performance.

--- source/Grove/CardsLibrary/ResearchAssistant.cs
+++ source/Grove/CardsLibrary/ResearchAssistant.cs
@@ -1,30 +1,33 @@
 ﻿namespace Grove.CardsLibrary
 {
-    using System.Collections.Generic;
-    using Costs;
-    using Effects;
+  using System.Collections.Generic;
+  using AI.TimingRules;
+  using Costs;
+  using Effects;
 
-    public class ResearchAssistant : CardTemplateSource
+  public class ResearchAssistant : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
     {
-        public override IEnumerable<CardTemplate> GetCards()
-        {
-            yield return Card
-              .Named("Research Assistant")
-              .ManaCost("{1}{U}")
-              .Type("Creature - Human Wizard")
-              .Text("{3}{U},{T}: Draw a card, then discard a card.")
-              .FlavorText("There are many words and phrases that can cause an experienced wizard to tremble in fear. Chief among them is \"oops.\"")
-              .Power(1)
-              .Toughness(3)
-              .ActivatedAbility(p =>
-              {
-                  p.Text = "{3}{U},{T}: Draw a card, then discard a card.";
-                  p.Cost = new AggregateCost(
-                      new PayMana("{3}{U}".Parse(), ManaUsage.Abilities),
-                      new Tap());                      
+      yield return Card
+        .Named("Research Assistant")
+        .ManaCost("{1}{U}")
+        .Type("Creature - Human Wizard")
+        .Text("{3}{U},{T}: Draw a card, then discard a card.")
+        .FlavorText(
+          "There are many words and phrases that can cause an experienced wizard to tremble in fear. Chief among them is \"oops.\"")
+        .Power(1)
+        .Toughness(3)
+        .ActivatedAbility(p =>
+          {
+            p.Text = "{3}{U},{T}: Draw a card, then discard a card.";
+            p.Cost = new AggregateCost(
+              new PayMana("{3}{U}".Parse(), ManaUsage.Abilities),
+              new Tap());
 
-                  p.Effect = () => new DrawCards(1, discardCount: 1);
-              });
-        }
+            p.Effect = () => new DrawCards(1, discardCount: 1);
+            p.TimingRule(new OnEndOfOpponentsTurn());
+          });
     }
+  }
 }
