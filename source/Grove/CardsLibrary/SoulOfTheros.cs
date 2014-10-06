0b4a203b73c8ceaac618828229294862746c347b
Sandmann123 <norfirill@gmail.com>
2014-10-06 17:37:02 +0800

    Added new card: Soul of Theros

---
+++ source/Grove/CardsLibrary/SoulOfTheros.cs
@@ -0,0 +1,58 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI;
+  using AI.TimingRules;
+  using Costs;
+  using Effects;
+  using Modifiers;
+
+  public class SoulOfTheros : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Soul of Theros")
+        .ManaCost("{4}{W}{W}")
+        .Type("Creature — Avatar")
+        .Text("{Vigilance}{EOL}{4}{W}{W}: Creatures you control get +2/+2 and gain first strike and lifelink until end of turn.{EOL}{4}{W}{W}, Exile Soul of Theros from your graveyard: Creatures you control get +2/+2 and gain first strike and lifelink until end of turn.")
+        .Power(6)
+        .Toughness(6)
+        .SimpleAbilities(Static.Vigilance)
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{4}{W}{W}: Creatures you control get +2/+2 and gain first strike and lifelink until end of turn.";
+          p.Cost = new PayMana("{4}{W}{W}".Parse(), ManaUsage.Abilities);
+
+          p.Effect = () => new ApplyModifiersToPermanents(
+            selector: (e, c) => c.Is().Creature,
+            controlledBy: ControlledBy.SpellOwner,
+            modifiers: new CardModifierFactory[]
+            {
+              () => new AddPowerAndToughness(2, 2){UntilEot = true},
+              () => new AddStaticAbility(Static.FirstStrike){UntilEot = true},
+              () => new AddStaticAbility(Static.Lifelink){UntilEot = true}
+            }).SetTags(EffectTag.IncreasePower, EffectTag.IncreaseToughness);
+
+          p.TimingRule(new Any(new AfterOpponentDeclaresAttackers(), new BeforeYouDeclareAttackers()));
+        })
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{4}{W}{W}, Exile Soul of Theros from your graveyard: Creatures you control get +2/+2 and gain first strike and lifelink until end of turn.";
+          p.Cost = new AggregateCost(
+            new PayMana("{4}{W}{W}".Parse(), ManaUsage.Abilities),
+            new Exile());
+
+          p.Effect = () => new ApplyModifiersToPermanents(
+            selector: (e, c) => c.Is().Creature,
+            controlledBy: ControlledBy.SpellOwner,
+            modifiers: new CardModifierFactory[]
+            {
+              () => new AddPowerAndToughness(2, 2){UntilEot = true},
+              () => new AddStaticAbility(Static.FirstStrike){UntilEot = true},
+              () => new AddStaticAbility(Static.Lifelink){UntilEot = true}
+            }).SetTags(EffectTag.IncreasePower, EffectTag.IncreaseToughness);
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/SoulOfTheros.cs
@@ -0,0 +1,22 @@
+﻿namespace Grove.Tests.Cards
+{
+  using System.Linq;
+  using Infrastructure;
+  using Xunit;
+
+  public class SoulOfTheros
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void AllCreaturesGet22()
+      {
+        Battlefield(P1, "Soul Of Theros", "Grizzly Bears", "Forest", "Forest", "Forest", "Plains", "Plains", "Plains");
+
+        RunGame(1);
+
+        Equal(8, P2.Life);
+      }
+    }
+  }
+}
