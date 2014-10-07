60420a893c33ae101ae8703b0d3d4c10092d445e
Sandmann123 <norfirill@gmail.com>
2014-10-07 15:03:04 +0800

    Added new card: Amphin Pathmage

---
+++ source/Grove/CardsLibrary/AmphinPathmage.cs
@@ -0,0 +1,37 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Costs;
+  using Effects;
+  using Modifiers;
+
+  public class AmphinPathmage : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Amphin Pathmage")
+        .ManaCost("{3}{U}")
+        .Type("Creature - Salamander Wizard")
+        .Text("{2}{U}: Target creature can't be blocked this turn.")
+        .FlavorText("\"There are those who do not believe in the existence of the amphin. This seems somehow to be of their own design.\"{EOL}—Gor Muldrak, Cryptohistories ")
+        .Power(3)
+        .Toughness(2)
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{2}{U}: Target creature can't be blocked this turn.";
+          p.Cost = new PayMana("{2}{U}".Parse(), ManaUsage.Abilities);
+
+          p.Effect = () => new ApplyModifiersToTargets(
+              () => new AddStaticAbility(Static.Unblockable) { UntilEot = true });
+
+          p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
+
+          p.TimingRule(new BeforeYouDeclareAttackers());
+          p.TargetingRule(new EffectBigWithoutEvasions());
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/AmphinPathmage.cs
@@ -0,0 +1,24 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class AmphinPathmage
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void GiveUnblockable()
+      {
+        Battlefield(P1, "Amphin Pathmage", "Island", "Plains", "Island");
+
+        P2.Life = 3;
+        Battlefield(P2, "Grizzly Bears", "Island", "Plains", "Island");
+
+        RunGame(1);
+
+        Equal(0, P2.Life);
+      }
+    }
+  }
+}
