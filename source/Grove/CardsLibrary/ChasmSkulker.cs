2820da6bbe850f28ff9edf1184300498828ca0d8
Sandmann123 <norfirill@gmail.com>
2014-10-07 16:00:20 +0800

    Added new card: Chasm Skulker

---
+++ source/Grove/CardsLibrary/ChasmSkulker.cs
@@ -0,0 +1,50 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI;
+  using Effects;
+  using Modifiers;
+  using Triggers;
+
+  public class ChasmSkulker : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Chasm Skulker")
+        .ManaCost("{2}{U}")
+        .Type("Creature - Squid Horror")
+        .Text("Whenever you draw a card, put a +1/+1 counter on Chasm Skulker.{EOL}When Chasm Skulker dies, put X 1/1 blue Squid creature tokens with islandwalk onto the battlefield, where X is the number of +1/+1 counters on Chasm Skulker. {I}(They can't be blocked as long as defending player controls an Island.){/I}")
+        .Power(1)
+        .Toughness(1)
+        .TriggeredAbility(p =>
+        {
+          p.Text = "Whenever you draw a card, put a +1/+1 counter on Chasm Skulker.";
+
+          p.Trigger(new OnPlayerDrawsCard((ability, player) => ability.OwningCard.Controller == player));
+
+          p.Effect = () => new ApplyModifiersToSelf(() => new AddCounters(() => new PowerToughness(1, 1), count: 1))
+            .SetTags(EffectTag.IncreasePower, EffectTag.IncreaseToughness);
+
+          p.TriggerOnlyIfOwningCardIsInPlay = true;
+        })
+        .TriggeredAbility(p =>
+        {
+          p.Text = "When Chasm Skulker dies, put X 1/1 blue Squid creature tokens with islandwalk onto the battlefield, where X is the number of +1/+1 counters on Chasm Skulker.";
+
+          p.Trigger(new OnZoneChanged(from: Zone.Battlefield, to: Zone.Graveyard));
+
+          p.Effect = () => new CreateTokens(
+              count: P(e => e.Source.OwningCard.Counters),
+              token: Card
+                .Named("Squid")
+                .Power(1)
+                .Toughness(1)
+                .Type("Token Creature - Squid")
+                .Text("{Islandwalk}")
+                .Colors(CardColor.Blue)
+                .SimpleAbilities(Static.Islandwalk));
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/ChasmSkulker.cs
@@ -0,0 +1,45 @@
+﻿namespace Grove.Tests.Cards
+{
+  using System.Linq;
+  using Infrastructure;
+  using Xunit;
+
+  public class ChasmSkulker
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void DrawCardAddCounter()
+      {
+        var skulker = C("Chasm Skulker");
+
+        Library(P1, "Swamp");
+
+        Library(P2, "Island");
+        Battlefield(P2, skulker);
+
+        RunGame(3);
+
+        Equal(2, C(skulker).Power);
+      }
+
+      [Fact]
+      public void DestroySkulkerCreateTokens()
+      {
+        var skulker = C("Chasm Skulker");
+
+        P1.Life = 2;
+        Battlefield(P1, "Grizzly Bears");
+
+        Library(P2, "Island");
+        Battlefield(P2, skulker);
+
+        RunGame(3);
+
+        Equal(0, P1.Battlefield.Count);
+        True(C(skulker).Zone == Zone.Graveyard);
+        Equal(1, P2.Battlefield.Creatures.Count(c => c.Is().Token));
+      }
+    }
+  }
+}
