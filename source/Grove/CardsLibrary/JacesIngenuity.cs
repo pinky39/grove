b7b0ed203dae626eef760bc4c271fdebe7bdeb11
Sandmann123 <norfirill@gmail.com>
2014-10-08 16:07:38 +0800

    Added new card: Jace's Ingenuity

---
+++ source/Grove/CardsLibrary/JacesIngenuity.cs
@@ -0,0 +1,22 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+
+  public class JacesIngenuity : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Jace's Ingenuity")
+        .ManaCost("{3}{U}{U}")
+        .Type("Instant")
+        .Text("Draw three cards.")
+        .FlavorText("\"Brute force can sometimes kick down a locked door, but knowledge is a skeleton key.\"")
+        .Cast(p =>
+        {
+          p.Effect = () => new DrawCards(3);
+        });
+    }
+  }
+}
