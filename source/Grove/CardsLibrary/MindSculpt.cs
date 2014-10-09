34ebad2af5306f3da3503ed0248455838e26a51d
Sandmann123 <norfirill@gmail.com>
2014-10-09 14:20:53 +0800

    Added new card: Mind Sculpt

---
+++ source/Grove/CardsLibrary/MindSculpt.cs
@@ -0,0 +1,22 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+
+  public class MindSculpt : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Mind Sculpt")
+        .ManaCost("{1}{U}")
+        .Type("Sorcery")
+        .Text("Target opponent puts the top seven cards of his or her library into his or her graveyard.")
+        .FlavorText("\"Your mind was a curious mix of madness and genius. I just took away the genius.\"{EOL}—Jace Beleren")
+        .Cast(p =>
+        {
+          p.Effect = () => new OpponentPutsTopCardsFromLibraryToGraveyard(7);;
+        });
+    }
+  }
+}
