1cd186576e4f1398ad59ac280fb89be1f62a3445
Sandmann123 <norfirill@gmail.com>
2014-10-09 22:17:22 +0800

    Added new card: Covenant of Blood

---
+++ source/Grove/CardsLibrary/CovenantOfBlood.cs
@@ -0,0 +1,28 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using Effects;
+
+  public class CovenantOfBlood : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Covenant of Blood")
+        .ManaCost("{6}{B}")
+        .Type("Sorcery")
+        .Text("{Convoke} {I}(Your creatures can help cast this spell. Each creature you tap while casting this spell pays for {1} or one mana of that creature's color.){/I}{EOL}Covenant of Blood deals 4 damage to target creature or player and you gain 4 life.")
+        .Convoke()
+        .Cast(p =>
+        {
+          p.Text = "Covenant of Blood deals 4 damage to target creature or player and you gain 4 life.";
+          p.Effect = () => new DealDamageToTargets(amount: 4, gainLife: true);
+
+          p.TargetSelector.AddEffect(trg => trg.Is.CreatureOrPlayer().On.Battlefield());
+
+          p.TargetingRule(new EffectDealDamage(4));
+        });
+    }
+  }
+}
