809f16774294dd118053a2eff503102f765a4db0
Sandmann123 <norfirill@gmail.com>
2014-10-09 17:19:16 +0800

    Added new card: Soul of Ravnica

---
+++ tests/Grove.Tests/Cards/SoulOfRavnica.cs
@@ -0,0 +1,22 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class SoulOfRavnica
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void Draw2Cards()
+      {
+        Library(P1, "Grizzly Bears", "Grizzly Bears", "Grizzly Bears");
+        Battlefield(P1, "Soul of Ravnica", "Grizzly Bears", "Grizzly Bears", "Juggernaut", "Island", "Island", "Island", "Island", "Plains", "Plains", "Plains");
+
+        RunGame(1);
+
+        Equal(2, P1.Hand.Count);
+      }
+    }
+  }
+}
