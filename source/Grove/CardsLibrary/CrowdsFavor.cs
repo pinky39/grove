88ca4f1065ad91eb213661032ef081cd57243992
Sandmann123 <norfirill@gmail.com>
2014-10-11 00:25:07 +0800

    Added new card: Crowd's Favor
    * changed tabs to 2

---
+++ source/Grove/CardsLibrary/CrowdsFavor.cs
@@ -0,0 +1,36 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+  using Modifiers;
+
+  public class CrowdsFavor : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Crowd's Favor")
+        .ManaCost("{R}")
+        .Type("Instant")
+        .Text("{Convoke} {I}(Your creatures can help cast this spell. Each creature you tap while casting this spell pays for {1} or one mana of that creature's color.){/I}{EOL}Target creature gets +1/+0 and gains first strike until end of turn. {I}(It deals combat damage before creatures without first strike.){/I}")
+        .Convoke()
+        .Cast(p =>
+        {
+          p.Text = "Target creature gets +1/+0 and gains first strike until end of turn.";
+
+          p.Effect = () => new ApplyModifiersToTargets(
+            () => new AddPowerAndToughness(1, 0){UntilEot = true},
+            () => new AddStaticAbility(Static.FirstStrike) { UntilEot = true }).SetTags(
+              EffectTag.IncreasePower);
+
+          p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
+
+          p.TimingRule(new PumpTargetCardTimingRule());
+          p.TargetingRule(new EffectPumpInstant(1, 0));
+        });
+    }
+  }
+}
--- source/Grove/CardsLibrary/EphemeralShields.cs
+++ source/Grove/CardsLibrary/EphemeralShields.cs
@@ -1,34 +1,34 @@
 ﻿namespace Grove.CardsLibrary
 {
-    using System.Collections.Generic;
-    using AI;
-    using AI.TargetingRules;
-    using Effects;
-    using Modifiers;
+  using System.Collections.Generic;
+  using AI;
+  using AI.TargetingRules;
+  using Effects;
+  using Modifiers;
 
-    public class EphemeralShields : CardTemplateSource
+  public class EphemeralShields : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
     {
-        public override IEnumerable<CardTemplate> GetCards()
+      yield return Card
+        .Named("Ephemeral Shields")
+        .ManaCost("{1}{W}")
+        .Type("Instant")
+        .Text("{Convoke} {I}(Your creatures can help cast this spell. Each creature you tap while casting this spell pays for {1} or one mana of that creature's color.){/I}{EOL}Target creature gains indestructible until end of turn.{I}(Damage and effects that say \"destroy\" don't destroy it.){/I}")
+        .FlavorText("\"Even your shadow is too foul to tolerate.\"")
+        .Convoke()
+        .Cast(p =>
         {
-          yield return Card
-            .Named("Ephemeral Shields")
-            .ManaCost("{1}{W}")
-            .Type("Instant")
-            .Text("{Convoke} (Your creatures can help cast this spell. Each creature you tap while casting this spell pays for {1} or one mana of that creature's color.){EOL}Target creature gains indestructible until end of turn.{I}(Damage and effects that say \"destroy\" don't destroy it.){/I}")
-            .FlavorText("\"Even your shadow is too foul to tolerate.\"")
-            .Convoke()
-            .Cast(p =>
-            {
-              p.Text = "Target creature gains indestructible until end of turn.";
+          p.Text = "Target creature gains indestructible until end of turn.";
 
-              p.Effect = () => new ApplyModifiersToTargets(
-                () => new AddStaticAbility(Static.Indestructible) {UntilEot = true}).SetTags(
-                  EffectTag.Indestructible);
+          p.Effect = () => new ApplyModifiersToTargets(
+            () => new AddStaticAbility(Static.Indestructible) { UntilEot = true }).SetTags(
+              EffectTag.Indestructible);
 
-              p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
+          p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
 
-              p.TargetingRule(new EffectGiveIndestructible());
-            });
-        }
+          p.TargetingRule(new EffectGiveIndestructible());
+        });
     }
+  }
 }
