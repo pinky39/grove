59aee51e3c3a7939d0f5f2117f022c12f87b87b8
Sandmann123 <norfirill@gmail.com>
2014-10-10 23:36:04 +0800

    Added new card: Borderland Marauder

---
+++ source/Grove/CardsLibrary/BorderlandMarauder.cs
@@ -0,0 +1,32 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI;
+  using Effects;
+  using Modifiers;
+  using Triggers;
+
+  public class BorderlandMarauder : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+          .Named("Borderland Marauder")
+          .ManaCost("{1}{R}")
+          .Type("Creature — Human Warrior")
+          .Text("Whenever Borderland Marauder attacks, it gets +2/+0 until end of turn.")
+          .FlavorText("Though she is rightly feared, there are relatively few tales of her deeds in battle, for few survive her raids.")
+          .Power(1)
+          .Toughness(2)
+          .TriggeredAbility(p =>
+          {
+            p.Text = "Whenever Borderland Marauder attacks, it gets +2/+0 until end of turn.";
+
+            p.Trigger(new OnAttack());
+
+            p.Effect = () => new ApplyModifiersToSelf(
+              () => new AddPowerAndToughness(2, 0) { UntilEot = true }).SetTags(EffectTag.IncreasePower, EffectTag.IncreaseToughness);
+          });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/BorderlandMarauder.cs
@@ -0,0 +1,21 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class BorderlandMarauder
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void IncreasePowerBy2OnAttack()
+      {
+        Battlefield(P1, "Borderland Marauder");
+
+        RunGame(1);
+
+        Equal(17, P2.Life);
+      }
+    }
+  }
+}
