0af647ee965a6e86d6dffff3ea066a74e59b3a85
Sandmann123 <norfirill@gmail.com>
2014-10-10 10:26:14 +0800

    Added new card: Endless Obedience

---
+++ source/Grove/CardsLibrary/EndlessObedience.cs
@@ -0,0 +1,30 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using Effects;
+
+  public class EndlessObedience : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Endless Obedience")
+        .ManaCost("{4}{B}{B}")
+        .Type("Sorcery")
+        .Text("{Convoke} {I}(Your creatures can help cast this spell. Each creature you tap while casting this spell pays for {1} or one mana of that creature's color.){/I}{EOL}Put target creature card from a graveyard onto the battlefield under your control.")
+        .FlavorText("The death of a scout can be as informative as a safe return.")
+        .Convoke()
+        .Cast(p =>
+        {
+          p.Text = "Put target creature card from a graveyard onto the battlefield under your control.";
+
+          p.Effect = () => new PutTargetsToBattlefield();
+
+          p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Graveyard());
+
+          p.TargetingRule(new EffectOrCostRankBy(c => -c.Score));
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/EndlessObedience.cs
@@ -0,0 +1,25 @@
+﻿namespace Grove.Tests.Cards
+{
+  using System.Linq;
+  using Infrastructure;
+  using Xunit;
+
+  public class EndlessObedience
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void ReturnForceFromOpponentsGraveyard()
+      {
+        Battlefield(P1, "Swamp", "Swamp", "Swamp", "Swamp", "Swamp", "Swamp");
+        Hand(P1, "Endless Obedience");
+        
+        Graveyard(P2, "Verdant Force");
+
+        RunGame(1);
+
+        Equal(1, P1.Battlefield.Creatures.Count());
+      }
+    }
+  }
+}
