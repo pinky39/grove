998188e1d48e7591cb5ddd89ed2fd25e85b35931
Sandmann123 <norfirill@gmail.com>
2014-10-10 16:14:22 +0800

    Added new card: Soul of Innistrad

---
+++ source/Grove/CardsLibrary/SoulOfInnistrad.cs
@@ -0,0 +1,56 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Costs;
+
+  public class SoulOfInnistrad : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Soul of Innistrad")
+        .ManaCost("{4}{B}{B}")
+        .Type("Creature — Avatar")
+        .Text("{Deathtouch}{EOL}{3}{B}{B}: Return up to three target creature cards from your graveyard to your hand.{EOL}{3}{B}{B}, Exile Soul of Innistrad from your graveyard: Return up to three target creature cards from your graveyard to your hand.")
+        .Power(6)
+        .Toughness(6)
+        .SimpleAbilities(Static.Deathtouch)
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{3}{B}{B}: Return up to three target creature cards from your graveyard to your hand.";
+          p.Cost = new PayMana("{3}{B}{B}".Parse(), ManaUsage.Abilities);
+
+          p.Effect = () => new Effects.ReturnToHand();
+          p.TargetSelector.AddEffect(trg =>
+          {
+            trg.MinCount = 0;
+            trg.MaxCount = 3;
+            trg.Is.Creature().On.YourGraveyard();
+          });
+
+          p.TargetingRule(new EffectOrCostRankBy(c => -c.Score));
+          p.TimingRule(new WhenYourGraveyardCountIs(c => c.Is().Creature));
+        })
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{3}{B}{B}, Exile Soul of Innistrad from your graveyard: Return up to three target creature cards from your graveyard to your hand.";
+          p.Cost = new AggregateCost(
+            new PayMana("{3}{B}{B}".Parse(), ManaUsage.Abilities),
+            new Exile(fromGraveyard: true));
+
+          p.Effect = () => new Effects.ReturnToHand();
+          p.TargetSelector.AddEffect(trg =>
+          {
+            trg.MinCount = 0;
+            trg.MaxCount = 3;
+            trg.Is.Creature().On.YourGraveyard();
+          });
+
+          p.TargetingRule(new EffectOrCostRankBy(c => -c.Score));
+          p.TimingRule(new WhenYourGraveyardCountIs(c => c.Is().Creature));
+        });
+    }
+  }
+}
