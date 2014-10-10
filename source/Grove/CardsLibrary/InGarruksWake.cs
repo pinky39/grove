565663a72de284c1d0a55432e039006eb0f70eea
Sandmann123 <norfirill@gmail.com>
2014-10-10 11:11:27 +0800

    Added new card: In Garruk's Wake

---
+++ source/Grove/CardsLibrary/InGarruksWake.cs
@@ -0,0 +1,23 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+
+  public class InGarruksWake : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("In Garruk's Wake")
+        .ManaCost("{7}{B}{B}")
+        .Type("Sorcery")
+        .Text("Destroy all creatures you don't control and all planeswalkers you don't control.")
+        .FlavorText("Beyond pain, beyond obsession and wild despair, there lies a place of twisted power only the most tormented souls can reach.")
+        .Cast(p =>
+        {
+          p.Effect = () => new DestroyAllPermanents(
+            filter: (effect, card) => (card.Is().Creature || card.Is("Planeswalker")) && card.Controller != effect.Controller);
+        });
+    }
+  }
+}
