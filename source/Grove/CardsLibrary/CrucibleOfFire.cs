d9116a57d3d6cbce8acba52a1f83a8260ae72c2b
Sandmann123 <norfirill@gmail.com>
2014-10-11 00:32:43 +0800

    Added new card: Crucible of Fire

---
+++ source/Grove/CardsLibrary/CrucibleOfFire.cs
@@ -0,0 +1,23 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Modifiers;
+
+  public class CrucibleOfFire : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Crucible of Fire")
+        .ManaCost("{3}{R}")
+        .Type("Enchantment")
+        .Text("Dragon creatures you control get +3/+3.")
+        .FlavorText("\"The dragon is a perfect marriage of power and the will to use it.\"—Sarkhan Vol")
+        .ContinuousEffect(p =>
+        {
+          p.CardFilter = (card, effect) => card.Is("Dragon") && card.Controller == effect.Source.Controller;
+          p.Modifier = () => new AddPowerAndToughness(3, 3);
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/CrucibleOfFire.cs
@@ -0,0 +1,21 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class CrucibleOfFire
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void DragonHasIncreasedStrenght()
+      {
+        Battlefield(P1, "Crucible Of Fire", "Shivan Dragon");
+
+        RunGame(1);
+
+        Equal(12, P2.Life);
+      }
+    }
+  }
+}
