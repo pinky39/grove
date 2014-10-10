97067b69900ba6c24e0c7b57158eb95079cb58ea
Sandmann123 <norfirill@gmail.com>
2014-10-11 00:19:34 +0800

    Added new card: Circle of Flame

---
+++ source/Grove/CardsLibrary/CircleOfFlame.cs
@@ -0,0 +1,31 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+  using Events;
+  using Triggers;
+
+  public class CircleOfFlame : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Circle of Flame")
+        .ManaCost("{1}{R}")
+        .Type("Enchantment")
+        .Text("Whenever a creature without flying attacks you or a planeswalker you control, Circle of Flame deals 1 damage to that creature.")
+        .FlavorText("\"Which do you think is a better deterrent: a moat of water or one of fire?\"—Chandra Nalaar")
+        .TriggeredAbility(p =>
+        {
+          p.Text = "Whenever a creature without flying attacks you or a planeswalker you control, Circle of Flame deals 1 damage to that creature.";
+
+          p.Trigger(new OnAttack(triggerForCreature: 
+            (card, trigger) => !card.Has().Flying && card.Controller != trigger.Controller));
+
+          p.Effect = () => new DealDamageToCreature(amount: 1, creature: P(e => e.TriggerMessage<AttackerJoinedCombatEvent>().Attacker.Card));
+
+          p.TriggerOnlyIfOwningCardIsInPlay = true;
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/CircleOfFlame.cs
@@ -0,0 +1,37 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class CircleOfFlame
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void DealDamagesToAttackerWithoutFlying()
+      {
+        Battlefield(P1, "Fugitive Wizard");
+
+        P2.Life = 1;
+        Battlefield(P2, "Circle Of Flame");
+
+        RunGame(1);
+
+        Equal(1, P2.Life);
+      }
+
+      [Fact]
+      public void DoNotDealDamagesToAttackerWithFlying()
+      {
+        Battlefield(P1, "Angelic Page");
+
+        P2.Life = 1;
+        Battlefield(P2, "Circle Of Flame");
+
+        RunGame(1);
+
+        Equal(0, P2.Life);
+      }
+    }
+  }
+}
