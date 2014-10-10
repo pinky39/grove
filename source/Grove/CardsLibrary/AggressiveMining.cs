dc721378080464a0c97646850f68ff2185be8409
Sandmann123 <norfirill@gmail.com>
2014-10-10 21:18:02 +0800

    Added new card: Aggressive Mining

---
+++ source/Grove/CardsLibrary/AggressiveMining.cs
@@ -0,0 +1,35 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using Costs;
+  using Effects;
+  using Modifiers;
+
+  public class AggressiveMining : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Aggressive Mining")
+        .ManaCost("{3}{R}")
+        .Type("Enchantment")
+        .Text("You can't play lands.{EOL}Sacrifice a land: Draw two cards. Activate this ability only once each turn.")
+        .StaticAbility(p => p.Modifier(() => new IncreaseLandLimit(amount: -1000))) // Just to avoid effects of spells increase land limit
+        .ActivatedAbility(p =>
+        {
+          p.Text = "Sacrifice a land: Draw two cards. Activate this ability only once each turn.";
+
+          p.Cost = new Sacrifice();
+
+          p.TargetSelector.AddCost(trg => trg.Is.Card(c => c.Is().Land).On.Battlefield());
+
+          p.Effect = () => new DrawCards(2);
+
+          p.TargetingRule(new CostSacrificeToDrawCards());
+
+          p.ActivateOnlyOnceEachTurn = true;
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/AggressiveMining.cs
@@ -0,0 +1,22 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class AggressiveMining
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void YouCannotPlayLands()
+      {
+        Battlefield(P1, "Aggressive Mining");
+        Hand(P1, "Grizzly Bears", "Forest");
+
+        RunGame(1);
+
+        Equal(2, P1.Hand.Count);
+      }
+    }
+  }
+}
