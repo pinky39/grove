362beafa8f35c846091f90f018921fac3ba35a6f
Sandmann123 <norfirill@gmail.com>
2014-10-11 22:49:38 +0800

    Added new card: Goblin Kaboomist
    * added new effect - FlipCoinDealDamageToItself

---
+++ source/Grove/CardsLibrary/GoblinKaboomist.cs
@@ -0,0 +1,49 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Costs;
+  using Effects;
+  using Triggers;
+
+  public class GoblinKaboomist : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+          .Named("Goblin Kaboomist")
+          .ManaCost("{1}{R}")
+          .Type("Creature — Goblin Warrior")
+          .Text("At the beginning of your upkeep, put a colorless artifact token named Land Mine onto the battlefield with \"{R}, Sacrifice this artifact: This artifact deals 2 damage to target attacking creature without flying.\" Then flip a coin. If you lose the flip, Goblin Kaboomist deals 2 damage to itself.")
+          .Power(1)
+          .Toughness(2)
+          .TriggeredAbility(p =>
+          {
+            p.Trigger(new OnStepStart(Step.Upkeep));
+            p.Effect = () => new CompoundEffect(
+              new CreateTokens(
+                count: 1,
+                token: Card
+                  .Named("Land Mine")
+                  .Type("Token Artifact")
+                  .Text("{R}, Sacrifice this artifact: This artifact deals 2 damage to target attacking creature without flying.")
+                  .Colors(CardColor.Colorless)
+                  .ActivatedAbility(ap =>
+                  {
+                    ap.Text = "{R}, Sacrifice this artifact: This artifact deals 2 damage to target attacking creature without flying.";
+                    ap.Cost = new AggregateCost(
+                      new PayMana(Mana.Red, ManaUsage.Abilities),
+                      new Sacrifice());
+                    ap.Effect = () => new DealDamageToTargets(2);
+                    ap.TargetSelector.AddEffect(trg => trg.Is.Card(c => c.IsAttacker && !c.Has().Flying));
+                    ap.TargetingRule(new EffectDealDamage(2));
+                    ap.TimingRule(new AfterOpponentDeclaresAttackers());
+                  })),
+              new FlipCoinDealDamageToItself(2));
+
+            p.TriggerOnlyIfOwningCardIsInPlay = true;
+          });
+    }
+  }
+}
---
+++ source/Grove/Core/Effects/FlipCoinDealDamageToItself.cs
@@ -0,0 +1,27 @@
+﻿namespace Grove.Effects
+{
+  public class FlipCoinDealDamageToItself : Effect
+  {
+    private readonly int _amount;
+
+    private FlipCoinDealDamageToItself() {}
+
+    public FlipCoinDealDamageToItself(int amount)
+    {
+      _amount = amount;
+    }
+
+    protected override void ResolveEffect()
+    {
+      var hasWon = FlipACoin(Controller);
+
+      if (hasWon)
+        return;
+
+      Source.OwningCard.DealDamageTo(
+          _amount,
+          Source.OwningCard,
+          isCombat: false);
+    }
+  }
+}
