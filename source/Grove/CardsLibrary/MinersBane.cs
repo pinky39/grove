e3133ad40ef295fac6e8407a981e1ce385b0c43a
Sandmann123 <norfirill@gmail.com>
2014-10-11 23:57:34 +0800

    Added new card: Miner's Bane

---
+++ source/Grove/CardsLibrary/MinersBane.cs
@@ -0,0 +1,34 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.RepetitionRules;
+  using AI.TimingRules;
+  using Costs;
+  using Effects;
+  using Modifiers;
+
+  public class MinersBane : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+          .Named("Miner's Bane")
+          .ManaCost("{4}{R}{R}")
+          .Type("Creature — Elemental")
+          .Text("{2}{R}: Miner's Bane gets +1/+0 and gains trample until end of turn. {I}(If it would assign enough damage to its blockers to destroy them, you may have it assign the rest of its damage to defending player or planeswalker.){/I}")
+          .Power(6)
+          .Toughness(3)
+          .ActivatedAbility(p =>
+          {
+            p.Text = "{2}{R}: Miner's Bane gets +1/+0 and gains trample until end of turn.";
+            p.Cost = new PayMana("{2}{R}".Parse(), ManaUsage.Abilities, supportsRepetitions: true);
+            p.Effect = () => new ApplyModifiersToSelf(
+              () => new AddPowerAndToughness(1, 0){UntilEot = true},
+              () => new AddStaticAbility(Static.Trample){UntilEot = true});
+
+            p.TimingRule(new PumpOwningCardTimingRule(1, 0));
+            p.RepetitionRule(new RepeatMaxTimes());
+          });
+    }
+  }
+}
