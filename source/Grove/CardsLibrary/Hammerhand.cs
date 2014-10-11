e3b63f5852ce5d838ed253704f33dc596e445229
Sandmann123 <norfirill@gmail.com>
2014-10-11 23:18:43 +0800

    Added new card: Hammerhand

---
+++ source/Grove/CardsLibrary/Hammerhand.cs
@@ -0,0 +1,44 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+  using Modifiers;
+  using Triggers;
+
+  public class Hammerhand : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Hammerhand")
+        .ManaCost("{R}")
+        .Type("Enchantment — Aura")
+        .Text("Enchant creature{EOL}When Hammerhand enters the battlefield, target creature can't block this turn.{EOL}Enchanted creature gets +1/+1 and has haste. {I}(It can attack and no matter when it came under your control.){/I}")
+        .Cast(p =>
+        {
+          p.Effect = () => new Attach(
+            () => new AddPowerAndToughness(1, 1),
+            () => new AddStaticAbility(Static.Haste)).SetTags(EffectTag.IncreasePower, EffectTag.IncreaseToughness);
+
+          p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
+
+          p.TimingRule(new OnFirstMain());
+          p.TargetingRule(new EffectCombatEnchantment());
+        })
+        .TriggeredAbility(p =>
+        {
+          p.Text = "When Hammerhand enters the battlefield, target creature can't block this turn.";
+
+          p.Trigger(new OnZoneChanged(to: Zone.Battlefield));
+
+          p.Effect = () => new ApplyModifiersToTargets(
+            () => new AddStaticAbility(Static.CannotBlock){UntilEot = true});
+          p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
+          p.TargetingRule(new EffectCannotBlockAttack(blockOnly: true));
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/Hammerhand.cs
@@ -0,0 +1,25 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class Hammerhand
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void EnchantBearTapTarget()
+      {
+        Hand(P1, "Hammerhand");
+        Battlefield(P1, "Grizzly Bears", "Mountain");
+
+        P2.Life = 3;
+        Battlefield(P2, "Grizzly Bears");
+
+        RunGame(1);
+
+        Equal(0, P2.Life);
+      }
+    }
+  }
+}
