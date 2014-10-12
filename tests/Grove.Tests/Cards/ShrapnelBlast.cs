8c0ab51797a0e837b1b9c7cc0eb65f810f4dfb9f
pinky <pinky@lab.org>
2014-10-12 22:31:56 +0200

    Improved Shrapnel Blast.
    * Removed timing rule, which was not needed since spell can never be cast
      if player controls no artifacts.
    * Added target removal timing rule (like the one on Lightning Bolt).

--- tests/Grove.Tests/Cards/ShrapnelBlast.cs
+++ tests/Grove.Tests/Cards/ShrapnelBlast.cs
@@ -1,24 +1,39 @@
 ﻿namespace Grove.Tests.Cards
 {
-    using Infrastructure;
-    using Xunit;
+  using Infrastructure;
+  using Xunit;
 
-    public class ShrapnelBlast
+  public class ShrapnelBlast
+  {
+    public class Ai : AiScenario
     {
-        public class Ai : AiScenario
-        {
-            [Fact]
-            public void CastShrapnelBlast()
-            {
-                Hand(P1, "Shrapnel Blast");
-                Battlefield(P1, "Profane Memento", "Mountain", "Mountain", "Mountain", "Mountain");
+      [Fact]
+      public void Deal5DamageToPlayer()
+      {
+        Hand(P1, "Shrapnel Blast");
+        Battlefield(P1, "Profane Memento", "Mountain", "Mountain", "Mountain", "Mountain");
 
-                P2.Life = 5;
+        P2.Life = 5;
 
-                RunGame(1);
+        RunGame(2);
 
-                Equal(0, P2.Life);
-            }
-        }
+        Equal(0, P2.Life);
+      }
+
+      [Fact]
+      public void Deal5DamageToDragon()
+      {
+        Hand(P1, "Shrapnel Blast");
+        Battlefield(P1, "Profane Memento", "Trained Armodon", "Trained Armodon", "Mountain", 
+          "Mountain", "Mountain", "Mountain");
+        
+        Battlefield(P2, "Shivan Dragon");
+        P2.Life = 6;
+
+        RunGame(1);
+
+        Equal(0, P2.Life);
+      }
     }
+  }
 }
