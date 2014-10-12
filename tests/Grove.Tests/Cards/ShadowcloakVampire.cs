b0eb18852a4ca8e93aa26df97d720417682a0bdc
pinky <pinky@lab.org>
2014-10-12 22:21:04 +0200

    Improved Shadowcloak Vampire.
    * Consider ability also on non-active turn.
    * Don't consider ability if tapped.

--- tests/Grove.Tests/Cards/ShadowcloakVampire.cs
+++ tests/Grove.Tests/Cards/ShadowcloakVampire.cs
@@ -1,27 +1,26 @@
 ﻿namespace Grove.Tests.Cards
 {
-    using System.Linq;
-    using Infrastructure;
-    using Xunit;
+  using Infrastructure;
+  using Xunit;
 
-    public class ShadowcloakVampire
+  public class ShadowcloakVampire
+  {
+    public class Ai : AiScenario
     {
-        public class Ai : AiScenario
-        {
-            [Fact]
-            public void PayLife()
-            {
-                Battlefield(P1, "Shadowcloak Vampire");
-                P1.Life = 10;
+      [Fact]
+      public void GiveFlyingAndAttackForWin()
+      {
+        Battlefield(P1, "Shadowcloak Vampire");
+        P1.Life = 10;
 
-                Battlefield(P2, "Grizzly Bears");
-                P2.Life = 4;
+        Battlefield(P2, "Grizzly Bears");
+        P2.Life = 4;
 
-                RunGame(1);
+        RunGame(1);
 
-                Equal(0, P2.Life);
-                Equal(8, P1.Life);                
-            }
-        }
+        Equal(0, P2.Life);
+        Equal(8, P1.Life);
+      }
     }
+  }
 }
