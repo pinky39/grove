d3b3cb188caffb5cf5066d78febda2db9e5144d2
Sandmann123 <norfirill@gmail.com>
2014-10-13 22:46:08 +0800

    Added new card: Verdant Haven
    * changed tabs

--- source/Grove/CardsLibrary/StabWound.cs
+++ source/Grove/CardsLibrary/StabWound.cs
@@ -1,48 +1,44 @@
 ﻿namespace Grove.CardsLibrary
 {
-    using System.Collections.Generic;
-    using AI;
-    using AI.TargetingRules;
-    using AI.TimingRules;
-    using Effects;
-    using Modifiers;
-    using Triggers;
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+  using Modifiers;
+  using Triggers;
 
-    public class StabWound : CardTemplateSource
+  public class StabWound : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
     {
-        public override IEnumerable<CardTemplate> GetCards()
-        {
-            yield return Card
-                .Named("Stab Wound")
-                .ManaCost("{2}{B}")
-                .Type("Enchantment — Aura")
-                .Text("Enchant creature{EOL}Enchanted creature gets -2/-2.{EOL}At the beginning of the upkeep of enchanted creature's controller, that player loses 2 life.")
-                .Cast(p =>
-                {
-                    p.Effect = () => new Attach(() => new AddPowerAndToughness(-2, -2));
+      yield return Card
+          .Named("Stab Wound")
+          .ManaCost("{2}{B}")
+          .Type("Enchantment — Aura")
+          .Text("Enchant creature{EOL}Enchanted creature gets -2/-2.{EOL}At the beginning of the upkeep of enchanted creature's controller, that player loses 2 life.")
+          .Cast(p =>
+          {
+            p.Effect = () => new Attach(() => new AddPowerAndToughness(-2, -2));
 
-                    p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
-                    
-                    p.TimingRule(new OnFirstMain());
+            p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
 
-                    p.TargetingRule(new EffectOrCostRankBy(c => -c.Score, ControlledBy.Opponent));
-                })
-                .TriggeredAbility(p =>
-                {
-                    p.Text = "At the beginning of the upkeep of enchanted creature's controller, that player loses 2 life.";
-                    
-                    p.Trigger(new OnStepStart(Step.Upkeep, activeTurn: true, passiveTurn: true)
-                    {
-                        Condition = (t, g) =>
-                        {
-                            return t.OwningCard.IsAttached && t.OwningCard.AttachedTo.Controller.IsActive;
-                        }
-                    });
+            p.TimingRule(new OnFirstMain());
 
-                    p.Effect = () => new ChangeLife(-2, forYou: true, useAttachedToAsYouSource: true);
+            p.TargetingRule(new EffectOrCostRankBy(c => -c.Score, ControlledBy.Opponent));
+          })
+          .TriggeredAbility(p =>
+          {
+            p.Text = "At the beginning of the upkeep of enchanted creature's controller, that player loses 2 life.";
 
-                    p.TriggerOnlyIfOwningCardIsInPlay = true;
-                });
-        }
+            p.Trigger(new OnStepStart(Step.Upkeep, activeTurn: true, passiveTurn: true)
+            {
+              Condition = (t, g) => t.OwningCard.IsAttached && t.OwningCard.AttachedTo.Controller.IsActive
+            });
+
+            p.Effect = () => new ChangeLife(-2, forYou: true, useAttachedToAsYouSource: true);
+
+            p.TriggerOnlyIfOwningCardIsInPlay = true;
+          });
     }
+  }
 }
---
+++ source/Grove/CardsLibrary/VerdantHaven.cs
@@ -0,0 +1,37 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+  using Modifiers;
+  using Triggers;
+
+  public class VerdantHaven : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Verdant Haven")
+        .ManaCost("{2}{G}")
+        .Type("Enchantment — Aura")
+        .Text("Enchant land{EOL}When Verdant Haven enters the battlefield, you gain 2 life.{EOL}Whenever enchanted land is tapped for mana, its controller adds one mana of any color to his or her mana pool {I}(in addition to the mana the land produces).{/I}")
+        .Cast(p =>
+        {
+          p.Effect = () => new Attach(() => new IncreaseManaOutput(Mana.Any));
+          p.TargetSelector.AddEffect(trg => trg.Is.Card(c => c.Is().Land).On.Battlefield());
+
+          p.TimingRule(new OnFirstMain());
+          p.TargetingRule(new EffectLandEnchantment(ControlledBy.SpellOwner));
+        })
+        .TriggeredAbility(p =>
+        {
+          p.Text = "When Verdant Haven enters the battlefield, you gain 2 life.";
+
+          p.Trigger(new OnZoneChanged(to: Zone.Battlefield));
+
+          p.Effect = () => new ChangeLife(2, forYou: true);
+        });
+    }
+  }
+}
