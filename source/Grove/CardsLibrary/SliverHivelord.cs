ca293d775e5256e4609362e9b2ac60ddbd214371
Sandmann123 <norfirill@gmail.com>
2014-10-13 23:27:04 +0800

    Added new card: Sliver Hivelord

---
+++ source/Grove/CardsLibrary/SliverHivelord.cs
@@ -0,0 +1,25 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Modifiers;
+
+  public class SliverHivelord : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Sliver Hivelord")
+        .ManaCost("{W}{U}{B}{R}{G}")
+        .Type("Legendary Creature — Sliver")
+        .Text("Sliver creatures you control have indestructible. {I}(Damage and effects that say \"destroy\" don't destroy them.){/I}")
+        .FlavorText("\"This is the source, the line unbroken since the calamity that brought such monsters to our shores.\"{EOL}—Hastric, Thunian scout")
+        .Power(5)
+        .Toughness(5)
+        .ContinuousEffect(p =>
+        {
+          p.CardFilter = (card, effect) => card.Is("Sliver") && card.Controller == effect.Source.Controller;
+          p.Modifier = () => new AddStaticAbility(Static.Indestructible);
+        });
+    }
+  }
+}
