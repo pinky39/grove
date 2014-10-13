dc316a0f7fa5667fbb17070e05d556a2d21a29ca
Sandmann123 <norfirill@gmail.com>
2014-10-13 22:37:10 +0800

    Added new card: Venom Sliver

---
+++ source/Grove/CardsLibrary/VenomSliver.cs
@@ -0,0 +1,25 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Modifiers;
+
+  public class VenomSliver : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Venom Sliver")
+        .ManaCost("{1}{G}")
+        .Type("Creature — Sliver")
+        .Text("Sliver creatures you control have deathtouch.{I}(Any amount of damage a creature with deathtouch deals to a creature is enough to destroy it.){/I}")
+        .FlavorText("\"We attacked with arrows dipped in poison. The slivers that did not die began to change.\"{EOL}—Hastric, Thunian scout")
+        .Power(1)
+        .Toughness(1)
+        .ContinuousEffect(p =>
+        {
+          p.CardFilter = (card, effect) => card.Is("Sliver") && card.Controller == effect.Source.Controller;
+          p.Modifier = () => new AddStaticAbility(Static.Deathtouch);
+        });
+    }
+  }
+}
