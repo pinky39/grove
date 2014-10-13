deea7385cbfb73a7fd58a078b1a95399cac94d56
Sandmann123 <norfirill@gmail.com>
2014-10-13 11:31:28 +0800

    Added new card: Elvish Mystic

---
+++ source/Grove/CardsLibrary/ElvishMystic.cs
@@ -0,0 +1,24 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class ElvishMystic : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Elvish Mystic")
+        .ManaCost("{G}")
+        .Type("Creature - Elf Druid")
+        .Text("{T}: Add {G} to your mana pool. ")
+        .FlavorText("\"Life grows everywhere. My kin merely find those places where it grows strongest.\"{EOL}—Nissa Revane")
+        .Power(1)
+        .Toughness(1)
+        .ManaAbility(p =>
+        {
+          p.Text = "{T}: Add {G} to your mana pool.";
+          p.ManaAmount(Mana.Green);
+        });
+    }
+  }
+}
