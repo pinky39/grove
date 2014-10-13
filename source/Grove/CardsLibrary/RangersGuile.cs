7b785f6ba58f2e4add61619c1b144d7bd7dd4f7d
Sandmann123 <norfirill@gmail.com>
2014-10-13 20:52:58 +0800

    Added new card: Ranger's Guile

---
+++ source/Grove/CardsLibrary/RangersGuile.cs
@@ -0,0 +1,32 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI;
+  using AI.TargetingRules;
+  using Effects;
+  using Modifiers;
+
+  public class RangersGuile : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Ranger's Guile")
+        .ManaCost("{G}")
+        .Type("Instant")
+        .Text("Target creature you control gets +1/+1 and gains hexproof until end of turn. {I}(It can't be the target of spells or abilities your opponents control.){/I}")
+        .FlavorText("\"You don't survive in the wild by standing in plain sight.\"{EOL}—Garruk Wildspeaker")
+        .Cast(p =>
+        {
+          p.Effect = () => new ApplyModifiersToTargets(
+            () => new AddPowerAndToughness(1, 1){UntilEot = true},
+            () => new AddStaticAbility(Static.Hexproof) { UntilEot = true }).SetTags(
+              EffectTag.IncreasePower, EffectTag.IncreaseToughness);
+
+          p.TargetSelector.AddEffect(trg => trg.Is.Creature(controlledBy: ControlledBy.SpellOwner).On.Battlefield());
+
+          p.TargetingRule(new EffectPumpInstant(1, 1));
+        });
+    }
+  }
+}
