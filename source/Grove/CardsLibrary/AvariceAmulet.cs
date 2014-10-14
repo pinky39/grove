c12ecb29e1ef032b5bf4109bb4fbb501e27a4238
Sandmann123 <norfirill@gmail.com>
2014-10-14 10:14:36 +0800

    Added new card: Avarice Amulet

---
+++ source/Grove/CardsLibrary/AvariceAmulet.cs
@@ -0,0 +1,62 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Costs;
+  using Effects;
+  using Modifiers;
+  using Triggers;
+
+  public class AvariceAmulet : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Avarice Amulet")
+        .ManaCost("{4}")
+        .Type("Artifact — Equipment")
+        .Text("Equipped creature gets +2/+0 and has vigilance and \"At the beginning of your upkeep, draw a card.\"{EOL}When equipped creature dies, target opponent gains control of Avarice Amulet.{EOL}Equip {2} ({2}: Attach to target creature you control. Equip only as a sorcery.)")
+        .FlavorText("")
+        .ActivatedAbility(p =>
+        {
+          p.Text = "Equip {2} ({2}: Attach to target creature you control. Equip only as a sorcery.)";
+
+          p.Cost = new PayMana(2.Colorless(), ManaUsage.Abilities);
+
+          p.Effect = () => new Attach(
+            () => new AddPowerAndToughness(2, 0),
+            () => new AddStaticAbility(Static.Vigilance),
+            () =>
+            {
+              var tp = new TriggeredAbility.Parameters();
+
+              tp.Text = "At the beginning of your upkeep, draw a card.";
+              tp.Trigger(new OnStepStart(Step.Upkeep));
+              tp.Effect = () => new DrawCards(1);
+
+              return new AddTriggeredAbility(new TriggeredAbility(tp));
+            });
+
+          p.TargetSelector.AddEffect(trg => trg.Is.ValidEquipmentTarget().On.Battlefield());
+
+          p.TargetingRule(new EffectCombatEquipment());
+          p.TimingRule(new OnFirstMain());
+
+          p.IsEquip = true;
+          p.ActivateAsSorcery = true;
+        })
+        .TriggeredAbility(p =>
+        {
+          p.Text = "When equipped creature dies, target opponent gains control of Avarice Amulet.";
+
+          p.Trigger(new OnZoneChanged(from: Zone.Battlefield, to: Zone.Graveyard,
+            filter: (card, ability, _) => card == ability.OwningCard.AttachedTo));
+
+          p.Effect = () => new ApplyModifiersToSelf(() => new ChangeController(modifier => modifier.OwningCard.Controller.Opponent));
+
+          p.TriggerOnlyIfOwningCardIsInPlay = true;
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/AvariceAmulet.cs
@@ -0,0 +1,28 @@
+﻿namespace Grove.Tests.Cards
+{
+  using System.Linq;
+  using Infrastructure;
+  using Xunit;
+
+  public class AvariceAmulet
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void GetControlOnAmulet()
+      {
+        Battlefield(P1, "Grizzly Bears", "Grizzly Bears");
+
+        P2.Life = 4;
+        Battlefield(P2, C("Grizzly Bears").IsEquipedWith("Avarice Amulet"));
+
+        RunGame(1);
+
+        Equal(2, P2.Life);
+        Equal(0, P2.Battlefield.Count);
+        Equal(2, P1.Battlefield.Count);
+        Equal(1, P1.Battlefield.Creatures.Count());
+      }
+    }
+  }
+}
