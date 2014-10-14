ddbb7022ceac7db3275d3a834c16f5cc3342b544
Sandmann123 <norfirill@gmail.com>
2014-10-14 23:13:02 +0800

    Added new card: Cancel

---
+++ source/Grove/CardsLibrary/Cancel.cs
@@ -0,0 +1,28 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+
+  public class Cancel : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Cancel")
+        .ManaCost("{1}{U}{U}")
+        .Type("Instant")
+        .Text("Counter target spell.")
+        .FlavorText("\"Even the greatest inferno begins as a spark. And anyone can snuff out a spark.\"{EOL}—Chanyi, mistfire sage")
+        .Cast(p =>
+        {
+          p.Effect = () => new CounterTargetSpell();
+          p.TargetSelector.AddEffect(trg => trg.Is.CounterableSpell().On.Stack());
+
+          p.TargetingRule(new EffectCounterspell());
+          p.TimingRule(new WhenTopSpellIsCounterable());
+        });
+    }
+  }
+}
