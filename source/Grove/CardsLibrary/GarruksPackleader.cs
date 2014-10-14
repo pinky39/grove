8ad9bf394037788664fade3f1ae3e76187cdc902
Sandmann123 <norfirill@gmail.com>
2014-10-14 23:42:30 +0800

    Added new card: Garruk's Packleader

---
+++ source/Grove/CardsLibrary/GarruksPackleader.cs
@@ -0,0 +1,33 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+  using Triggers;
+
+  public class GarruksPackleader : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Garruk's Packleader")
+        .ManaCost("{4}{G}")
+        .Type("Creature - Beast")
+        .Text("Whenever another creature with power 3 or greater enters the battlefield under your control, you may draw a card.")
+        .FlavorText("\"He has learned much in his long years. And unlike selfish humans, he's willing to share.\"{EOL}—Garruk Wildspeaker")
+        .Power(4)
+        .Toughness(4)
+        .TriggeredAbility(p =>
+        {
+          p.Text = "Whenever another creature with power 3 or greater enters the battlefield under your control, you may draw a card.";
+
+          p.Trigger(new OnZoneChanged(
+            to: Zone.Battlefield,
+            filter: (card, ability, _) => card.Is().Creature && ability.OwningCard.Controller == card.Controller));
+          
+          p.TriggerOnlyIfOwningCardIsInPlay = true;
+          
+          p.Effect = () => new DrawCards(1);
+        });
+    }
+  }
+}
