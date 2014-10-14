f1201e873c2466a668217aea7fe57627f69f0382
Sandmann123 <norfirill@gmail.com>
2014-10-14 17:00:06 +0800

    Added new card: Battlefield Forge

---
+++ source/Grove/CardsLibrary/BattlefieldForge.cs
@@ -0,0 +1,33 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TimingRules;
+  using Costs;
+  using Effects;
+
+  public class BattlefieldForge : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Battlefield Forge")
+        .Type("Land")
+        .Text("{T}: Add {1} to your mana pool.{EOL}{T}: Add {R} or {W} to your mana pool. Battlefield Forge deals 1 damage to you.")
+        .ManaAbility(p =>
+        {
+          p.Text = "{T}: Add {1} to your mana pool.";
+          p.ManaAmount(1.Colorless());
+        })
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{T}: Add {R} or {W} to your mana pool. Battlefield Forge deals 1 damage to you.";
+          p.Cost = new Tap();
+          p.Effect = () => new CompoundEffect(
+            new AddManaToPool(Mana.Colored(isRed: true, isWhite: true)),
+            new DealDamageToPlayer(1, P(e => e.Controller)));            
+          p.TimingRule(new WhenYouNeedAdditionalMana(1));
+          p.UsesStack = false;
+        });
+    }
+  }
+}
