ab93bf848bb4a8b5cb766d4256b7c83741481148
Sandmann123 <norfirill@gmail.com>
2014-10-14 23:10:27 +0800

    Added new card: Serra Angel

---
+++ source/Grove/CardsLibrary/SerraAngel.cs
@@ -0,0 +1,20 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class SerraAngel : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Serra Angel")
+        .ManaCost("{3}{W}{W}")
+        .Type("Creature - Angel")
+        .Text("{Flying}{EOL}{Vigilance} {I}(Attacking doesn't cause this creature to tap.){/I}")
+        .FlavorText("Follow the light. In its absence, follow her.")
+        .Power(4)
+        .Toughness(4)
+        .SimpleAbilities(Static.Flying, Static.Vigilance);
+    }
+  }
+}
