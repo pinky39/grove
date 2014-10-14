d2c127a985df616b986f63ad1b09940fa9d5ed72
Sandmann123 <norfirill@gmail.com>
2014-10-14 20:56:36 +0800

    Added new card: Evolving Wilds

---
+++ source/Grove/CardsLibrary/EvolvingWilds.cs
@@ -0,0 +1,37 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TimingRules;
+  using Costs;
+  using Effects;
+
+  public class EvolvingWilds : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Evolving Wilds")
+        .Type("Land")
+        .Text("{T}, Sacrifice Evolving Wilds: Search your library for a basic land card and put it onto the battlefield tapped. Then shuffle your library.")
+        .FlavorText("Without the interfering hands of civilization, nature will always shape itself to its own needs.")
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{T}, Sacrifice Evolving Wilds: Search your library for a basic land card and put it onto the battlefield tapped. Then shuffle your library.";
+
+          p.Cost = new AggregateCost(
+            new Tap(),
+            new Sacrifice());
+
+          p.Effect = () => new SearchLibraryPutToZone(
+              zone: Zone.Battlefield,
+              afterPutToZone: (c, g) => c.Tap(),
+              minCount: 0,
+              maxCount: 1,
+              validator: (e, c) => c.Is().BasicLand,
+              text: "Search your library for a basic land card.");
+
+          p.TimingRule(new OnFirstMain());
+        });
+    }
+  }
+}
