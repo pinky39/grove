2a96110f95889ce9871b1a5366a8ef0589e0eeb6
Sandmann123 <norfirill@gmail.com>
2014-10-14 11:06:40 +0800

    Added new card: Grindclock
    * added targeting and dynamic count to PlayerPutsTopCardsFromLibraryToGraveyard

---
+++ source/Grove/CardsLibrary/Grindclock.cs
@@ -0,0 +1,43 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Costs;
+  using Effects;
+  using Modifiers;
+
+  public class Grindclock : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Grindclock")
+        .ManaCost("{2}")
+        .Type("Artifact")
+        .Text("{T}: Put a charge counter on Grindclock.{EOL}{T}: Target player puts the top X cards of his or her library into his or her graveyard, where X is the number of charge counters on Grindclock.")
+        .FlavorText("Pray you never hear it chime.")
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{T}: Put a charge counter on Grindclock.";
+          p.Cost = new Tap();
+          p.Effect = () => new ApplyModifiersToSelf(
+            () => new AddCounters(() => new SimpleCounter(CounterType.Charge), 1));
+          p.TimingRule(new OnEndOfOpponentsTurn());
+        })
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{T}: Target player puts the top X cards of his or her library into his or her graveyard, where X is the number of charge counters on Grindclock.";
+          p.Cost = new Tap();
+          p.Effect = () => new PlayerPutsTopCardsFromLibraryToGraveyard(
+            player: null,
+            count: P(e => e.Source.OwningCard.CountersCount(CounterType.Charge)));
+
+          p.TargetSelector.AddEffect(trg => trg.Is.Player());
+
+          p.TargetingRule(new EffectOpponent());
+          p.TimingRule(new WhenCardHasCounters(3));
+        });
+    }
+  }
+}
--- source/Grove/Core/Effects/PlayerPutsTopCardsFromLibraryToGraveyard.cs
+++ source/Grove/Core/Effects/PlayerPutsTopCardsFromLibraryToGraveyard.cs
@@ -2,22 +2,26 @@
 {
   public class PlayerPutsTopCardsFromLibraryToGraveyard : Effect
   {
-    private readonly int _count;
+    private readonly DynParam<int> _count;
     private readonly DynParam<Player> _player;
 
     private PlayerPutsTopCardsFromLibraryToGraveyard() {}
 
-    public PlayerPutsTopCardsFromLibraryToGraveyard(DynParam<Player> player, int count)
+    public PlayerPutsTopCardsFromLibraryToGraveyard(DynParam<Player> player, DynParam<int> count)
     {
       _count = count;
       _player = player;
 
-      RegisterDynamicParameters(player);
+      RegisterDynamicParameters(player, count);
     }
 
     protected override void ResolveEffect()
     {
-      _player.Value.Mill(_count);
+      var player = Target != null && Target.IsPlayer() 
+        ? Target.Player() 
+        : _player.Value;
+      
+      player.Mill(_count.Value);
     }
   }
 }
