c72c7a9d24d2cd1e502f9ad7a6f892a7b8e48850
Sandmann123 <norfirill@gmail.com>
2014-10-14 23:37:22 +0800

    Added new card: Centaur Courser

---
+++ source/Grove/CardsLibrary/CentaurCourser.cs
@@ -0,0 +1,18 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class CentaurCourser : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Centaur Courser")
+        .ManaCost("{2}{G}")
+        .Type("Creature - Centaur Warrior")
+        .FlavorText("\"The centaurs are truly free. Never will they be tamed by temptation or controlled by fear. They live in total harmony, a feat not yet achieved by our kind.\"{EOL}—Ramal, sage of Westgate")
+        .Power(3)
+        .Toughness(3);
+    }
+  }
+}
