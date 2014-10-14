2be32de6410824bd4d53209b3e615db7d879cc5f
pinky <pinky@lab.org>
2014-10-14 22:04:19 +0200

    Fixed Restock.
    * Fixed target selector.
    * Fixed targeting rule.
    * Added a test.

--- source/Grove/CardsLibrary/Restock.cs
+++ source/Grove/CardsLibrary/Restock.cs
@@ -1,37 +1,33 @@
 ﻿namespace Grove.CardsLibrary
 {
-    using System.Collections.Generic;
-    using AI;
-    using AI.TargetingRules;
-    using AI.TimingRules;
-    using Effects;
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using Effects;
 
-    public class Restock : CardTemplateSource
+  public class Restock : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
     {
-        public override IEnumerable<CardTemplate> GetCards()
-        {
-            yield return Card
-              .Named("Restock")
-              .ManaCost("{3}{G}{G}")
-              .Type("Sorcery")
-              .Text("Return two target cards from your graveyard to your hand. Exile Restock.")
-              .FlavorText("What looked like a retreat was actually a replenishing.")
-              .Cast(p =>
+      yield return Card
+        .Named("Restock")
+        .ManaCost("{3}{G}{G}")
+        .Type("Sorcery")
+        .Text("Return two target cards from your graveyard to your hand. Exile Restock.")
+        .FlavorText("What looked like a retreat was actually a replenishing.")
+        .Cast(p =>
+          {
+            p.Effect = () => new ReturnToHand();
+
+            p.TargetSelector.AddEffect(trg =>
               {
-                  p.Effect = () => new ReturnToHand();
-
-                  p.TargetSelector.AddEffect(trg =>
-                  {
-                      trg.In.YourGraveyard();
-                      trg.MinCount = 2;
-                      trg.MaxCount = 2;
-                  });
-
-                  p.TargetingRule(new EffectBounce());
-                  p.TimingRule(new TargetRemovalTimingRule(removalTag: EffectTag.Bounce));
-
-                  p.PutToZoneAfterResolve = card => card.Exile();
+                trg.Is.Card().In.YourGraveyard();                
+                trg.MinCount = 2;
+                trg.MaxCount = 2;
               });
-        }
+
+            p.TargetingRule(new EffectOrCostRankBy(c => -c.Score));
+            p.PutToZoneAfterResolve = card => card.Exile();
+          });
     }
+  }
 }
---
+++ tests/Grove.Tests/Cards/Restock.cs
@@ -0,0 +1,29 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class Restock
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void ReturnDragons()
+      {
+        var dragon1 = C("Shivan Dragon");
+        var dragon2 = C("Shivan Dragon");
+        var restock = C("Restock");
+        
+        Graveyard(P1, "Grizzly Bears", dragon1, dragon2);        
+        Hand(P1, restock);
+        Battlefield(P1, "Mountain", "Forest", "Forest", "Forest", "Mountain", "Forest");
+
+        RunGame(1);
+
+        Equal(Zone.Exile, C(restock).Zone);
+        Equal(Zone.Hand, C(dragon1).Zone);
+        Equal(Zone.Hand, C(dragon2).Zone);
+      }
+    }
+  }
+}
