4b3c1ab9cd04c82283f5ac3d7375d3e71e3cb16a
Sandmann123 <norfirill@gmail.com>
2014-10-14 23:31:24 +0800

    Added new card: Furnace Whelp

---
+++ source/Grove/CardsLibrary/FurnaceWhelp.cs
@@ -0,0 +1,25 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class FurnaceWhelp : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Furnace Whelp")
+        .ManaCost("{2}{R}{R}")
+        .Type("Creature - Dragon")
+        .Text("{Flying}{EOL}{R}: Furnace Whelp gets +1/+0 until end of turn.")
+        .FlavorText("Baby dragons can't figure out humans—if they didn't want to be killed, why were they made of meat and treasure?")
+        .Power(2)
+        .Toughness(2)
+        .SimpleAbilities(Static.Flying)
+        .Pump(
+          cost: Mana.Red,
+          text: "{R}: Furnace Whelp gets +1/+0 until end of turn.",
+          powerIncrease: 1,
+          toughnessIncrease: 0);
+    }
+  }
+}
