73d5a42f9d3e613d08a72f56b783a4cc944eceb8
Sandmann123 <norfirill@gmail.com>
2014-10-14 23:28:36 +0800

    Added new card: Walking Corpse

---
+++ source/Grove/CardsLibrary/WalkingCorpse.cs
@@ -0,0 +1,18 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class WalkingCorpse : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Walking Corpse")
+        .ManaCost("{1}{B}")
+        .Type("Creature - Zombie")
+        .FlavorText("\"Feeding a normal army is a problem of logistics. With zombies, it is an asset. Feeding is why they fight. Feeding is why they are feared.\"{EOL}—Jadar, ghoulcaller of Nephalia")
+        .Power(2)
+        .Toughness(2);
+    }
+  }
+}
