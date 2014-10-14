4912bff3daf0a9b0a1523347591ba4874e22a9aa
pinky <pinky@lab.org>
2014-10-14 21:57:41 +0200

    Fixed Raise The Alarm.
    * Better timing rule for suprise.
    * Added a test.

--- source/Grove/CardsLibrary/OreskosSwiftclaw.cs
+++ source/Grove/CardsLibrary/OreskosSwiftclaw.cs
@@ -1,18 +1,19 @@
 ﻿namespace Grove.CardsLibrary
 {
-    using System.Collections.Generic;
+  using System.Collections.Generic;
 
-    public class OreskosSwiftclaw : CardTemplateSource
+  public class OreskosSwiftclaw : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
     {
-        public override IEnumerable<CardTemplate> GetCards()
-        {
-            yield return Card
-                .Named("Oreskos Swiftclaw")
-                .ManaCost("{1}{W}")
-                .Type("Creature — Cat Warrior")
-                .FlavorText("After the Battle of Pharagax Bridge, the Champion spent many months among the leonin of Oreskos. She found that they were quick to take offense, not because they were thin-skinned, but because they were always eager for a fight.{EOL}—The Theriad")
-                .Power(3)
-                .Toughness(1);
-        }
+      yield return Card
+        .Named("Oreskos Swiftclaw")
+        .ManaCost("{1}{W}")
+        .Type("Creature — Cat Warrior")
+        .FlavorText(
+          "After the Battle of Pharagax Bridge, the Champion spent many months among the leonin of Oreskos. She found that they were quick to take offense, not because they were thin-skinned, but because they were always eager for a fight.{EOL}—The Theriad")
+        .Power(3)
+        .Toughness(1);
     }
+  }
 }
---
+++ tests/Grove.Tests/Cards/RaiseTheAlarm.cs
@@ -0,0 +1,23 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class RaiseTheAlarm
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void KillAttackers()
+      {
+        Hand(P1, "Raise The Alarm");
+        Battlefield(P1, "Plains", "Plains");
+        Battlefield(P2, "Savannah Lions", "Savannah Lions");
+
+        RunGame(2);
+
+        Equal(2, P2.Graveyard.Count);
+      }
+    }
+  }
+}
