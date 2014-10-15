cd3cfea22ff71e6187163bdb5ce147a0e1a41951
pinky <pinky@lab.org>
2014-10-15 20:43:37 +0200

    Refactorings.

--- source/Grove/CardsLibrary/StaffOfTheMindMagus.cs
+++ source/Grove/CardsLibrary/StaffOfTheMindMagus.cs
@@ -1,33 +1,35 @@
 ﻿namespace Grove.CardsLibrary
 {
-    using System.Collections.Generic;
-    using Effects;
-    using Triggers;
+  using System.Collections.Generic;
+  using Effects;
+  using Triggers;
 
-    public class StaffOfTheMindMagus : CardTemplateSource
+  public class StaffOfTheMindMagus : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
     {
-        public override IEnumerable<CardTemplate> GetCards()
-        {
-            yield return Card
-                .Named("Staff of the Mind Magus")
-                .ManaCost("{3}")
-                .Type("Artifact")
-                .Text("Whenever you cast a blue spell or an Island enters the battlefield under your control, you gain 1 life.")
-                .FlavorText("A symbol of sagacity in bewildering times.")
-                .TriggeredAbility(p =>
-                {
-                    p.Text = "Whenever you cast a blue spell or an Island enters the battlefield under your control, you gain 1 life.";
+      yield return Card
+        .Named("Staff of the Mind Magus")
+        .ManaCost("{3}")
+        .Type("Artifact")
+        .Text("Whenever you cast a blue spell or an Island enters the battlefield under your control, you gain 1 life.")
+        .FlavorText("A symbol of sagacity in bewildering times.")
+        .TriggeredAbility(p =>
+          {
+            p.Text =
+              "Whenever you cast a blue spell or an Island enters the battlefield under your control, you gain 1 life.";
 
-                    p.Trigger(new OnCastedSpell((ability, card) => card.HasColor(CardColor.Blue) && card.Controller == ability.OwningCard.Controller));
-                    p.Trigger(new OnZoneChanged(
-                        to: Zone.Battlefield,
-                        filter: (card, ability, game) => card.Is().Land && card.Is("Island") && card.Controller == ability.OwningCard.Controller
-                        ));
+            p.Trigger(new OnCastedSpell((a, c) =>
+              c.HasColor(CardColor.Blue) && c.Controller == a.OwningCard.Controller));
 
-                    p.Effect = () => new ChangeLife(amount: 1, forYou: true);
+            p.Trigger(new OnZoneChanged(
+              to: Zone.Battlefield,
+              filter: (c, a, _) =>
+                c.Is("Island") && c.Controller == a.OwningCard.Controller));
 
-                    p.TriggerOnlyIfOwningCardIsInPlay = true;
-                });
-        }
+            p.Effect = () => new ChangeLife(amount: 1, forYou: true);
+            p.TriggerOnlyIfOwningCardIsInPlay = true;
+          });
     }
+  }
 }
--- source/Grove/CardsLibrary/TitanicGrowth.cs
+++ source/Grove/CardsLibrary/TitanicGrowth.cs
@@ -1,32 +1,34 @@
 ﻿namespace Grove.CardsLibrary
 {
-    using System.Collections.Generic;
-    using AI;
-    using AI.TargetingRules;
-    using AI.TimingRules;
-    using Effects;
-    using Modifiers;
+  using System.Collections.Generic;
+  using AI;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+  using Modifiers;
 
-    public class TitanicGrowth : CardTemplateSource
+  public class TitanicGrowth : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
     {
-        public override IEnumerable<CardTemplate> GetCards()
-        {
-            yield return Card
-                .Named("Titanic Growth")
-                .ManaCost("{1}{G}")
-                .Type("Instant")
-                .Text("Target creature gets +4/+4 until end of turn.")
-                .FlavorText("The massive dominate through might. The tiny survive with guile. Beware the tiny who become massive.")
-                .Cast(p =>
-                {
-                    p.Effect = () => new ApplyModifiersToTargets(
-                        () => new AddPowerAndToughness(4, 4) { UntilEot = true }).SetTags(EffectTag.IncreasePower, EffectTag.IncreaseToughness);
+      yield return Card
+        .Named("Titanic Growth")
+        .ManaCost("{1}{G}")
+        .Type("Instant")
+        .Text("Target creature gets +4/+4 until end of turn.")
+        .FlavorText(
+          "The massive dominate through might. The tiny survive with guile. Beware the tiny who become massive.")
+        .Cast(p =>
+          {
+            p.Effect = () => new ApplyModifiersToTargets(
+              () => new AddPowerAndToughness(4, 4) {UntilEot = true}).SetTags(EffectTag.IncreasePower,
+                EffectTag.IncreaseToughness);
 
-                    p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
+            p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
 
-                    p.TargetingRule(new EffectPumpInstant(4, 4));
-                    p.TimingRule(new PumpTargetCardTimingRule());
-                });
-        }
+            p.TimingRule(new PumpTargetCardTimingRule());
+            p.TargetingRule(new EffectPumpInstant(4, 4));
+          });
     }
+  }
 }
