0ebd1cf2c315eba183d326fe7de645517b624521
pinky <pinky@lab.org>
2014-10-24 22:28:29 +0200

    Hoarding Dragon improved test.

--- tests/Grove.Tests/Cards/HoardingDragon.cs
+++ tests/Grove.Tests/Cards/HoardingDragon.cs
@@ -1,34 +1,33 @@
 ﻿namespace Grove.Tests.Cards
 {
-    using System.Linq;
-    using Infrastructure;
-    using Xunit;
+  using System.Linq;
+  using Infrastructure;
+  using Xunit;
 
-    public class HoardingDragon
+  public class HoardingDragon
+  {
+    public class Ai : AiScenario
     {
-        public class Ai : AiScenario
-        {
-            [Fact]
-            public void CastHoardingDragon()
-            {
-                Hand(P1, "Hoarding Dragon");
-                Library(P1, "Profane Memento");
-                Battlefield(P1, "Mountain", "Mountain", "Mountain", "Mountain", "Mountain");
+      [Fact]
+      public void PutEnginetoToHand()
+      {
+        // Turn 1: Cast Hoarding Dragon. Wurmcoil Engine is exiled
+        // Turn 2: Draw card
+        // Turn 3: Draw card. Attack with dragon. Opponent casts Flesh to Dust. Artifact is put into Hand
+        
+        var engine = C("Wurmcoil Engine");
+        
+        Hand(P1, "Hoarding Dragon");
+        Library(P1, "Grizzly Bears", "Grizzly Bears", engine);
+        Battlefield(P1, "Mountain", "Mountain", "Mountain", "Mountain", "Mountain");
 
-                P2.Life = 4;
-                Hand(P2, "Flesh to Dust");
-                Battlefield(P2, "Swamp", "Swamp", "Mountain", "Mountain", "Mountain");
+        P2.Life = 4;
+        Hand(P2, "Flesh to Dust");
+        Battlefield(P2, "Swamp", "Swamp", "Mountain", "Mountain", "Mountain");
 
-                RunGame(3);
-
-                // Turn 1: Cast Hoarding Dragon. Profane Memento is exiled
-                // Turn 2: Draw card
-                // Turn 3: Draw card. Attack with dragon. Opponent casts Flesh to Dust. Artifact is put into Hand. Play artifact.
-
-                Equal(1, P1.Graveyard.Count);
-                Equal(1, P1.Battlefield.Lands.Count(c => c.IsTapped));
-                Equal(1, P2.Graveyard.Count);
-            }
-        }
+        RunGame(3);        
+        Equal(Zone.Hand, C(engine).Zone);                
+      }
     }
+  }
 }
