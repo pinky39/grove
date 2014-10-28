5b81f0b837e2c87db3999b88b739a087f81b710f
pinky <pinky@lab.org>
2014-10-28 18:46:31 +0100

    Wall of Frost, improved test.

--- tests/Grove.Tests/Cards/WallOfFrost.cs
+++ tests/Grove.Tests/Cards/WallOfFrost.cs
@@ -1,26 +1,24 @@
 ﻿namespace Grove.Tests.Cards
 {
-    using System.Linq;
-    using Infrastructure;
-    using Xunit;
+  using Infrastructure;
+  using Xunit;
 
-    public class WallOfFrost
+  public class WallOfFrost
+  {
+    public class Ai : AiScenario
     {
-        public class Ai : AiScenario
-        {
-            [Fact]
-            public void BlockWithWall()
-            {
-                var wall = C("Wall Of Frost");
-                Battlefield(P1, "Thran War Machine", "Forest", "Forest", "Forest", "Forest");
-                Battlefield(P2, wall);
+      [Fact]
+      public void MachineDoesNotUntapDuringNextUntap()
+      {
+        var machine = C("Thran War Machine");
 
-                RunGame(1);
+        Battlefield(P1, machine, "Forest", "Forest", "Forest", "Forest");
+        Battlefield(P2, C("Wall Of Frost"));
 
-                Equal(7, C(wall).Toughness);
-                Equal(5, P1.Battlefield.Count);                
-                True(P1.Battlefield.Creatures.Any(c => c.IsTapped));
-            }
-        }
+        RunGame(1);
+
+        True(C(machine).Has().DoesNotUntap);
+      }
     }
+  }
 }
