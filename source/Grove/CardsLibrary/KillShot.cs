a0a2655f646614db27fee70d437168743f0141c1
Sandmann123 <norfirill@gmail.com>
2014-10-28 15:19:12 +0700

    Added new card: Kill Shot

---
+++ source/Grove/CardsLibrary/KillShot.cs
@@ -0,0 +1,33 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+
+  public class KillShot : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+          .Named("Kill Shot")
+          .ManaCost("{2}{W}")
+          .Type("Instant")
+          .Text("Destroy target attacking creature.")
+          .FlavorText("Mardu archers are trained in Dakla, the way of the bow. They never miss their target, no matter how small, how fast, or how far away.")
+          .Cast(p =>
+          {
+            p.Effect = () => new DestroyTargetPermanents();
+
+            p.TargetSelector.AddEffect(trg =>
+            {
+              trg.Is.Card(c => c.Is().Creature && c.IsAttacker).On.Battlefield();
+              trg.Message = "Select target attacking creature.";
+            });
+
+            p.TimingRule(new AfterOpponentDeclaresAttackers());
+            p.TargetingRule(new EffectDestroy());
+          });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/KillShot.cs
@@ -0,0 +1,27 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class KillShot
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void DestrpyAttackingDragon()
+      {
+        Battlefield(P1, "Shivan Dragon");
+
+        Hand(P2, "Kill Shot");
+        Battlefield(P2, "Plains", "Plains", "Plains");
+
+        P2.Life = 5;
+
+        RunGame(1);
+
+        Equal(5, P2.Life);
+        Equal(1, P1.Graveyard.Count);
+      }
+    }
+  }
+}
