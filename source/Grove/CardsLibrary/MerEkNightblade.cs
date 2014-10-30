ffb71e8fab4ef90e2844e75d5e85a4df76ab0b8b
Sandmann123 <norfirill@gmail.com>
2014-10-30 15:38:29 +0700

    Added new card: Mer-Ek Nightblade

---
+++ source/Grove/CardsLibrary/MerEkNightblade.cs
@@ -0,0 +1,28 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Modifiers;
+
+  public class MerEkNightblade : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Mer-Ek Nightblade")
+        .ManaCost("{3}{B}")
+        .Type("Creature - Orc Assassin")
+        .Text("Outlast {B}{I}({B}, {T}: Put a +1/+1 counter on this creature. Outlast only as a sorcery.){/I}{EOL}Each creature you control with a +1/+1 counter on it has deathtouch.")
+        .Power(2)
+        .Toughness(3)
+        .Outlast("{B}")
+        .ContinuousEffect(p =>
+        {
+          p.CardFilter = (card, effect) =>
+            card.Is().Creature &&
+            card.CountersCount(CounterType.PowerToughness) > 0 &&
+            card.Controller == effect.Source.Controller;
+          p.Modifier = () => new AddStaticAbility(Static.Deathtouch);
+        });
+    }
+  }
+}
