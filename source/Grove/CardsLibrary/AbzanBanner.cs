0c96ee243dde03de2d6f31a8d2d7f6fecf1b049f
Sandmann123 <norfirill@gmail.com>
2014-10-30 14:05:39 +0700

    Added new card: Abzan Banner

---
+++ source/Grove/CardsLibrary/AbzanBanner.cs
@@ -0,0 +1,31 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Costs;
+  using Effects;
+
+  public class AbzanBanner : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Abzan Banner")
+        .ManaCost("{3}")
+        .Type("Artifact")
+        .Text("{T}: Add {W}, {B}, or {G} to your mana pool.{EOL}{W}{B}{G}, {T}, Sacrifice Abzan Banner: Draw a card.")
+        .ManaAbility(p =>
+        {
+          p.Text = "{T}: Add {W}, {B} or {G} to your mana pool.";
+          p.ManaAmount(Mana.Colored(isBlack: true, isGreen: true, isWhite: true));
+        })
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{W}{B}{G}, {T}, Sacrifice Abzan Banner: Draw a card.";
+          p.Cost = new AggregateCost(
+            new PayMana("{W}{B}{G}".Parse(), ManaUsage.Abilities),
+            new Tap());
+          p.Effect = () => new DrawCards(1);
+        });
+    }
+  }
+}
