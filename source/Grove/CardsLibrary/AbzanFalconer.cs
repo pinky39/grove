4a55ca6ae2551f4c57c8a0e43ecd0dba8c238342
Sandmann123 <norfirill@gmail.com>
2014-10-30 15:14:58 +0700

    Added new card: Abzan Falconer

---
+++ source/Grove/CardsLibrary/AbzanFalconer.cs
@@ -0,0 +1,29 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Modifiers;
+
+  public class AbzanFalconer : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Abzan Falconer")
+        .ManaCost("{2}{W}")
+        .Type("Creature - Human Soldier")
+        .Text("Outlast {W}{I}({W}, {T}: Put a +1/+1 counter on this creature. Outlast only as a sorcery.){/I}{EOL}Each creature you control with a +1/+1 counter on it has flying.")
+        .FlavorText("The fastest way across the dunes is above.")
+        .Power(2)
+        .Toughness(3)
+        .Outlast("{W}")
+        .ContinuousEffect(p =>
+        {
+          p.CardFilter = (card, effect) =>
+            card.Is().Creature &&
+            card.CountersCount(CounterType.PowerToughness) > 0 &&
+            card.Controller == effect.Source.Controller;
+          p.Modifier = () => new AddStaticAbility(Static.Flying);
+        });
+    }
+  }
+}
