f0e376edfc8ff76859ee6c44f3ef547949e3488c
Sandmann123 <norfirill@gmail.com>
2014-10-30 15:22:29 +0700

    Added new card: Abzan Battle Priest

---
+++ source/Grove/CardsLibrary/AbzanBattlePriest.cs
@@ -0,0 +1,29 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Modifiers;
+
+  public class AbzanBattlePriest : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Abzan Battle Priest")
+        .ManaCost("{3}{W}")
+        .Type("Creature - Human Cleric")
+        .Text("Outlast {W}{I}({W}, {T}: Put a +1/+1 counter on this creature. Outlast only as a sorcery.){/I}{EOL}Each creature you control with a +1/+1 counter on it has lifelink.")
+        .FlavorText("\"Wherever I walk, the ancestors walk too.\"")
+        .Power(3)
+        .Toughness(2)
+        .Outlast("{W}")
+        .ContinuousEffect(p =>
+        {
+          p.CardFilter = (card, effect) =>
+            card.Is().Creature &&
+            card.CountersCount(CounterType.PowerToughness) > 0 &&
+            card.Controller == effect.Source.Controller;
+          p.Modifier = () => new AddStaticAbility(Static.Lifelink);
+        });
+    }
+  }
+}
