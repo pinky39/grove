939411901265b791174806124c1dd506bbee43fc
pinky <pinky@lab.org>
2014-10-31 19:24:13 +0100

    Better tests for Glacial Crasher.

--- tests/Grove.Tests/Cards/GlacialCrasher.cs
+++ tests/Grove.Tests/Cards/GlacialCrasher.cs
@@ -1,30 +1,32 @@
 ﻿namespace Grove.Tests.Cards
 {
-    using Infrastructure;
-    using Xunit;
+  using Infrastructure;
+  using Xunit;
 
-    public class GlacialCrasher
+  public class GlacialCrasher
+  {
+    public class Ai : AiScenario
     {
-        public class Predefined : PredefinedScenario
-        {
-            [Fact]
-            public void CastGlacialCrasher()
-            {
-                var destroyLand = C("Lay Waste");
-                var creature = C("Glacial Crasher");
-                var land = C("Mountain");
+      [Fact]
+      public void CantAttack()
+      {
+        Battlefield(P1, "Glacial Crasher");
+        P2.Life = 5;
 
-                Hand(P1, creature, destroyLand);
-                Battlefield(P1, land);
+        RunGame(1);
+        Equal(5, P2.Life);
+      }
 
-                Exec(
-                  At(Step.FirstMain)
-                    .Cast(creature)
-                    .Verify(() => { False(C(creature).Has().CannotAttack); })
-                    .Cast(destroyLand, target: land)
-                    .Verify(() => { True(C(creature).Has().CannotAttack); })
-                  );
-            }
-        }
+      [Fact]
+      public void CanAttack()
+      {
+        Hand(P1, "Mountain");
+        Battlefield(P1, "Glacial Crasher");
+        P2.Life = 5;
+
+        RunGame(1);
+        Equal(0, P2.Life);
+      }
     }
+  }
 }
