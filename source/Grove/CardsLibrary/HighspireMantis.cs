bbf658c1244b35852ac85c83eaed607bfa7976a9
Sandmann123 <norfirill@gmail.com>
2014-11-03 14:13:41 +0700

    Added new card: Highspire Mantis

---
+++ source/Grove/CardsLibrary/HighspireMantis.cs
@@ -0,0 +1,20 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class HighspireMantis : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Highspire Mantis")
+        .ManaCost("{2}{R}{W}")
+        .Type("Creature — Insect")
+        .Text("{Flying}, {trample}")
+        .FlavorText("ts wings produce a high-pitched, barely audible whirring sound in flight. Only Jeskai masters are quiet enough to hear one coming.")
+        .Power(3)
+        .Toughness(3)
+        .SimpleAbilities(Static.Flying, Static.Trample);
+    }
+  }
+}
