7c66951e9366c5aef26c299c9bcbd2bacad8ab69
Sandmann123 <norfirill@gmail.com>
2014-11-03 14:22:37 +0700

    Added new card: Sage of the Inward Eye

---
+++ source/Grove/CardsLibrary/SageOfTheInwardEye.cs
@@ -0,0 +1,35 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+  using Modifiers;
+  using Triggers;
+
+  public class SageOfTheInwardEye : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Sage of the Inward Eye")
+        .ManaCost("{2}{U}{R}{W}")
+        .Type("Creature — Djinn Wizard")
+        .Text("{Flying}{EOL}Whenever you cast a noncreature spell, creatures you control gain lifelink until end of turn.")
+        .FlavorText("\"No one petal claims beauty for the lotus.\"")
+        .Power(3)
+        .Toughness(4)
+        .TriggeredAbility(p =>
+        {
+          p.Text = "Whenever you cast a noncreature spell, creatures you control gain lifelink until end of turn.";
+          p.Trigger(new OnCastedSpell((a, c) =>
+            c.Controller == a.OwningCard.Controller && !c.Is().Creature));
+
+          p.Effect = () => new ApplyModifiersToPermanents(
+            selector: (e, c) => c.Is().Creature, 
+            controlledBy: ControlledBy.SpellOwner, 
+            modifiers: () => new AddStaticAbility(Static.Lifelink) { UntilEot = true });
+
+          p.TriggerOnlyIfOwningCardIsInPlay = true;
+        });
+    }
+  }
+}
