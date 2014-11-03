873cd188a301359fed47c675d49363decbefc01e
Sandmann123 <norfirill@gmail.com>
2014-11-03 14:04:22 +0700

    Added new card: Bloodfire Expert

---
+++ source/Grove/CardsLibrary/BloodfireExpert.cs
@@ -0,0 +1,20 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class BloodfireExpert : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Bloodfire Expert")
+        .ManaCost("{2}{R}")
+        .Type("Creature — Efreet Monk")
+        .Text("{Prowess} {I}(Whenever you cast a noncreature spell, this creature gets +1/+1 until end of turn.){/I}")
+        .FlavorText("Some efreet abandon their homes in the volcanic Fire Rim to embrace the Jeskai Way and discipline their innate flames.")
+        .Power(3)
+        .Toughness(1)
+        .Prowess();
+    }
+  }
+}
