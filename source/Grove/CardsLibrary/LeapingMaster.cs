b0ef7a6f3185389a945f4b7bbfa612464dc7f4a2
Sandmann123 <norfirill@gmail.com>
2014-11-03 13:53:27 +0700

    Added new card: Leaping Master

---
+++ source/Grove/CardsLibrary/LeapingMaster.cs
@@ -0,0 +1,30 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TimingRules;
+  using Costs;
+  using Effects;
+  using Modifiers;
+
+  public class LeapingMaster : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Leaping Master")
+        .ManaCost("{1}{R}")
+        .Type("Creature — Human Monk")
+        .Text("{2}{W}: Leaping Master gains flying until end of turn.")
+        .FlavorText("\"Strength batters down barriers. Discipline ignores them.\"")
+        .Power(2)
+        .Toughness(1)
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{2}{W}: Leaping Master gains flying until end of turn.";
+          p.Cost = new PayMana("{2}{W}".Parse(), ManaUsage.Abilities);
+          p.Effect = () => new ApplyModifiersToSelf(() => new AddStaticAbility(Static.Flying){ UntilEot = true });
+          p.TimingRule(new WhenCardHas(c => !c.Has().Flying));
+        });
+    }
+  }
+}
