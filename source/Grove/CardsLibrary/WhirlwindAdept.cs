9767c2110d648f5e3fb672e32d6c3aa58573877b
Sandmann123 <norfirill@gmail.com>
2014-11-03 14:16:20 +0700

    Added new card: Whirlwind Adept

---
+++ source/Grove/CardsLibrary/WhirlwindAdept.cs
@@ -0,0 +1,20 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class WhirlwindAdept : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Whirlwind Adept")
+        .ManaCost("{4}{U}")
+        .Type("Creature — Djinn Monk")
+        .Text("{Hexproof} {I}(This creature can't be the target of spells or abilities your opponents control.){/I}{EOL}{Prowess} {I}(Whenever you cast a noncreature spell, this creature gets +1/+1 until end of turn.){/I}")
+        .Power(4)
+        .Toughness(2)
+        .Prowess()
+        .SimpleAbilities(Static.Hexproof);
+    }
+  }
+}
