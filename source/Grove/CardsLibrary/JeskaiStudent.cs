c6597f78472574d88978d1a00480564a67b56c49
Sandmann123 <norfirill@gmail.com>
2014-11-03 13:55:50 +0700

    Added new card: Jeskai Student

---
+++ source/Grove/CardsLibrary/JeskaiStudent.cs
@@ -0,0 +1,20 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class JeskaiStudent : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Jeskai Student")
+        .ManaCost("{1}{W}")
+        .Type("Creature — Human Monk")
+        .Text("{Prowess} {I}(Whenever you cast a noncreature spell, this creature gets +1/+1 until end of turn.){/I}")
+        .FlavorText("Discipline is the first pillar of the Jeskai Way. Each member of the clan trains in a weapon, perfecting its use over a lifetime.")
+        .Power(1)
+        .Toughness(3)
+        .Prowess();
+    }
+  }
+}
