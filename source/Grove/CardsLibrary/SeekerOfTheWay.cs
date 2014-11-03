d8cbaa111933d4ab2d1e1bd28b13c5c97859e00a
Sandmann123 <norfirill@gmail.com>
2014-11-03 13:59:42 +0700

    Added new card: Seeker of the Way

---
+++ source/Grove/CardsLibrary/SeekerOfTheWay.cs
@@ -0,0 +1,33 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+  using Modifiers;
+  using Triggers;
+
+  public class SeekerOfTheWay : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Seeker of the Way")
+        .ManaCost("{1}{W}")
+        .Type("Creature — Human Warrior")
+        .Text("{Prowess} {I}(Whenever you cast a noncreature spell, this creature gets +1/+1 until end of turn.){/I}{EOL}Whenever you cast a noncreature spell, Seeker of the Way gains lifelink until end of turn.")
+        .FlavorText("\"I don't know where my destiny lies, but I know it isn't here.\"")
+        .Power(2)
+        .Toughness(2)
+        .Prowess()
+        .TriggeredAbility(p =>
+        {
+          p.Text = "Whenever you cast a noncreature spell, Seeker of the Way gains lifelink until end of turn.";
+          p.Trigger(new OnCastedSpell((a, c) =>
+            c.Controller == a.OwningCard.Controller && !c.Is().Creature));
+
+          p.Effect = () => new ApplyModifiersToSelf(() => new AddStaticAbility(Static.Lifelink) { UntilEot = true });
+
+          p.TriggerOnlyIfOwningCardIsInPlay = true;
+        });
+    }
+  }
+}
