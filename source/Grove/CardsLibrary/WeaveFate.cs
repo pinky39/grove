420efcfa2b15b6df3e5cb12ea9a861702fc30f51
Sandmann123 <norfirill@gmail.com>
2014-11-03 20:32:28 +0700

    Added new card: Weave Fate

---
+++ source/Grove/CardsLibrary/WeaveFate.cs
@@ -0,0 +1,22 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+
+  public class WeaveFate : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Weave Fate")
+        .ManaCost("{3}{U}")
+        .Type("Instant")
+        .Text("Draw two cards.")
+        .FlavorText("Temur shamans speak of three destinies: the now, the echo of the past, and the unwritten. They find flickering paths among tangled possibilities.")
+        .Cast(p =>
+        {
+          p.Effect = () => new DrawCards(2);
+        });
+    }
+  }
+}
