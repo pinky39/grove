198e7de14d1304d8aba9bd0c3834bdbf490adcd4
Sandmann123 <norfirill@gmail.com>
2014-11-03 14:02:22 +0700

    Added new card: Jeskai Windscout

---
+++ source/Grove/CardsLibrary/JeskaiWindscout.cs
@@ -0,0 +1,21 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class JeskaiWindscout : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Jeskai Windscout")
+        .ManaCost("{2}{U}")
+        .Type("Creature — Bird Scout")
+        .Text("{Flying}{EOL}{Prowess} {I}(Whenever you cast a noncreature spell, this creature gets +1/+1 until end of turn.){/I}")
+        .FlavorText("They range from Sage-Eye Stronghold to the farthest reaches of Tarkir.")
+        .Power(2)
+        .Toughness(1)
+        .Prowess()
+        .SimpleAbilities(Static.Flying);
+    }
+  }
+}
