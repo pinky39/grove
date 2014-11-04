4fa527b78564a9a2af6e0bd136fa03aca286eb25
Sandmann123 <norfirill@gmail.com>
2014-11-04 14:48:52 +0700

    Added new card: Mardu Hateblade

---
+++ source/Grove/CardsLibrary/MarduHateblade.cs
@@ -0,0 +1,31 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TimingRules;
+  using Costs;
+  using Effects;
+  using Modifiers;
+
+  public class MarduHateblade : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Mardu Hateblade")
+        .ManaCost("{W}")
+        .Type("Creature — Human Warrior")
+        .Text("{B}: Mardu Hateblade gains deathtouch until end of turn.{I}(Any amount of damage it deals to a creature is enough to destroy it.){/I}")
+        .FlavorText("\"There may be little honor in my tactics, but there is no honor in losing.\"")
+        .Power(1)
+        .Toughness(1)
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{B}: Mardu Hateblade gains deathtouch until end of turn.";
+          p.Cost = new PayMana(Mana.Black, ManaUsage.Abilities);
+          p.Effect = () => new ApplyModifiersToSelf(
+            () => new AddStaticAbility(Static.Deathtouch) { UntilEot = true });
+          p.TimingRule(new WhenCardHas(c => !c.Has().Deathtouch));
+        });
+    }
+  }
+}
