168ac5d1e37d1e906eb4b7d53f7e6a895513112d
Sandmann123 <norfirill@gmail.com>
2014-11-04 14:56:14 +0700

    Added new card: Gurmag Swiftwing

---
+++ source/Grove/CardsLibrary/GurmagSwiftwing.cs
@@ -0,0 +1,20 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class GurmagSwiftwing : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Gurmag Swiftwing")
+        .ManaCost("{1}{B}")
+        .Type("Creature — Bat")
+        .Text("{Flying}, {first strike}, {haste}")
+        .FlavorText("\"Anything a falcon can do, a bat can do in pitch darkness.\"{EOL}—Urdnan the Wanderer")
+        .Power(1)
+        .Toughness(2)
+        .SimpleAbilities(Static.Flying, Static.FirstStrike, Static.Haste);
+    }
+  }
+}
