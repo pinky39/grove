16af88d95f39635f427600df931a73bb3d29e40d
Sandmann123 <norfirill@gmail.com>
2014-11-04 23:37:24 +0700

    Added new cards: Nomad Outpost, Bloodfell Caves

---
+++ source/Grove/CardsLibrary/BloodfellCaves.cs
@@ -0,0 +1,29 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+  using Triggers;
+
+  public class BloodfellCaves : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Bloodfell Caves")
+        .Type("Land")
+        .Text("Bloodfell Caves enters the battlefield tapped.{EOL}When Bloodfell Caves enters the battlefield, you gain 1 life.{EOL}{T}: Add {B} or {R} to your mana pool.")
+        .Cast(p => p.Effect = () => new CastPermanent(tap: true))
+        .ManaAbility(p =>
+        {
+          p.Text = "{T}: Add {B} or {R} to your mana pool.";
+          p.ManaAmount(Mana.Colored(isBlack: true, isRed: true));
+        })
+        .TriggeredAbility(p =>
+        {
+          p.Text = "When Bloodfell Caves enters the battlefield, you gain 1 life.";
+          p.Trigger(new OnZoneChanged(to: Zone.Battlefield));
+          p.Effect = () => new ChangeLife(1, yours: true);
+        });
+    }
+  }
+}
---
+++ source/Grove/CardsLibrary/NomadOutpost.cs
@@ -0,0 +1,23 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+
+  public class NomadOutpost : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Nomad Outpost")
+        .Type("Land")
+        .Text("Nomad Outpost enters the battlefield tapped.{EOL}{T}: Add {R}, {W} or {B} to your mana pool.")
+        .FlavorText("\"Only the weak imprison themselves behind walls. We live free under the wind, and our freedom makes us strong.\"{EOL}—Zurgo, khan of the Mardu")
+        .Cast(p => p.Effect = () => new CastPermanent(tap: true))
+        .ManaAbility(p =>
+        {
+          p.Text = "{T}: Add {R}, {W} or {B} to your mana pool.";
+          p.ManaAmount(Mana.Colored(isBlack: true, isRed: true, isWhite: true));
+        });
+    }
+  }
+}
