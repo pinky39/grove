7a211fdb47de3ea8c714af468739174853bb6e33
Sandmann123 <norfirill@gmail.com>
2014-11-04 16:12:37 +0700

    Added new card: Ankle Shanker

---
+++ source/Grove/CardsLibrary/AnkleShanker.cs
@@ -0,0 +1,38 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+  using Modifiers;
+  using Triggers;
+
+  public class AnkleShanker : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+          .Named("Ankle Shanker")
+          .ManaCost("{2}{R}{W}{B}")
+          .Type("Creature — Goblin Berserker")
+          .Text("{Haste}{EOL}Whenever Ankle Shanker attacks, creatures you control gain first strike and deathtouch until end of turn.")
+          .FlavorText("The stature of the fighter matters less than the depth of the cut.")
+          .Power(2)
+          .Toughness(2)
+          .SimpleAbilities(Static.Haste)
+          .TriggeredAbility(p =>
+          {
+            p.Text = "Whenever Ankle Shanker attacks, creatures you control gain first strike and deathtouch until end of turn.";
+
+            p.Trigger(new WhenThisAttacks());
+
+            p.Effect = () => new ApplyModifiersToPermanents(
+              selector: (e, c) => c.Is().Creature, 
+              controlledBy: ControlledBy.SpellOwner,
+              modifiers: new CardModifierFactory[]
+              {
+                () => new AddStaticAbility(Static.FirstStrike) { UntilEot = true },
+                () => new AddStaticAbility(Static.Deathtouch) { UntilEot = true },
+              });
+          });
+    }
+  }
+}
