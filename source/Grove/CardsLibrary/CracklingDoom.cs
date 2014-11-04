e234ef6a6392a52a59fde2a026670b09a8480966
Sandmann123 <norfirill@gmail.com>
2014-11-05 00:08:21 +0700

    Added new card: Crackling Doom

---
+++ source/Grove/CardsLibrary/CracklingDoom.cs
@@ -0,0 +1,33 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using System.Linq;
+  using AI.TimingRules;
+  using Effects;
+
+  public class CracklingDoom : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Crackling Doom")
+        .ManaCost("{R}{W}{B}")
+        .Type("Instant")
+        .Text("Crackling Doom deals 2 damage to each opponent. Each opponent sacrifices a creature with the greatest power among creatures he or she controls.")
+        .FlavorText("Do not fear the lightning. Fear the one it obeys.")
+        .Cast(p =>
+        {
+          p.Effect = () => new CompoundEffect(
+            new DealDamageToPlayer(2, P(e => e.Controller.Opponent)),
+            new PlayerSacrificePermanents(
+              count: 1,
+              player: P(e => e.Controller.Opponent),
+              filter: c => c.Is().Creature && c.Controller.Battlefield.Creatures.All(cr => cr.Power <= c.Power),
+              text: "Sacrifice a creature."));
+
+
+          p.TimingRule(new Any(new NonTargetRemovalTimingRule(1), new OnSecondMain()));
+        });
+    }
+  }
+}
