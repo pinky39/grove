d4cceaf0caf90692711296ff7d608cd3742d4cb7
Sandmann123 <norfirill@gmail.com>
2014-11-04 14:45:01 +0700

    Added new card: Firehoof Cavalry
    * fixed flavor text of War-Name Aspirant

---
+++ source/Grove/CardsLibrary/FirehoofCavalry.cs
@@ -0,0 +1,35 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI;
+  using AI.RepetitionRules;
+  using AI.TimingRules;
+  using Costs;
+  using Effects;
+  using Modifiers;
+
+  public class FirehoofCavalry : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Firehoof Cavalry")
+        .ManaCost("{W}")
+        .Type("Creature — Human Berserker")
+        .Text("{3}{R}: Firehoof Cavalry gets +2/+0 and gains trample until end of turn.")
+        .FlavorText("\"What warrior worth the name fears to leave a trail? If my enemies seek me, let them follow the ashes in my wake.\"")
+        .Power(1)
+        .Toughness(1)
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{3}{R}: Firehoof Cavalry gets +2/+0 and gains trample until end of turn.";
+          p.Cost = new PayMana("{3}{R}".Parse(), ManaUsage.Abilities, supportsRepetitions: true);
+          p.Effect = () => new ApplyModifiersToSelf(
+            () => new AddPowerAndToughness(2, 0){UntilEot = true},
+            () => new AddStaticAbility(Static.Trample) { UntilEot = true }).SetTags(EffectTag.IncreasePower);
+          p.TimingRule(new PumpOwningCardTimingRule(2, 0));
+          p.RepetitionRule(new RepeatMaxTimes());
+        });
+    }
+  }
+}
--- source/Grove/CardsLibrary/WarNameAspirant.cs
+++ source/Grove/CardsLibrary/WarNameAspirant.cs
@@ -11,7 +11,7 @@
         .ManaCost("{1}{R}")
         .Type("Creature — Human Warrior")
         .Text("{I}Raid{/I} — War-Name Aspirant enters the battlefield with a +1/+1 counter on it if you attacked with a creature this turn.{EOL}War-Name Aspirant can't be blocked by creatures with power 1 or less.")
-        .FlavorText("The Abzan avoid extended supply lines by incorporating weapons stores into their battle formations.")
+        .FlavorText("No battle means more to a Mardu warrior than the one that earns her war name.")
         .Power(2)
         .Toughness(1)
         .Raid()
