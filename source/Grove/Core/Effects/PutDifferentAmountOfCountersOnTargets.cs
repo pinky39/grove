ac384bfe435eb412aadd0edee9f1ad0a646dcabf
Sandmann123 <norfirill@gmail.com>
2014-11-06 16:34:32 +0700

    Improved Incremental Growth.
    * better implementation and name for PutDifferentAmountOfCountersOnTargets
    * added test for Incremental Growth

---
+++ source/Grove/Core/Effects/PutDifferentAmountOfCountersOnTargets.cs
@@ -0,0 +1,44 @@
+﻿namespace Grove.Effects
+{
+  using System.Collections.Generic;
+  using AI;
+  using Modifiers;
+
+  public class PutDifferentAmountOfCountersOnTargets : Effect
+  {
+    private readonly int _power;
+    private readonly int _toughness;
+    private readonly List<int> _amounts = new List<int>();
+
+    private PutDifferentAmountOfCountersOnTargets() {}
+
+    public PutDifferentAmountOfCountersOnTargets(int power, int toughness, IEnumerable<int> amounts)
+    {
+      _power = power;
+      _toughness = toughness;
+      _amounts.AddRange(amounts);
+
+      SetTags(EffectTag.IncreasePower, EffectTag.IncreaseToughness);
+    }
+
+    protected override void ResolveEffect()
+    {
+      var p = new ModifierParameters
+      {
+        SourceEffect = this,
+        SourceCard = Source.OwningCard,
+        X = X
+      };
+
+      for (var i = 0; i < _amounts.Count; i++)
+      {
+        var target = Targets.Effect[i];
+
+        if (IsValid(target))
+        {
+          target.Card().AddModifier(new AddCounters(() => new PowerToughness(_power, _toughness), _amounts[i]), p);
+        }
+      }    
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/IncrementalGrowth.cs
@@ -0,0 +1,30 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class IncrementalGrowth
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void PutThreeCountersOnThreeDifferentTargets()
+      {
+        var bear1 = C("Grizzly Bears");
+        var bear2 = C("Grizzly Bears");
+        var bear3 = C("Grizzly Bears");
+
+        Hand(P1, "Incremental Growth");
+        Battlefield(P1, "Mountain", "Mountain", "Mountain", "Forest", "Forest", bear1, bear2, bear3, "Fugitive Wizard");
+
+        P2.Life = 13;
+        RunGame(1);
+
+        Equal(0, P2.Life);
+        Equal(3, C(bear1).Power);
+        Equal(4, C(bear2).Power);
+        Equal(5, C(bear3).Power);
+      }
+    }
+  }
+}
