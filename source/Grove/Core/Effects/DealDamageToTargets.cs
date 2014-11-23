b5ed4d91072425cb62aba3a2f254642523fcbf1f
Sandmann123 <norfirill@gmail.com>
2014-11-23 14:14:58 +0700

    Minor refactoring: remove redundant condition;

--- source/Grove/Core/Effects/DealDamageToTargets.cs
+++ source/Grove/Core/Effects/DealDamageToTargets.cs
@@ -41,7 +41,7 @@
           _amount.Value,
           (IDamageable) target,
           isCombat: false,
-          canBePrevented: _canBePrevented == null || _canBePrevented.Value);
+          canBePrevented: _canBePrevented.Value);
 
 
         if (_gainLife)
