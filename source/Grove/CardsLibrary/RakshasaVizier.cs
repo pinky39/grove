239f8121fc5c4d91c8c061cb6c0bef434278fb0b
Sandmann123 <norfirill@gmail.com>
2014-11-26 23:23:37 +0700

    Added new card: Rakshasa Vizier

---
+++ source/Grove/CardsLibrary/RakshasaVizier.cs
@@ -0,0 +1,36 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+  using Modifiers;
+  using Triggers;
+
+  public class RakshasaVizier : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Rakshasa Vizier")
+        .ManaCost("{2}{B}{G}{U}")
+        .Type("Creature - Cat Demon")
+        .Text("Whenever one or more cards are put into exile from your graveyard, put that many +1/+1 counters on Rakshasa Vizier.")
+        .FlavorText("Rakshasa offer deals that sound advantageous to those who forget who they are dealing with.")
+        .Power(4)
+        .Toughness(4)
+        .TriggeredAbility(p =>
+        {
+          p.Text = "Whenever one or more cards are put into exile from your graveyard, put that many +1/+1 counters on Rakshasa Vizier.";
+
+          p.Trigger(new OnZoneChanged(
+            from: Zone.Graveyard,
+            to: Zone.Exile,
+            filter: (c, a, _) => c.Controller == a.OwningCard.Controller && c != a.OwningCard));
+
+          p.Effect = () => new ApplyModifiersToSelf(
+            () => new AddCounters(() => new PowerToughness(1, 1), count: 1));
+
+          p.TriggerOnlyIfOwningCardIsInPlay = true;
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/RakshasaVizier.cs
@@ -0,0 +1,28 @@
+﻿namespace Grove.Tests.Cards
+{
+  using System.Linq;
+  using Infrastructure;
+  using Xunit;
+
+  public class RakshasaVizier
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void RakshasaGetsCounterForExiled()
+      {
+        var rakshasa = C("Rakshasa Vizier").IsEnchantedWith("Pacifism");
+
+        Battlefield(P1, "Juggernaut");
+
+        P2.Life = 2;
+        Battlefield(P2, rakshasa, "Grizzly Bears", "Planar Void");
+
+        RunGame(1);
+
+        Equal(1, P2.Exile.Count());
+        Equal(5, C(rakshasa).Power);
+      }
+    }
+  }
+}
