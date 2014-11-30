9137aa91aa9819930657cf157199af8527372c8c
Sandmann123 <norfirill@gmail.com>
2014-11-30 20:20:50 +0700

    Added new card: Shambling Attendants

---
+++ source/Grove/CardsLibrary/ShamblingAttendants.cs
@@ -0,0 +1,20 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class ShamblingAttendants : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+          .Named("Shambling Attendants")
+          .ManaCost("{7}{B}")
+          .Type("Creature — Zombie")
+          .Text("{Delve}{I}(Each card you exile from your graveyard while casting this spell pays for {1}.){/I}{EOL}{Deathtouch}{I}(Any amount of damage this deals to a creature is enough to destroy it.){/I}")
+          .FlavorText("\"Let the world behold what becomes of those who defy us.\"{EOL}—Taigam, Sidisi's Hand")
+          .Power(3)
+          .Toughness(5)
+          .SimpleAbilities(Static.Deathtouch, Static.Delve);
+    }
+  }
+}
