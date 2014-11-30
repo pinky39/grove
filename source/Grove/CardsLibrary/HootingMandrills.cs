264fe3835d429f58e022bfddff590150310d5846
Sandmann123 <norfirill@gmail.com>
2014-11-30 20:18:15 +0700

    Added new card: Hooting Mandrills

---
+++ source/Grove/CardsLibrary/HootingMandrills.cs
@@ -0,0 +1,20 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class HootingMandrills : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+          .Named("Hooting Mandrills")
+          .ManaCost("{5}{G}")
+          .Type("Creature — Ape")
+          .Text("{Delve}{I}(Each card you exile from your graveyard while casting this spell pays for {1}.){/I}{EOL}{Trample}")
+          .FlavorText("Interlopers in Sultai territory usually end up as crocodile chow or baboon bait.")
+          .Power(4)
+          .Toughness(4)
+          .SimpleAbilities(Static.Trample, Static.Delve);
+    }
+  }
+}
