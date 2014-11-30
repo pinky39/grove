17d9e5cc130c581cc85cd6973cbe4d019363316a
Sandmann123 <norfirill@gmail.com>
2014-11-30 20:32:22 +0700

    Added new card: Rakshasa's Secret

---
+++ source/Grove/CardsLibrary/RakshasasSecret.cs
@@ -0,0 +1,24 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+
+  public class RakshasasSecret : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Rakshasa's Secret")
+        .ManaCost("{2}{B}")
+        .Type("Sorcery")
+        .Text("Target opponent discards two cards. Put the top two cards of your library into your graveyard.")
+        .FlavorText("\"It saddens me to lose a source of inspiration. This one seemed especially promising.\"—Ashiok")
+        .Cast(p =>
+        {
+          p.Effect = () => new CompoundEffect(
+            new OpponentDiscardsCards(selectedCount: 2),
+            new PlayerPutsTopCardsFromLibraryToGraveyard(P(e => e.Controller), count: 2));
+        });
+    }
+  }
+}
