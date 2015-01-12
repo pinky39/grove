a1e9380725e32a05cfa763278d8e53d14cabd21d
Sandmann123 <norfirill@gmail.com>
2015-01-12 22:11:26 +0700

    Added new card: Heir of the Wilds

---
+++ source/Grove/CardsLibrary/HeirOfTheWilds.cs
@@ -0,0 +1,36 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using System.Linq;
+  using Effects;
+  using Modifiers;
+  using Triggers;
+
+  public class HeirOfTheWilds : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Heir of the Wilds")
+        .ManaCost("{1}{G}")
+        .Type("Creature — Human Warrior")
+        .Text("{Deathtouch}{EOL}{I}Ferocious{/I} — Whenever Heir of the Wilds attacks, if you control a creature with power 4 or greater, Heir of the Wilds gets +1/+1 until end of turn.")
+        .FlavorText("In the high caves of the Qal Sisma mountains, young hunters quest to hear the echoes of their fierce ancestors.")
+        .Power(2)
+        .Toughness(2)
+        .SimpleAbilities(Static.Deathtouch)
+        .TriggeredAbility(p =>
+        {
+          p.Text = "Whenever Heir of the Wilds attacks, if you control a creature with power 4 or greater, Heir of the Wilds gets +1/+1 until end of turn.";
+
+          p.Trigger(new WhenThisAttacks()
+          {
+            Condition = (t, g) => t.OwningCard.Controller.Battlefield.Creatures.Any(x => x.Power >= 4)
+          });
+
+          p.Effect = () => new ApplyModifiersToSelf(() => new AddPowerAndToughness(1, 1) { UntilEot = true });
+          p.TriggerOnlyIfOwningCardIsInPlay = true;
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/HeirOfTheWilds.cs
@@ -0,0 +1,21 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class HeirOfTheWilds
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void HeirGains11OnAttack()
+      {
+        Battlefield(P1, "Heir of the Wilds", "Juggernaut");
+
+        RunGame(1);
+
+        Equal(12, P2.Life);
+      }
+    }
+  }
+}
