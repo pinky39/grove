b4e1df9d7b260be24469d6a53c31d3c3e37e4b5b
Sandmann123 <norfirill@gmail.com>
2015-01-12 22:32:52 +0700

    Improved Suspension Field
    * fixed condition for a trigger ability
    * re-implemented the trigger ability
    * added test

--- source/Grove/CardsLibrary/SuspensionField.cs
+++ source/Grove/CardsLibrary/SuspensionField.cs
@@ -1,6 +1,7 @@
 ﻿namespace Grove.CardsLibrary
 {
   using System.Collections.Generic;
+  using AI.TargetingRules;
   using Effects;
   using Triggers;
 
@@ -12,25 +13,30 @@
         .Named("Suspension Field")
         .ManaCost("{1}{W}")
         .Type("Enchantment")
-        .Text("When Suspension Field enters the battlefield, you may exile target creature with toughness 3 or greater until Suspension Field leaves the battlefield. {I}(That creature returns under its owner's control.){/I}")
+        .Text(
+          "When Suspension Field enters the battlefield, you may exile target creature with toughness 3 or greater until Suspension Field leaves the battlefield. {I}(That creature returns under its owner's control.){/I}")
         .TriggeredAbility(p =>
         {
           p.Trigger(new OnZoneChanged(to: Zone.Battlefield));
-          p.Effect = () => new CompoundEffect(
-            new ExileTargets(),
-            new Attach());
+
+          p.Effect = () => new ExileTargetsUntilOwnerLeavesBattlefield();
+//          p.Effect = () => new CompoundEffect(
+//            new ExileTargets(),
+//            new Attach());
 
           p.TargetSelector.AddEffect(
-            trg => trg.Is.Card(c => c.Is().Creature && c.Power >= 3).On.Battlefield());
-        })
-        .TriggeredAbility(p =>
-        {
-          p.Trigger(new OnZoneChanged(
-                  from: Zone.Battlefield,
-                  filter: (c, a, g) => a.OwningCard == c && a.OwningCard.AttachedTo != null));
+            trg => trg.Is.Card(c => c.Is().Creature && c.Toughness >= 3).On.Battlefield());
 
-          p.Effect = () => new PutCardToBattlefield(P(e => e.Source.OwningCard.AttachedTo), Zone.Exile);
+          p.TargetingRule(new EffectExileBattlefield());
         });
+//        .TriggeredAbility(p =>
+//        {
+//          p.Trigger(new OnZoneChanged(
+//                  from: Zone.Battlefield,
+//                  filter: (c, a, g) => a.OwningCard == c && a.OwningCard.AttachedTo != null));
+//
+//          p.Effect = () => new PutCardToBattlefield(P(e => e.Source.OwningCard.AttachedTo), Zone.Exile);
+//        });
     }
   }
 }
---
+++ tests/Grove.Tests/Cards/SuspensionField.cs
@@ -0,0 +1,39 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class SuspensionField
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void ExileDragon()
+      {
+        Hand(P1, "Suspension Field");
+        Battlefield(P1, "Plains", "Plains");
+
+        Battlefield(P2, "Shivan Dragon");
+
+        RunGame(1);
+
+        Equal(0, P2.Battlefield.Count);
+      }
+
+      [Fact]
+      public void ReturnDragonFromExile()
+      {
+        Hand(P1, "Suspension Field");
+        Battlefield(P1, "Plains", "Plains");
+
+        Hand(P2, "Naturalize");
+        Battlefield(P2, "Shivan Dragon", "Forest", "Plains");
+
+        RunGame(2);
+
+        Equal(3, P2.Battlefield.Count);
+        Equal(1, P1.Graveyard.Count);
+      }
+    }
+  }
+}
