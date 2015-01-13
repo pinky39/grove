0091a80295c11268a6e5e813e7d333ba82f32f56
Sandmann123 <norfirill@gmail.com>
2015-01-13 10:44:18 +0700

    Added new card: Summit Prowler

---
+++ source/Grove/CardsLibrary/SummitProwler.cs
@@ -0,0 +1,18 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class SummitProwler : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Summit Prowler")
+        .ManaCost("{2}{R}{R}")
+        .Type("Creature - Yeti")
+        .FlavorText("\"Do you hunt the yetis of the high peaks, stripling? They are as fierce as the bear that fears no foe and as sly as the mink that creeps unseen. You will be as much prey as they.\"{EOL}—Nitula, the Hunt Caller")
+        .Power(4)
+        .Toughness(3);
+    }
+  }
+}
