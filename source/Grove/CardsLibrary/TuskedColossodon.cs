839c50798688ca747b8d0eb44ed1dd8cd123a6a7
Sandmann123 <norfirill@gmail.com>
2015-01-13 10:55:19 +0700

    Added new card: Tusked Colossodon

---
+++ source/Grove/CardsLibrary/TuskedColossodon.cs
@@ -0,0 +1,18 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class TuskedColossodon : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Tusked Colossodon")
+        .ManaCost("{4}{G}{G}")
+        .Type("Creature - Beast")
+        .FlavorText("A band of Temur hunters, fleeing the Mardu, dug a hideout beneath such a creature as it slept. The horde found them and attacked. For three days the Temur held them at bay, and all the while the great beast slumbered.")
+        .Power(6)
+        .Toughness(5);
+    }
+  }
+}
