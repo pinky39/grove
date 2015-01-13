c6669ec382042941c7a63b5c8aee7c001f961128
Sandmann123 <norfirill@gmail.com>
2015-01-13 10:59:06 +0700

    Added new card: Temur Banner

---
+++ source/Grove/CardsLibrary/TemurBanner.cs
@@ -0,0 +1,33 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Costs;
+  using Effects;
+
+  public class TemurBanner : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Temur Banner")
+        .ManaCost("{3}")
+        .Type("Artifact")
+        .Text("{T}: Add {G}, {U}, or {R} to your mana pool.{EOL}{G}{U}{R}, {T}, Sacrifice Temur Banner: Draw a card.")
+        .FlavorText("Savagery to survive, courage to triumph.")
+        .ManaAbility(p =>
+        {
+          p.Text = "{T}: Add {G}, {U}, or {R} to your mana pool.";
+          p.ManaAmount(Mana.Colored(isRed: true, isGreen: true, isBlue: true));
+        })
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{G}{U}{R}, {T}, Sacrifice Temur Banner: Draw a card.";
+          p.Cost = new AggregateCost(
+            new PayMana("{G}{U}{R}".Parse(), ManaUsage.Abilities),
+            new Tap(),
+            new Sacrifice());
+          p.Effect = () => new DrawCards(1);
+        });
+    }
+  }
+}
