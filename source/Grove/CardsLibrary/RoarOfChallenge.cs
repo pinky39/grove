df75dfbd0bf96e69db8b700b699d8cb7be0eaf92
Sandmann123 <norfirill@gmail.com>
2015-01-13 13:42:33 +0700

    Added new card: Roar of Challenge

---
+++ source/Grove/CardsLibrary/RoarOfChallenge.cs
@@ -0,0 +1,45 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using System.Linq;
+  using AI;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+  using Events;
+  using Modifiers;
+  using Triggers;
+
+  public class RoarOfChallenge : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Roar of Challenge")
+        .ManaCost("{2}{G}")
+        .Type("Sorcery")
+        .Text(
+          "All creatures able to block target creature this turn do so.{EOL}{I}Ferocious{/I} — That creature gains indestructible until end of turn if you control a creature with power 4 or greater.")
+        .Cast(p =>
+        {
+          p.Effect = () => new ApplyModifiersToTargets(
+            () => new AddStaticAbility(Static.Lure) {UntilEot = true});
+
+          p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
+
+          p.TargetingRule(new EffectBigWithoutEvasions());
+          p.TimingRule(new OnFirstMain());
+        })
+        .TriggeredAbility(p =>
+        {
+          p.Trigger(new OnCastedSpell((a, c) => a.OwningCard == c)
+          {
+            Condition = (t, g) => t.Controller.Battlefield.Creatures.Any(x => x.Power >= 4)
+          });
+
+          p.Effect = () => new ApplyModifiersToCard(P((e, t) => e.TriggerMessage<SpellPutOnStackEvent>().Targets.Effect.First().Card()),
+            () => new AddStaticAbility(Static.Indestructible) { UntilEot = true }).SetTags(EffectTag.Indestructible);
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/RoarOfChallenge.cs
@@ -0,0 +1,43 @@
+﻿namespace Grove.Tests.Cards
+{
+  using System.Linq;
+  using Infrastructure;
+  using Xunit;
+
+  public class RoarOfChallenge
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void CastLureOnRats()
+      {
+        Hand(P1, "Roar of Challenge");
+        Battlefield(P1, "Typhoid Rats", "Grizzly Bears", "Grizzly Bears", "Forest", "Forest", "Forest");
+
+        Battlefield(P2, "Grizzly Bears", "Grizzly Bears", "Grizzly Bears");
+
+        RunGame(1);
+
+        Equal(16, P2.Life);
+        Equal(2, P1.Battlefield.Creatures.Count());
+        Equal(2, P2.Battlefield.Creatures.Count());
+      }
+
+      [Fact]
+      public void CastLureAndIndestructibleOnRats()
+      {
+        Hand(P1, "Roar of Challenge");
+        Battlefield(P1, "Typhoid Rats", "Juggernaut", "Forest", "Forest", "Forest");
+
+        P2.Life = 6;
+        Battlefield(P2, "Grizzly Bears", "Grizzly Bears", "Grizzly Bears");
+
+        RunGame(1);
+
+        Equal(1, P2.Life);
+        Equal(2, P1.Battlefield.Creatures.Count());
+        Equal(2, P2.Battlefield.Creatures.Count());
+      }
+    }
+  }
+}
