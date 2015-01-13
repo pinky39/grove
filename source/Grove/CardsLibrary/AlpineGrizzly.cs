360ca61bf7402f6242a155a4cbaca1b175ceaa8b
Sandmann123 <norfirill@gmail.com>
2015-01-13 10:41:09 +0700

    Added new card: Alpine Grizzly

---
+++ source/Grove/CardsLibrary/AlpineGrizzly.cs
@@ -0,0 +1,18 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class AlpineGrizzly : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Alpine Grizzly")
+        .ManaCost("{2}{G}")
+        .Type("Creature - Bear")
+        .FlavorText("The Temur welcome bears into the clan, fighting alongside them in battle. The relationship dates back to when they labored side by side under Sultai rule.")
+        .Power(4)
+        .Toughness(2);
+    }
+  }
+}
