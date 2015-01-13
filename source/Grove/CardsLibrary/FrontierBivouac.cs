386f60f7db4a13f2b11cc5fa28d5ee895e376343
Sandmann123 <norfirill@gmail.com>
2015-01-13 14:08:47 +0700

    Added new cards: Rugged Highlands; Frontier Bivouac

---
+++ source/Grove/CardsLibrary/FrontierBivouac.cs
@@ -0,0 +1,23 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+
+  public class FrontierBivouac : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Frontier Bivouac")
+        .Type("Land")
+        .Text("Frontier Bivouac enters the battlefield tapped.{EOL}{T}: Add {G}, {U} or {R} to your mana pool.")
+        .FlavorText("\"The most powerful dreams visit those who shelter in a dragon's skull.\"{EOL}—Chianul, Who Whispers Twice")
+        .Cast(p => p.Effect = () => new CastPermanent(tap: true))
+        .ManaAbility(p =>
+        {
+          p.Text = "{T}: Add {G}, {U} or {R} to your mana pool.";
+          p.ManaAmount(Mana.Colored(isBlue: true, isRed: true, isGreen: true));
+        });
+    }
+  }
+}
---
+++ source/Grove/CardsLibrary/RuggedHighlands.cs
@@ -0,0 +1,29 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+  using Triggers;
+
+  public class RuggedHighlands : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Rugged Highlands")
+        .Type("Land")
+        .Text("Rugged Highlands enters the battlefield tapped.{EOL}When Rugged Highlands enters the battlefield, you gain 1 life.{EOL}{T}: Add {R} or {G} to your mana pool.")
+        .Cast(p => p.Effect = () => new CastPermanent(tap: true))
+        .ManaAbility(p =>
+        {
+          p.Text = "{T}: Add {R} or {G} to your mana pool.";
+          p.ManaAmount(Mana.Colored(isGreen: true, isRed: true));
+        })
+        .TriggeredAbility(p =>
+        {
+          p.Text = "When Rugged Highlands enters the battlefield, you gain 1 life.";
+          p.Trigger(new OnZoneChanged(to: Zone.Battlefield));
+          p.Effect = () => new ChangeLife(1, yours: true);
+        });
+    }
+  }
+}
