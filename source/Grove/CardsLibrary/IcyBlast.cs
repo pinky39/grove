677f607a3c5992074298ae73d3540619b55d140e
Sandmann123 <norfirill@gmail.com>
2015-01-14 23:03:23 +0700

    Added new card: Icy Blast

---
+++ source/Grove/CardsLibrary/IcyBlast.cs
@@ -0,0 +1,55 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.CostRules;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+  using Modifiers;
+
+  public class IcyBlast : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Icy Blast")
+        .ManaCost("{U}").HasXInCost()
+        .Type("Instant")
+        .Text("Tap X target creatures.{EOL}{I}Ferocious{/I} — If you control a creature with power 4 or greater, those creatures don't untap during their controllers' next untap steps.")
+        .FlavorText("\"Do not think the sand or the sun will hold back the breath of winter.\"")
+        .Cast(p =>
+        {
+          p.Effect = () => new FerociousEffect(
+            normalEffects: new Effect[]
+            {
+              new TapTargets(), 
+            },
+            ferociousEffects: new Effect[]
+            {
+              new TapTargets(),
+              new ApplyModifiersToTargets(() =>
+                {
+                  var modifier = new AddStaticAbility(Static.DoesNotUntap);
+
+                  modifier.AddLifetime(new EndOfStep(
+                    Step.Untap,
+                    l => l.Modifier.SourceCard.Controller.IsActive));
+
+                  return modifier;
+                }),
+            });
+
+          p.TargetSelector.AddEffect(trg =>
+          {
+            trg.MinCount = Value.PlusX;
+            trg.MaxCount = Value.PlusX;
+            trg.Is.Creature().On.Battlefield();
+          });
+
+          p.TimingRule(new OnStep(Step.BeginningOfCombat));
+          p.CostRule(new XIsOpponentsCreatureCount());
+          p.TargetingRule(new EffectTapCreature());
+        });
+    }
+  }
+}
---
+++ source/Grove/Core/AI/CostRules/XIsOpponentsCreatureCount.cs
@@ -0,0 +1,12 @@
+﻿namespace Grove.AI.CostRules
+{
+  using System.Linq;
+
+  public class XIsOpponentsCreatureCount : CostRule
+  {
+    public override int CalculateX(CostRuleParameters p)
+    {
+      return p.Controller.Opponent.Battlefield.Creatures.Count();
+    }
+  }
+}
