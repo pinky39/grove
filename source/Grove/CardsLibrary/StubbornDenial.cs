6f20eb9a41473c1a3c483b98668fa7a9f2faf9b9
Sandmann123 <norfirill@gmail.com>
2015-01-14 11:29:37 +0700

    Added new card: Stubborn Denial

---
+++ source/Grove/CardsLibrary/StubbornDenial.cs
@@ -0,0 +1,44 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using System.Linq;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+
+  public class StubbornDenial : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Stubborn Denial")
+        .ManaCost("{U}")
+        .Type("Instant")
+        .Text("Counter target noncreature spell unless its controller pays {1}.{EOL}{I}Ferocious{/I} — If you control a creature with power 4 or greater, counter that spell instead.")
+        .FlavorText("The Temur have no patience for subtlety.")
+        .Cast(p =>
+        {
+          p.Condition = (card, game) => card.Controller.Battlefield.Creatures.All(x => x.Power < 4);
+
+          p.Effect = () => new CounterTargetSpell(doNotCounterCost: 1);
+          p.TargetSelector.AddEffect(trg => trg
+            .Is.CounterableSpell(e => !e.Source.OwningCard.Is().Creature)
+            .On.Stack());
+          p.TimingRule(new WhenTopSpellIsCounterable(1));
+          p.TargetingRule(new EffectCounterspell());
+        })
+        .Cast(p =>
+        {
+          p.Condition = (card, game) => card.Controller.Battlefield.Creatures.Any(x => x.Power >= 4);
+
+          p.Effect = () => new CounterTargetSpell();
+          p.TargetSelector.AddEffect(t => t
+            .Is.CounterableSpell(e => !e.Source.OwningCard.Is().Creature)
+            .On.Stack());
+
+          p.TargetingRule(new EffectCounterspell());
+          p.TimingRule(new WhenTopSpellIsCounterable());
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/StubbornDenial.cs
@@ -0,0 +1,27 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class StubbornDenial
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void CounterAxe()
+      {
+        Hand(P1, "Lava Axe");
+        Battlefield(P1, "Mountain", "Mountain", "Mountain", "Mountain", "Mountain");
+
+        P2.Life = 5;
+        Hand(P2, "Stubborn Denial");
+        Battlefield(P2, "Island", "Shivan Dragon");
+
+        RunGame(1);
+
+        Equal(1, P1.Graveyard.Count);
+        Equal(5, P2.Life);
+      }
+    }
+  }
+}
