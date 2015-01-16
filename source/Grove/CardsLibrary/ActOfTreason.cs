e2dad2878cf57768e961fedd4e7ff6fea4db56dd
Sandmann123 <norfirill@gmail.com>
2015-01-16 14:59:55 +0700

    Added new card: Act of Treason

---
+++ source/Grove/CardsLibrary/ActOfTreason.cs
@@ -0,0 +1,33 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+  using Modifiers;
+
+  public class ActOfTreason : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Act of Treason")
+        .ManaCost("{2}{R}")
+        .Type("Sorcery")
+        .Text("Gain control of target creature until end of turn. Untap that creature. It gains haste until end of turn.")
+        .FlavorText("\"The Sultai take our dead, so we shall take their living!\"{EOL}—Taklai, Mardu ragesinger")
+        .Cast(p =>
+        {
+          p.Effect = () => new CompoundEffect(
+            new ApplyModifiersToTargets(
+              () => new ChangeController(m => m.SourceCard.Controller) { UntilEot = true },
+              () => new AddStaticAbility(Static.Haste) { UntilEot = true }),
+            new UntapTargetPermanents());
+
+          p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
+          p.TimingRule(new OnFirstMain());
+          p.TargetingRule(new EffectGainControl());
+        });
+    }
+  }
+}
