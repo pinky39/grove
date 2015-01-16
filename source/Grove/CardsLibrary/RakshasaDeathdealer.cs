b290e332fd55b554b9d8dcc90a288b21984d326e
Sandmann123 <norfirill@gmail.com>
2015-01-16 16:17:52 +0700

    Added new card: Rakshasa Deathdealer

---
+++ source/Grove/CardsLibrary/RakshasaDeathdealer.cs
@@ -0,0 +1,25 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class RakshasaDeathdealer : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Rakshasa Deathdealer")
+        .ManaCost("{B}{G}")
+        .Type("Creature - Demon Cat")
+        .Text("{B}{G}: Rakshasa Deathdealer gets +2/+2 until end of turn.{EOL}{B}{G}: Regenerate Rakshasa Deathdealer.")
+        .FlavorText("\"Death fills me and makes me strong. You, it will reduce to nothing.\"")
+        .Power(2)
+        .Toughness(2)
+        .Pump(
+          cost: "{B}{G}".Parse(),
+          text: "{B}{G}: Rakshasa Deathdealer gets +2/+2 until end of turn.",
+          powerIncrease: 2,
+          toughnessIncrease: 2)
+        .Regenerate(cost: "{B}{G}".Parse(), text: "{B}{G}: Regenerate Rakshasa Deathdealer.");
+    }
+  }
+}
