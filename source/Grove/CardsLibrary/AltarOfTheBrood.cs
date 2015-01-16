5242ab3c05bd4e73f6c7298130d85ff92e9330d2
Sandmann123 <norfirill@gmail.com>
2015-01-16 15:12:31 +0700

    Added new card: Altar of the Brood

---
+++ source/Grove/CardsLibrary/AltarOfTheBrood.cs
@@ -0,0 +1,35 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+  using Triggers;
+
+  public class AltarOfTheBrood : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Altar of the Brood")
+        .ManaCost("{1}")
+        .Type("Artifact")
+        .Text("Whenever another permanent enters the battlefield under your control, each opponent puts the top card of his or her library into his or her graveyard.")
+        .FlavorText("Supplicants offer flesh and silver, flowers and blood. The altar takes what it will, eyes gleaming with unspoken promises.")
+        .TriggeredAbility(p =>
+        {
+          p.Text = "Whenever another permanent enters the battlefield under your control, each opponent puts the top card of his or her library into his or her graveyard.";
+
+          p.Trigger(new OnZoneChanged(
+              to: Zone.Battlefield,
+              filter: (card, ability, _) =>
+              {
+                if (ability.OwningCard == card)
+                  return false;
+
+                return ability.OwningCard.Controller == card.Controller;
+              }));
+
+          p.Effect = () => new PlayerPutsTopCardsFromLibraryToGraveyard(P(e => e.Controller.Opponent), count: 1);
+        });
+    }
+  }
+}
