503f72cb8b85b2f76d83a49587d3842efde940c4
Sandmann123 <norfirill@gmail.com>
2015-01-16 16:05:29 +0700

    Added new card: Anafenza, the Foremost

---
+++ source/Grove/CardsLibrary/AnafenzaTheForemost.cs
@@ -0,0 +1,49 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using Effects;
+  using Events;
+  using Modifiers;
+  using Triggers;
+
+  public class AnafenzaTheForemost : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Anafenza, the Foremost")
+        .ManaCost("{W}{B}{G}")
+        .Type("Legendary Creature — Human Soldier")
+        .Text("Whenever Anafenza, the Foremost attacks, put a +1/+1 counter on another target tapped creature you control.{EOL}If a creature card would be put into an opponent's graveyard from anywhere, exile it instead.")
+        .FlavorText("Rarely at rest on the Amber Throne, Anafenza always leads the Abzan Houses to battle.")
+        .Power(4)
+        .Toughness(4)
+        .TriggeredAbility(p =>
+        {
+          p.Text = "Whenever Anafenza, the Foremost attacks, put a +1/+1 counter on another target tapped creature you control.";
+          p.Trigger(new WhenThisAttacks());
+          p.Effect = () => new ApplyModifiersToTargets(() => new AddCounters(() => new PowerToughness(1, 1), count: 1));
+
+          p.TargetSelector.AddEffect(trg =>
+          {
+            trg.MinCount = 1;
+            trg.MaxCount = 1;
+            trg.Message = "Select another target tapped creature you control";
+            trg.Is.Creature(controlledBy: ControlledBy.SpellOwner,
+              canTargetSelf: false).Is.Card(c => c.IsTapped).On.Battlefield();
+          });
+
+          p.TargetingRule(new EffectCombatEnchantment());
+        })
+        .TriggeredAbility(p =>
+        {
+          p.Text = "If a creature card would be put into an opponent's graveyard from anywhere, exile it instead.";
+          p.Trigger(new OnZoneChanged(
+            to: Zone.Graveyard,
+            filter: (card, ability, _) => ability.OwningCard.Controller != card.Controller));
+          p.Effect = () => new ExileCard(P(e => e.TriggerMessage<ZoneChangedEvent>().Card), Zone.Graveyard);
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/AnafenzaTheForemost.cs
@@ -0,0 +1,27 @@
+﻿namespace Grove.Tests.Cards
+{
+  using System.Linq;
+  using Infrastructure;
+  using Xunit;
+
+  public class AnafenzaTheForemost
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void PutCounteOnDragonAndExileBear()
+      {
+        var bear = C("Grizzly Bears");
+        Battlefield(P1, "Shivan Dragon", "Anafenza, The Foremost");
+
+        P2.Life = 7;
+        Battlefield(P2, bear);
+
+        RunGame(1);
+
+        Equal(1, P2.Life);
+        Equal(Zone.Exile, C(bear).Zone);
+      }
+    }
+  }
+}
