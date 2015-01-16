e3f968e83bd94d24818ffd4ec5a8597e098fb1e8
Sandmann123 <norfirill@gmail.com>
2015-01-16 17:01:40 +0700

    Added new card: Windswept Heath

---
+++ source/Grove/CardsLibrary/WindsweptHeath.cs
@@ -0,0 +1,37 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TimingRules;
+  using Costs;
+  using Effects;
+
+  public class WindsweptHeath : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Windswept Heath")
+        .Type("Land")
+        .Text("{T}, Pay 1 life, Sacrifice Windswept Heath: Search your library for a Forest or Plains card and put it onto the battlefield. Then shuffle your library.")
+        .FlavorText("Where dragons once roared, their bones now keen.")
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{T}, Pay 1 life, Sacrifice Windswept Heath: Search your library for a Forest or Plains card and put it onto the battlefield. Then shuffle your library.";
+
+          p.Cost = new AggregateCost(
+            new Tap(),
+            new PayLife(1),
+            new Sacrifice());
+
+          p.Effect = () => new SearchLibraryPutToZone(
+              zone: Zone.Battlefield,
+              minCount: 0,
+              maxCount: 1,
+              validator: (e, c) => c.Is("forest") || c.Is("plains"),
+              text: "Search your library for a Forest or Plains card.");
+
+          p.TimingRule(new OnFirstMain());
+        });
+    }
+  }
+}
