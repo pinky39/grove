c36899710afd2f0e62d782b8f89c4138abb99e27
Sandmann123 <norfirill@gmail.com>
2015-01-24 14:10:42 +0700

    Added new card: Chief of the Edge

---
+++ source/Grove/CardsLibrary/ChiefOfTheEdge.cs
@@ -0,0 +1,25 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Modifiers;
+
+  public class ChiefOfTheEdge : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Chief of the Edge")
+        .ManaCost("{W}{B}")
+        .Type("Creature - Human Warrior")
+        .Text("Other Warrior creatures you control get +1/+0.")
+        .FlavorText("\"We are the swift, the strong, the blade's sharp shriek! Fear nothing, and strike!\"")
+        .Power(3)
+        .Toughness(2)
+        .ContinuousEffect(p =>
+        {
+          p.Modifier = () => new AddPowerAndToughness(1, 0);
+          p.CardFilter = (c, e) => c.Controller == e.Source.Controller && c.Is().Creature && c != e.Source;
+        });
+    }
+  }
+}
