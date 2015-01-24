f026ec1f5dfbc00a86a7f15f0ae14bba6ffd7e81
Sandmann123 <norfirill@gmail.com>
2015-01-24 14:29:22 +0700

    Added new card: Abzan Skycaptain

---
+++ source/Grove/CardsLibrary/AbzanSkycaptain.cs
@@ -0,0 +1,45 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using System.Linq;
+  using AI;
+  using AI.TargetingRules;
+  using Effects;
+  using Modifiers;
+  using Triggers;
+
+  public class AbzanSkycaptain : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Abzan Skycaptain")
+        .ManaCost("{3}{W}")
+        .Type("Creature - Bird Soldier")
+        .Text(
+          "{Flying}{EOL}When Abzan Skycaptain dies, bolster 2.{I}(Choose a creature with the least toughness among creatures you control and put two +1/+1 counters on it.){/I}")
+        .FlavorText("\"A tempest is coming. Turn your faces toward the wind!\"")
+        .Power(2)
+        .Toughness(2)
+        .SimpleAbilities(Static.Flying)
+        .ContinuousEffect(p =>
+        {
+          p.Modifier = () => new AddPowerAndToughness(0, 1);
+          p.CardFilter =
+            (c, e) => c.Is("warrior") && c.Controller == e.Source.Controller && c.Is().Creature && c != e.Source;
+        })
+        .TriggeredAbility(p =>
+        {
+          p.Text = "When Abzan Skycaptain dies, bolster 2.";
+          p.Trigger(new OnZoneChanged(
+            from:Zone.Battlefield,
+            to:Zone.Graveyard));
+          p.Effect = () => new ApplyModifiersToTargets(() => new AddCounters(
+            () => new PowerToughness(1, 1), count: 2)).SetTags(EffectTag.IncreasePower, EffectTag.IncreaseToughness);
+          p.TargetSelector.AddEffect(
+            trg => trg.Is.Card(c => c.Is().Creature && c.Controller.Battlefield.Creatures.All(x => x.Toughness >= c.Toughness), ControlledBy.SpellOwner).On.Battlefield());
+          p.TargetingRule(new EffectOrCostRankBy(rank: c => -c.Score, controlledBy: ControlledBy.SpellOwner));
+        });
+    }
+  }
+}
