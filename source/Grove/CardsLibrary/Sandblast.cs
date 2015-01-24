999247596030e9a92a88bab5c826f45197cd52f7
Sandmann123 <norfirill@gmail.com>
2015-01-24 21:26:25 +0700

    Added new card: Sandblast

---
+++ source/Grove/CardsLibrary/Sandblast.cs
@@ -0,0 +1,32 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+
+  public class Sandblast : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Sandblast")
+        .ManaCost("{2}{W}")
+        .Type("Instant")
+        .Text("Sandblast deals 5 damage to target attacking or blocking creature.")
+        .FlavorText("In the Shifting Wastes, more are killed by sand than by steel.")
+        .Cast(p =>
+        {
+          p.Effect = () => new DealDamageToTargets(5);
+
+          p.TargetSelector.AddEffect(trg => trg.Is
+            .Card(c => c.Is().Creature && (c.IsAttacker || c.IsBlocker))
+            .On.Battlefield());
+
+
+          p.TimingRule(new OnStep(Step.DeclareBlockers));
+          p.TargetingRule(new EffectDealDamage(5));
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/Sandblast.cs
@@ -0,0 +1,26 @@
+﻿namespace Grove.Tests.Cards
+{
+  using System.Linq;
+  using Infrastructure;
+  using Xunit;
+
+  public class Sandblast
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void DestroyDragon()
+      {
+        Battlefield(P1, "Shivan Dragon");
+
+        P2.Life = 5;
+        Hand(P2, "Sandblast");
+        Battlefield(P2, "Forest", "Forest", "Plains");
+
+        RunGame(1);
+
+        Equal(0, P1.Battlefield.Count);
+      }
+    }
+  }
+}
