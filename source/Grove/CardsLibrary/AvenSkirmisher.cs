e077813d14fcc99057d5b4f2934fa4d81d5c2e4f
Sandmann123 <norfirill@gmail.com>
2015-01-24 14:05:27 +0700

    Added new card: Aven Skirmisher

---
+++ source/Grove/CardsLibrary/AvenSkirmisher.cs
@@ -0,0 +1,20 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class AvenSkirmisher : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Aven Skirmisher")
+        .ManaCost("{W}")
+        .Type("Creature - Bird Warrior")
+        .Text("{Flying}")
+        .FlavorText("\"We do not hide from the dragons that pretend to rule the skies. If we did, the dragons would become our rulers, and our way of life would be lost.\"")
+        .Power(1)
+        .Toughness(1)
+        .SimpleAbilities(Static.Flying);
+    }
+  }
+}
