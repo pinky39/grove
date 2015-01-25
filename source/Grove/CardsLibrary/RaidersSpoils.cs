dc9c651ee4b398a7549176c14659bc3cfc7104c6
Sandmann123 <norfirill@gmail.com>
2015-01-25 15:07:10 +0700

    Added new card: Raiders' Spoils

---
+++ source/Grove/CardsLibrary/RaidersSpoils.cs
@@ -0,0 +1,45 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI;
+  using AI.TimingRules;
+  using Effects;
+  using Modifiers;
+  using Triggers;
+
+  public class RaidersSpoils : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Raiders' Spoils")
+        .ManaCost("{3}{B}")
+        .Type("Enchantment")
+        .Text("Creatures you control get +1/+0.{EOL}Whenever a Warrior you control deals combat damage to a player, you may pay 1 life. If you do, draw a card.")
+        .FlavorText("\"To conquer is to eat.\"{EOL}—Edicts of Ilagra")
+        .Cast(p =>
+        {
+          p.TimingRule(new OnFirstMain());
+          p.Effect = () => new CastPermanent().SetTags(EffectTag.IncreasePower);
+        })
+        .ContinuousEffect(p =>
+        {
+          p.Modifier = () => new AddPowerAndToughness(1, 0);
+          p.CardFilter = (card, effect) => card.Controller == effect.Source.Controller && card.Is().Creature;
+        })
+        .TriggeredAbility(p =>
+        {
+          p.Text = "Whenever a Warrior you control deals combat damage to a player, you may pay 1 life. If you do, draw a card.";
+
+          p.Trigger(new OnDamageDealt(dmg =>
+            dmg.IsDealtToPlayer &&
+            dmg.IsCombat &&
+            dmg.Source.Is("warrior")));
+
+          p.Effect = () => new PayLifeToDrawCard(1);
+
+          p.TriggerOnlyIfOwningCardIsInPlay = true;
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/RaidersSpoils.cs
@@ -0,0 +1,25 @@
+﻿namespace Grove.Tests.Cards
+{
+  using System.Linq;
+  using Infrastructure;
+  using Xunit;
+
+  public class RaidersSpoils
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void DrawCardForCombatDamage()
+      {
+        Hand(P1, "Raiders' Spoils");
+        Battlefield(P1, "Swamp","Swamp","Swamp","Swamp", "Grizzly Bears", "Aven Skirmisher");
+
+        RunGame(1);
+
+        Equal(15, P2.Life);
+        Equal(19, P1.Life);
+        Equal(1, P1.Hand.Count);
+      }
+    }
+  }
+}
