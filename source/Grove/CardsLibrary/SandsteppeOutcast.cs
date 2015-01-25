7a6e7a10ce0355f79592092d714bd02fe8185d3c
Sandmann123 <norfirill@gmail.com>
2015-01-25 23:00:51 +0700

    Added new card: Sandsteppe Outcast

---
+++ source/Grove/CardsLibrary/SandsteppeOutcast.cs
@@ -0,0 +1,44 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+  using Modifiers;
+  using Triggers;
+
+  public class SandsteppeOutcast : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Sandsteppe Outcast")
+        .ManaCost("{2}{W}")
+        .Type("Creature — Human Warrior")
+        .Text("When Sandsteppe Outcast enters the battlefield, choose one —{EOL}• Put a +1/+1 counter on Sandsteppe Outcast.{EOL}• Put a 1/1 white Spirit creature token with flying onto the battlefield.")
+        .Power(2)
+        .Toughness(1)
+        .TriggeredAbility(p =>
+        {
+          p.Text = "When Sandsteppe Outcast enters the battlefield, choose one —{EOL}• Put a +1/+1 counter on Sandsteppe Outcast.{EOL}• Put a 1/1 white Spirit creature token with flying onto the battlefield.";
+          p.Trigger(new OnZoneChanged(to:Zone.Battlefield));
+          p.Effect = () => new ChooseOneEffectFromGiven(
+            message: "Press 'Yes' to put a +1/+1 counter on Sandsteppe Outcast. Press 'No' to put a creature token.",
+            ifTrueEffect: new ApplyModifiersToSelf(() => new AddCounters(() => new PowerToughness(1, 1), count: 1)),
+            ifFalseEffect: new CreateTokens(
+              count: 1,
+              token: Card
+                .Named("Spirit")
+                .Power(1)
+                .Toughness(1)
+                .Type("Token Creature - Spirit")
+                .Text("{Flying}")
+                .Colors(CardColor.White)
+                .SimpleAbilities(Static.Flying)), 
+            chooseAi: e =>
+            {
+              // TODO: Add tweaks for choosing first effect
+              return false; // AI prefers to put token instead adding a counter. Always.
+            });
+        });
+    }
+  }
+}
---
+++ source/Grove/Core/Effects/ChooseOneEffectFromGiven.cs
@@ -0,0 +1,63 @@
+﻿namespace Grove.Effects
+{
+  using System;
+  using Decisions;
+  using Modifiers;
+
+  public class ChooseOneEffectFromGiven : Effect, IChooseDecisionResults<BooleanResult>,
+    IProcessDecisionResults<BooleanResult>
+  {
+    private readonly Effect _ifTrueEffect;
+    private readonly Effect _ifFalseEffect;
+    private readonly Func<Effect, bool> _chooseAi;
+    private readonly string _message;
+
+    private ChooseOneEffectFromGiven() { }
+
+    public ChooseOneEffectFromGiven(string message, Effect ifTrueEffect, Effect ifFalseEffect, Func<Effect, bool> chooseAi)
+    {
+      _chooseAi = chooseAi;
+      _ifTrueEffect = ifTrueEffect;
+      _ifFalseEffect = ifFalseEffect;
+      _message = message;
+    }
+
+    public override Effect Initialize(EffectParameters p, Game game, bool evaluateParameters = true)
+    {
+      base.Initialize(p, game);
+
+      _ifTrueEffect.Initialize(p, game, evaluateParameters);
+      _ifFalseEffect.Initialize(p, game, evaluateParameters);
+
+      return this;
+    }
+
+    public override void SetTriggeredAbilityTargets(Targets targets)
+    {
+      _ifTrueEffect.SetTriggeredAbilityTargets(targets);
+      _ifFalseEffect.SetTriggeredAbilityTargets(targets);
+    }
+
+    protected override void ResolveEffect()
+    {
+      Enqueue(new ChooseTo(Controller, p =>
+      {
+        p.Text = _message;
+        p.ProcessDecisionResults = this;
+        p.ChooseDecisionResults = this;
+      }));
+    }
+
+    public BooleanResult ChooseResult()
+    {
+      return _chooseAi(this);
+    }
+
+    public void ProcessResults(BooleanResult results)
+    {
+      var effect = results.IsTrue ? _ifTrueEffect : _ifFalseEffect;
+
+      effect.BeginResolve();
+    }
+  }
+}
