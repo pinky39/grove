d1d64e81f71c7db127d5798fee5c930d6ab32e6e
Sandmann123 <norfirill@gmail.com>
2015-01-28 15:20:39 +0700

    Added new card: Whisk Away

---
+++ source/Grove/CardsLibrary/WhiskAway.cs
@@ -0,0 +1,31 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI;
+  using AI.TargetingRules;
+  using AI.TimingRules;
+  using Effects;
+
+  public class WhiskAway : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Whisk Away")
+        .ManaCost("{2}{U}")
+        .Type("Instant")
+        .Text("Put target attacking or blocking creature on top of its owner's library.")
+        .FlavorText("The maneuvers of the Abzan formation intrigued Ojutai in both their complexity and their futility.")
+        .Cast(p =>
+        {
+          p.Effect = () => new PutTargetsOnTopOfLibrary();
+
+          p.TargetSelector.AddEffect(trg =>
+            trg.Is.Card(c => c.IsAttacker || c.IsBlocker).On.Battlefield());
+
+          p.TargetingRule(new EffectPutOnTopOfLibrary());
+          p.TimingRule(new TargetRemovalTimingRule().RemovalTags(EffectTag.Bounce, EffectTag.CreaturesOnly));
+        });
+    }
+  }
+}
