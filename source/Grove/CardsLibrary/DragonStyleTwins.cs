f462ee2b7bf2ffbccf9ce08e7626b955794a3fa7
Sandmann123 <norfirill@gmail.com>
2015-01-28 15:06:07 +0700

    Added new cards: Goblinslide; Dragon-Style Twins

---
+++ source/Grove/CardsLibrary/DragonStyleTwins.cs
@@ -0,0 +1,21 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class DragonStyleTwins : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Dragon-Style Twins")
+        .ManaCost("{3}{R}{R}")
+        .Type("Creature — Human Monk")
+        .Text("{Double strike}{EOL}{Prowess} {I}(Whenever you cast a noncreature spell, this creature gets +1/+1 until end of turn.){/I}")
+        .FlavorText("\"We are the flicker of the flame and its heat, the two sides of a single blade.\"")
+        .Power(3)
+        .Toughness(3)
+        .Prowess()
+        .SimpleAbilities(Static.DoubleStrike);
+    }
+  }
+}
---
+++ source/Grove/CardsLibrary/Goblinslide.cs
@@ -0,0 +1,40 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TimingRules;
+  using Effects;
+  using Triggers;
+
+  public class Goblinslide : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Goblinslide")
+        .ManaCost("{2}{R}")
+        .Type("Enchantment")
+        .Text("Whenever you cast a noncreature spell, you may pay {1}. If you do, put a 1/1 red Goblin creature token with haste onto the battlefield.")
+        .FlavorText("Goblins, like snowflakes, are only dangerous in numbers.")
+        .Cast(p => p.TimingRule(new OnFirstMain()))
+        .TriggeredAbility(p =>
+        {
+          p.Text = "Whenever you cast a noncreature spell, you may pay {1}. If you do, put a 1/1 red Goblin creature token with haste onto the battlefield.";
+          p.Trigger(new OnCastedSpell((a, c) =>
+            c.Controller == a.OwningCard.Controller && !c.Is().Creature));
+
+          p.Effect = () => new PayManaCreateTokens(1.Colorless(), new CreateTokens(
+            count: 1,
+            token: Card
+              .Named("Goblin")
+              .Power(1)
+              .Toughness(1)
+              .Type("Token Creature - Goblin")
+              .Text("{Haste}")
+              .Colors(CardColor.Red)
+              .SimpleAbilities(Static.Haste)));
+
+          p.TriggerOnlyIfOwningCardIsInPlay = true;
+        });
+    }
+  }
+}
