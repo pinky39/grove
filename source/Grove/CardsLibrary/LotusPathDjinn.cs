becc75a0b871d749d9db2e78301dc13b3805185b
Sandmann123 <norfirill@gmail.com>
2015-01-28 15:59:41 +0700

    Added new card: Lotus Path Djinn

---
+++ source/Grove/CardsLibrary/LotusPathDjinn.cs
@@ -0,0 +1,21 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class LotusPathDjinn : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Lotus Path Djinn")
+        .ManaCost("{3}{U}")
+        .Type("Creature — Djinn Monk")
+        .Text("{Flying}{EOL}{Prowess} {I}(Whenever you cast a noncreature spell, this creature gets +1/+1 until end of turn.){/I}")
+        .FlavorText("\"The lotus takes root where body and mind intersect. It blooms when body and mind become one.\"")
+        .Power(2)
+        .Toughness(3)
+        .Prowess()
+        .SimpleAbilities(Static.Flying);
+    }
+  }
+}
