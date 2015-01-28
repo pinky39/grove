f0850393c03234f1aa21f995311c3ea5d6b1a462
Sandmann123 <norfirill@gmail.com>
2015-01-28 15:56:22 +0700

    Added new card: Mistfire Adept

---
+++ source/Grove/CardsLibrary/MistfireAdept.cs
@@ -0,0 +1,37 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using Effects;
+  using Modifiers;
+  using Triggers;
+
+  public class MistfireAdept : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Mistfire Adept")
+        .ManaCost("{3}{U}")
+        .Type("Creature — Human Monk")
+        .Text("{Prowess} {I}(Whenever you cast a noncreature spell, this creature gets +1/+1 until end of turn.){/I}{EOL}Whenever you cast a noncreature spell, target creature gains flying until end of turn.")
+        .FlavorText("\"I don't know where my destiny lies, but I know it isn't here.\"")
+        .Power(2)
+        .Toughness(2)
+        .Prowess()
+        .TriggeredAbility(p =>
+        {
+          p.Text = "Whenever you cast a noncreature spell, target creature gains flying until end of turn.";
+          p.Trigger(new OnCastedSpell((a, c) =>
+            c.Controller == a.OwningCard.Controller && !c.Is().Creature));
+
+          p.Effect = () => new ApplyModifiersToTargets(() => new AddStaticAbility(Static.Flying) { UntilEot = true });
+
+          p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
+          p.TargetingRule(new EffectBigWithoutEvasions(x => !x.Has().Flying));
+
+          p.TriggerOnlyIfOwningCardIsInPlay = true;
+        });
+    }
+  }
+}
