aceb7d06d15209d0400dbaeb4e78d6d431a0dc04
Sandmann123 <norfirill@gmail.com>
2015-01-29 16:12:37 +0700

    Added new card: Jeskai Sage

---
+++ source/Grove/CardsLibrary/JeskaiSage.cs
@@ -0,0 +1,28 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+  using Triggers;
+
+  public class JeskaiSage : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Jeskai Sage")
+        .ManaCost("{1}{U}")
+        .Type("Creature — Human Monk")
+        .Text("{Prowess} {I}(Whenever you cast a noncreature spell, this creature gets +1/+1 until end of turn.){/I}{EOL}When Jeskai Sage dies, draw a card.")
+        .FlavorText("\"The one who conquers the mind is greater than the one who conquers the world.\"")
+        .Power(1)
+        .Toughness(1)
+        .Prowess()
+        .TriggeredAbility(p =>
+        {
+          p.Text = "When Jeskai Sage dies, draw a card.";
+          p.Trigger(new OnZoneChanged(from: Zone.Battlefield, to: Zone.Graveyard));
+          p.Effect = () => new DrawCards(1);
+        });
+    }
+  }
+}
