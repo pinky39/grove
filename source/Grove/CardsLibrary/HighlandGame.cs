803c974d767d612bf0934c918a4f3c38dc5b27cc
Sandmann123 <norfirill@gmail.com>
2015-01-29 17:23:53 +0700

    Added new card: Highland Game

---
+++ source/Grove/CardsLibrary/HighlandGame.cs
@@ -0,0 +1,27 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+  using Triggers;
+
+  public class HighlandGame : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+          .Named("Highland Game")
+          .ManaCost("{1}{G}")
+          .Type("Creature — Elk")
+          .Text("When Highland Game dies, you gain 2 life.")
+          .FlavorText("\"Bring down a stag and fix its horns upon her head. This one hears the whispers.\"{EOL}—Chianul, at the weaving of Arel")
+          .Power(2)
+          .Toughness(1)
+          .TriggeredAbility(p =>
+          {
+            p.Text = "When Highland Game dies, you gain 2 life.";
+            p.Trigger(new OnZoneChanged(from: Zone.Battlefield, to: Zone.Graveyard));
+            p.Effect = () => new ChangeLife(2, yours: true);
+          });
+    }
+  }
+}
