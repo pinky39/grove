654e03c4246d0aef6d2d8b4a5398e96648e2efd5
Sandmann123 <norfirill@gmail.com>
2015-01-30 14:49:07 +0700

    Added new card: Sibsig Host

---
+++ source/Grove/CardsLibrary/SibsigHost.cs
@@ -0,0 +1,27 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+  using Triggers;
+
+  public class SibsigHost : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Sibsig Host")
+        .ManaCost("{4}{B}")
+        .Type("Creature - Zombie")
+        .Text("When Sibsig Host enters the battlefield, each player puts the top three cards of his or her library into his or her graveyard.")
+        .FlavorText("\"They were your friends, your family, your clan. They want only to welcome you.\"{EOL}—Tasigur, the Golden Fang")
+        .Power(2)
+        .Toughness(6)
+        .TriggeredAbility(p =>
+        {
+          p.Text = "When Sibsig Host enters the battlefield, each player puts the top three cards of his or her library into his or her graveyard.";
+          p.Trigger(new OnZoneChanged(to: Zone.Battlefield));
+          p.Effect = () => new EachPlayerPutTopCardsFromLibraryToGraveyard(3);
+        });
+    }
+  }
+}
