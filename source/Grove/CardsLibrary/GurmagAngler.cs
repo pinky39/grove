ed9945061db5949942831149049e090b515b2255
Sandmann123 <norfirill@gmail.com>
2015-01-30 14:54:06 +0700

    Added new card: Gurmag Angler

---
+++ source/Grove/CardsLibrary/GurmagAngler.cs
@@ -0,0 +1,20 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class GurmagAngler : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+          .Named("Gurmag Angler")
+          .ManaCost("{6}{B}")
+          .Type("Creature — Zombie Fish")
+          .Text("{Delve}{I}(Each card you exile from your graveyard while casting this spell pays for {1}.){/I}")
+          .FlavorText("If everything in the Gurmag Swamp hungers for human flesh, what bait could be more effective?")
+          .Power(5)
+          .Toughness(5)
+          .SimpleAbilities(Static.Delve);
+    }
+  }
+}
