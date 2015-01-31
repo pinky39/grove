19d388e621a515bcca4af7e5d17f557c846dd0be
Sandmann123 <norfirill@gmail.com>
2015-01-31 22:48:13 +0700

    Added new card: Abzan Beastmaster

---
+++ source/Grove/CardsLibrary/AbzanBeastmaster.cs
@@ -0,0 +1,37 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using System.Linq;
+  using Effects;
+  using Triggers;
+
+  public class AbzanBeastmaster : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Abzan Beastmaster")
+        .ManaCost("{2}{G}")
+        .Type("Creature - Hound Shaman")
+        .Text("At the beginning of your upkeep, draw a card if you control the creature with the greatest toughness or tied for the greatest toughness.")
+        .FlavorText("His beasts move the great siege towers of the Abzan across the endless sands.")
+        .Power(2)
+        .Toughness(1)
+        .TriggeredAbility(p =>
+        {
+          p.Text = "At the beginning of your upkeep, draw a card if you control the creature with the greatest toughness or tied for the greatest toughness.";
+          p.Trigger(new OnStepStart(Step.Upkeep)
+          {
+            Condition = (trigger, game) =>
+            {
+              var creatures = game.Players.Permanents().Where(c => c.Is().Creature);
+
+              return trigger.OwningCard.Controller.Battlefield.Creatures.Any(
+                c => creatures.All(x => x.Toughness <= c.Toughness));
+            }
+          });
+          p.Effect = () => new DrawCards(1);
+        });
+    }
+  }
+}
