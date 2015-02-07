de8a6d31d3fbc0aea668f5925d6c4c26741ccd4e
Sandmann123 <norfirill@gmail.com>
2015-02-07 16:53:43 +0700

    Added new card: War Flare

---
+++ source/Grove/CardsLibrary/WarFlare.cs
@@ -0,0 +1,30 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI;
+  using AI.TimingRules;
+  using Effects;
+  using Modifiers;
+
+  public class WarFlare : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("War Flare")
+        .ManaCost("{2}{R}{W}")
+        .Type("Instant")
+        .Text("Creatures you control get +2/+1 until end of turn. Untap those creatures.")
+        .FlavorText("\"No sun or fire can warm my blood quite like a war flare.\"{EOL}—Urut Barzeel, Mardu hordechief")
+        .Cast(p =>
+        {
+          p.Effect = () => new CompoundEffect(
+            new UntapEachPermanent(c => c.Is().Creature, controlledBy: ControlledBy.SpellOwner),
+            new ApplyModifiersToPermanents((e, c) => c.Is().Creature, ControlledBy.SpellOwner,
+                () => new AddPowerAndToughness(2, 1) { UntilEot = true }).SetTags(EffectTag.IncreasePower, EffectTag.IncreaseToughness));
+
+          p.TimingRule(new Any(new AfterOpponentDeclaresAttackers(), new BeforeYouDeclareAttackers()));
+        });
+    }
+  }
+}
