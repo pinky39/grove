db1f7fe82e86f370b1689500065303fb9c9f0e59
Sandmann123 <norfirill@gmail.com>
2015-02-14 14:26:02 +0700

    Added new card: Bellowing Saddlebrute

---
+++ source/Grove/CardsLibrary/BellowingSaddlebrute.cs
@@ -0,0 +1,30 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+  using Triggers;
+
+  public class BellowingSaddlebrute : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Bellowing Saddlebrute")
+        .ManaCost("{3}{B}")
+        .Type("Creature — Orc Warrior")
+        .Text("{I}Raid{/I} — When Bellowing Saddlebrute enters the battlefield, you lose 4 life unless you attacked with a creature this turn.")
+        .Power(4)
+        .Toughness(5)
+        .TriggeredAbility(p =>
+        {
+          p.Text = "When Bellowing Saddlebrute enters the battlefield, you lose 4 life unless you attacked with a creature this turn.";
+          p.Trigger(new OnZoneChanged(to: Zone.Battlefield)
+          {
+            Condition = (t, g) => !g.Turn.Events.HasActivePlayerAttackedThisTurn,
+          });
+
+          p.Effect = () => new ChangeLife(-4, yours: true);
+        });
+    }
+  }
+}
