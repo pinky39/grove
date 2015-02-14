03ed7bc75a8e7a66664dfa799890999d8cfe32cd
Sandmann123 <norfirill@gmail.com>
2015-02-14 14:37:44 +0700

    Added new card: Blinding Spray

---
+++ source/Grove/CardsLibrary/BlindingSpray.cs
@@ -0,0 +1,30 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TimingRules;
+  using Effects;
+  using Modifiers;
+
+  public class BlindingSpray : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Blinding Spray")
+        .ManaCost("{4}{U}")
+        .Type("Instant")
+        .Text("Creatures your opponents control get -4/-0 until end of turn.{EOL}Draw a card.")
+        .FlavorText("\"The stronger our enemies seem, the more vulnerable they are.\"{EOL}—Sultai secret")
+        .Cast(p =>
+        {
+          p.Effect = () => new CompoundEffect(
+            new ApplyModifiersToPermanents(
+              selector: (effect, card) => card.Is().Creature && card.Controller != effect.Source.OwningCard.Controller,
+              modifiers: () => new AddPowerAndToughness(-4, 0) { UntilEot = true }),
+            new DrawCards(1));
+
+          p.TimingRule(new Any(new AfterOpponentDeclaresAttackers(), new AfterOpponentDeclaresBlockers()));
+        });
+    }
+  }
+}
