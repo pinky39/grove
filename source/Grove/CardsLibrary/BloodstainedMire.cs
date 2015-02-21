51b9267b4616ad73cd694b7516f0ee332a57e666
Sandmann123 <norfirill@gmail.com>
2015-02-21 14:27:41 +0700

    Added new card: Bloodstained Mire

---
+++ source/Grove/CardsLibrary/BloodstainedMire.cs
@@ -0,0 +1,37 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TimingRules;
+  using Costs;
+  using Effects;
+
+  public class BloodstainedMire : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Bloodstained Mire")
+        .Type("Land")
+        .Text("{T}, Pay 1 life, Sacrifice Bloodstained Mire: Search your library for a Swamp or Mountain card and put it onto the battlefield. Then shuffle your library.")
+        .FlavorText("Where dragons once triumphed, their bones now molder.")
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{T}, Pay 1 life, Sacrifice Bloodstained Mire: Search your library for a Swamp or Mountain card and put it onto the battlefield. Then shuffle your library.";
+
+          p.Cost = new AggregateCost(
+            new Tap(),
+            new PayLife(1),
+            new Sacrifice());
+
+          p.Effect = () => new SearchLibraryPutToZone(
+              zone: Zone.Battlefield,
+              minCount: 0,
+              maxCount: 1,
+              validator: (e, c) => c.Is("swamp") || c.Is("mountain"),
+              text: "Search your library for a Swamp or Mountain card.");
+
+          p.TimingRule(new OnFirstMain());
+        });
+    }
+  }
+}
