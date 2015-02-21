eb25ae040a18ddd938b0db466a6764bc195a5e8a
Sandmann123 <norfirill@gmail.com>
2015-02-21 14:23:47 +0700

    Added new card: Bloodsoaked Champion

---
+++ source/Grove/CardsLibrary/BloodsoakedChampion.cs
@@ -0,0 +1,30 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Costs;
+  using Effects;
+
+  public class BloodsoakedChampion : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Bloodsoaked Champion")
+        .ManaCost("{B}")
+        .Type("Creature — Human Warrior")
+        .Text("Bloodsoaked Champion can't block.{EOL}{I}Raid{/I} — {1}{B}: Return Bloodsoaked Champion from your graveyard to the battlefield. Activate this ability only if you attacked with a creature this turn.")
+        .FlavorText("\"Death is merely another foe the Mardu will overcome.\"")
+        .Power(2)
+        .Toughness(1)
+        .ActivatedAbility(p =>
+        {
+          p.Text = "{1}{B}: Return Bloodsoaked Champion from your graveyard to the battlefield.";
+
+          p.Cost = new PayMana("{1}{B}".Parse());
+          p.Condition = (card, game) => game.Turn.Events.HasActivePlayerAttackedThisTurn;
+          p.Effect = () => new PutOwnerToBattlefield(from: Zone.Graveyard);
+          p.ActivationZone = Zone.Graveyard;
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/BloodsoakedChampion.cs
@@ -0,0 +1,24 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class BloodsoakedChampion
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void ReturnChampionToBattlefield()
+      {
+        var champion = C("Bloodsoaked Champion");
+
+        Battlefield(P1, "Grizzly Bears", "Swamp", "Mountain");
+        Graveyard(P1, champion);
+
+        RunGame(1);
+
+        Equal(Zone.Battlefield, C(champion).Zone);
+      }
+    }
+  }
+}
