56b784f9897290b7624dd5420f133ddc5da800de
Sandmann123 <norfirill@gmail.com>
2015-03-03 21:06:21 +0700

    Added new card: Path to Exile

---
+++ source/Grove/CardsLibrary/PathToExile.cs
@@ -0,0 +1,34 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using AI.TargetingRules;
+  using Effects;
+
+  public class PathToExile : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Path to Exile")
+        .ManaCost("{W}")
+        .Type("Instant")
+        .Text("Exile target creature. Its controller may search his or her library for a basic land card, put that card onto the battlefield tapped, then shuffle his or her library.")
+        .Cast(p =>
+        {
+          p.Effect = () => new CompoundEffect(
+            new ExileTargets(),
+            new SearchLibraryPutToZone(
+              zone: Zone.Battlefield,
+              afterPutToZone: (c, g) => c.Tap(),
+              minCount: 0,
+              maxCount: 1,
+              validator: (e, c) => c.Is().BasicLand,
+              text: "Search your library for a basic land card.",
+              player: P(e => e.Target.Controller())));
+
+          p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
+          p.TargetingRule(new EffectExileBattlefield());
+        });
+    }
+  }
+}
---
+++ tests/Grove.Tests/Cards/PathToExile.cs
@@ -0,0 +1,30 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class PathToExile
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void ExileDragonItsControllerSearchesLand()
+      {
+        var land = C("Forest");
+        var dragon = C("Shivan Dragon");
+
+        Library(P1, land);
+        Battlefield(P1, dragon);
+
+        P2.Life = 5;
+        Hand(P2, "Path To Exile");
+        Battlefield(P2, "Plains");
+
+        RunGame(1);
+
+        Equal(Zone.Exile, C(dragon).Zone);
+        Equal(Zone.Battlefield, C(land).Zone);
+      }
+    }
+  }
+}
