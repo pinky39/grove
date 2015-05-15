98c0f2570d42b3c18c98b8f31a89e62ae1e2efcc
pinky <pinky@lab.org>
2015-05-15 14:44:17 +0200

    Formatting.

--- source/Grove/CardsLibrary/ForgeDevil.cs
+++ source/Grove/CardsLibrary/ForgeDevil.cs
@@ -10,28 +10,29 @@
     public override IEnumerable<CardTemplate> GetCards()
     {
       yield return Card
-          .Named("Forge Devil")
-          .ManaCost("{R}")
-          .Type("Creature — Devil")
-          .Text("When Forge Devil enters the battlefield, it deals 1 damage to target creature and 1 damage to you.")
-          .FlavorText("A bit of pain never hurts.")
-          .Power(1)
-          .Toughness(1)
-          .TriggeredAbility(p =>
+        .Named("Forge Devil")
+        .ManaCost("{R}")
+        .Type("Creature — Devil")
+        .Text("When Forge Devil enters the battlefield, it deals 1 damage to target creature and 1 damage to you.")
+        .FlavorText("A bit of pain never hurts.")
+        .Power(1)
+        .Toughness(1)
+        .TriggeredAbility(p =>
           {
-            p.Text = "When Forge Devil enters the battlefield, it deals 1 damage to target creature and 1 damage to you.";
+            p.Text =
+              "When Forge Devil enters the battlefield, it deals 1 damage to target creature and 1 damage to you.";
             p.Trigger(new OnZoneChanged(to: Zone.Battlefield));
 
             p.Effect = () => new CompoundEffect(
               new DealDamageToTargets(1),
               new DealDamageToPlayer(1, P(e => e.Controller)));
             p.TargetSelector.AddEffect(trg =>
-            {
-              trg.Message = "Select a target creature";
-              trg.MinCount = 1;
-              trg.MaxCount = 1;
-              trg.Is.Creature().On.Battlefield();
-            });
+              {
+                trg.Message = "Select a target creature";
+                trg.MinCount = 1;
+                trg.MaxCount = 1;
+                trg.Is.Creature().On.Battlefield();
+              });
             p.TargetingRule(new EffectDealDamage(1));
           });
     }
