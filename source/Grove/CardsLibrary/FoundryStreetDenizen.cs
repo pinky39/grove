79f2e009764656ad0b1cd23e4444cc9f56ad70c2
pinky <pinky@lab.org>
2015-05-15 14:50:48 +0200

    Formatting.

--- source/Grove/CardsLibrary/FoundryStreetDenizen.cs
+++ source/Grove/CardsLibrary/FoundryStreetDenizen.cs
@@ -11,25 +11,29 @@
     public override IEnumerable<CardTemplate> GetCards()
     {
       yield return Card
-          .Named("Foundry Street Denizen")
-          .ManaCost("{R}")
-          .Type("Creature — Goblin Warrior")
-          .Text("Whenever another red creature enters the battlefield under your control, Foundry Street Denizen gets +1/+0 until end of turn.")
-          .FlavorText("After the Foundry Street riot, Arrester Hulbein tried to ban bludgeons. Which, inevitably, resulted in another riot.")
-          .Power(1)
-          .Toughness(1)
-          .TriggeredAbility(p =>
+        .Named("Foundry Street Denizen")
+        .ManaCost("{R}")
+        .Type("Creature — Goblin Warrior")
+        .Text(
+          "Whenever another red creature enters the battlefield under your control, Foundry Street Denizen gets +1/+0 until end of turn.")
+        .FlavorText(
+          "After the Foundry Street riot, Arrester Hulbein tried to ban bludgeons. Which, inevitably, resulted in another riot.")
+        .Power(1)
+        .Toughness(1)
+        .TriggeredAbility(p =>
           {
-            p.Text = "Whenever another red creature enters the battlefield under your control, Foundry Street Denizen gets +1/+0 until end of turn.";
+            p.Text =
+              "Whenever another red creature enters the battlefield under your control, Foundry Street Denizen gets +1/+0 until end of turn.";
 
             p.Trigger(new OnZoneChanged(
               to: Zone.Battlefield,
-              selector: (c, ctx) => c.Is().Creature && c.HasColor(CardColor.Red) && ctx.You == c.Controller && ctx.OwningCard != c));
+              selector: (c, ctx) =>
+                c.Is().Creature && c.HasColor(CardColor.Red) && ctx.You == c.Controller && ctx.OwningCard != c));
 
             p.TriggerOnlyIfOwningCardIsInPlay = true;
 
             p.Effect = () => new ApplyModifiersToSelf(
-              () => new AddPowerAndToughness(1, 0) { UntilEot = true })
+              () => new AddPowerAndToughness(1, 0) {UntilEot = true})
               .SetTags(EffectTag.IncreasePower);
           });
     }
--- source/Grove/CardsLibrary/FrenziedGoblin.cs
+++ source/Grove/CardsLibrary/FrenziedGoblin.cs
@@ -11,23 +11,24 @@
     public override IEnumerable<CardTemplate> GetCards()
     {
       yield return Card
-          .Named("Frenzied Goblin")
-          .ManaCost("{R}")
-          .Type("Creature — Goblin Berserker")
-          .Text("Whenever Frenzied Goblin attacks, you may pay {R}. If you do, target creature can't block this turn.")
-          .FlavorText("What he lacks in stature, he makes up for with enthusiasm.")
-          .Power(1)
-          .Toughness(1)
-          .TriggeredAbility(p =>
+        .Named("Frenzied Goblin")
+        .ManaCost("{R}")
+        .Type("Creature — Goblin Berserker")
+        .Text("Whenever Frenzied Goblin attacks, you may pay {R}. If you do, target creature can't block this turn.")
+        .FlavorText("What he lacks in stature, he makes up for with enthusiasm.")
+        .Power(1)
+        .Toughness(1)
+        .TriggeredAbility(p =>
           {
-            p.Text = "Whenever Frenzied Goblin attacks, you may pay {R}. If you do, target creature can't block this turn.";
+            p.Text =
+              "Whenever Frenzied Goblin attacks, you may pay {R}. If you do, target creature can't block this turn.";
             p.Trigger(new WhenThisAttacks());
-            
+
             p.Effect = () => new PayManaThen(
-              amount: Mana.Red, 
-              effect: new ApplyModifiersToTargets(() => 
-                new AddStaticAbility(Static.CannotBlock){UntilEot = true}));
-            
+              amount: Mana.Red,
+              effect: new ApplyModifiersToTargets(() =>
+                new AddStaticAbility(Static.CannotBlock) {UntilEot = true}));
+
             p.TargetSelector.AddEffect(trg => trg.Is.Creature().On.Battlefield());
             p.TargetingRule(new EffectTapCreature());
           });
