cd84a43aadef87c16b59fee85c9c5f8c8fa4bd0e
pinky <pinky@lab.org>
2015-05-26 10:52:44 +0200

    Fix formatting

--- source/Grove/CardsLibrary/MinersBane.cs
+++ source/Grove/CardsLibrary/MinersBane.cs
@@ -12,19 +12,20 @@
     public override IEnumerable<CardTemplate> GetCards()
     {
       yield return Card
-          .Named("Miner's Bane")
-          .ManaCost("{4}{R}{R}")
-          .Type("Creature — Elemental")
-          .Text("{2}{R}: Miner's Bane gets +1/+0 and gains trample until end of turn. {I}(If it would assign enough damage to its blockers to destroy them, you may have it assign the rest of its damage to defending player or planeswalker.){/I}")
-          .Power(6)
-          .Toughness(3)
-          .ActivatedAbility(p =>
+        .Named("Miner's Bane")
+        .ManaCost("{4}{R}{R}")
+        .Type("Creature — Elemental")
+        .Text(
+          "{2}{R}: Miner's Bane gets +1/+0 and gains trample until end of turn. {I}(If it would assign enough damage to its blockers to destroy them, you may have it assign the rest of its damage to defending player or planeswalker.){/I}")
+        .Power(6)
+        .Toughness(3)
+        .ActivatedAbility(p =>
           {
             p.Text = "{2}{R}: Miner's Bane gets +1/+0 and gains trample until end of turn.";
             p.Cost = new PayMana("{2}{R}".Parse(), supportsRepetitions: true);
             p.Effect = () => new ApplyModifiersToSelf(
-              () => new AddPowerAndToughness(1, 0){UntilEot = true},
-              () => new AddStaticAbility(Static.Trample){UntilEot = true});
+              () => new AddPowerAndToughness(1, 0) {UntilEot = true},
+              () => new AddStaticAbility(Static.Trample) {UntilEot = true});
 
             p.TimingRule(new PumpOwningCardTimingRule(1, 0));
             p.RepetitionRule(new RepeatMaxTimes());
--- source/Grove/CardsLibrary/ParagonOfFierceDefiance.cs
+++ source/Grove/CardsLibrary/ParagonOfFierceDefiance.cs
@@ -16,34 +16,35 @@
         .Named("Paragon of Fierce Defiance")
         .ManaCost("{3}{R}")
         .Type("Creature — Human Warrior")
-        .Text("Other red creatures you control get +1/+1.{EOL}{R},{T}: Another target red creature you control gains haste until end of turn. {I}(It can attack and {T} this turn.){/I}")
+        .Text(
+          "Other red creatures you control get +1/+1.{EOL}{R},{T}: Another target red creature you control gains haste until end of turn. {I}(It can attack and {T} this turn.){/I}")
         .Power(2)
         .Toughness(2)
-        .Cast(p =>
-        {
-          p.Effect = () => new CastPermanent().SetTags(EffectTag.IncreasePower, EffectTag.IncreaseToughness);
-        })
+        .Cast(
+          p => { p.Effect = () => new CastPermanent().SetTags(EffectTag.IncreasePower, EffectTag.IncreaseToughness); })
         .ContinuousEffect(p =>
-        {
-          p.Modifier = () => new AddPowerAndToughness(1, 1);
-          p.Selector = (c, ctx) => c.Controller == ctx.You && c.Is().Creature && c.HasColor(CardColor.Red) && c != ctx.Source;
-        })
+          {
+            p.Modifier = () => new AddPowerAndToughness(1, 1);
+            p.Selector =
+              (c, ctx) => c.Controller == ctx.You && c.Is().Creature && c.HasColor(CardColor.Red) && c != ctx.Source;
+          })
         .ActivatedAbility(p =>
-        {
-          p.Text = "{R},{T}: Another target red creature you control gains haste until end of turn.";
-          p.Cost = new AggregateCost(
-            new PayMana("{R}".Parse()),
-            new Tap());
+          {
+            p.Text = "{R},{T}: Another target red creature you control gains haste until end of turn.";
+            p.Cost = new AggregateCost(
+              new PayMana("{R}".Parse()),
+              new Tap());
 
-          p.Effect = () => new ApplyModifiersToTargets(() => new AddStaticAbility(Static.Haste) { UntilEot = true });
+            p.Effect = () => new ApplyModifiersToTargets(() => new AddStaticAbility(Static.Haste) {UntilEot = true});
 
-          p.TargetSelector.AddEffect(trg => trg
-              .Is.Card(c => c.Is().Creature && c.HasColor(CardColor.Red), controlledBy: ControlledBy.SpellOwner, canTargetSelf: false)
+            p.TargetSelector.AddEffect(trg => trg
+              .Is.Card(c => c.Is().Creature && c.HasColor(CardColor.Red), ControlledBy.SpellOwner,
+                canTargetSelf: false)
               .On.Battlefield());
 
-          p.TimingRule(new OnFirstMain());
-          p.TargetingRule(new EffectOrCostRankBy(c => -c.Score, controlledBy: ControlledBy.SpellOwner));
-        });
+            p.TimingRule(new OnFirstMain());
+            p.TargetingRule(new EffectOrCostRankBy(c => -c.Score, ControlledBy.SpellOwner));
+          });
     }
   }
 }
