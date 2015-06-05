a1f95052ac28b9b9fbdee1ce7e17a18a0d7641f0
pinky <pinky@lab.org>
2015-06-05 10:29:10 +0200

    Test Kalonian Twingrove

---
+++ tests/Grove.Tests/Cards/KalonianTwingrove.cs
@@ -0,0 +1,22 @@
+﻿namespace Grove.Tests.Cards
+{
+  using System.Linq;
+  using Infrastructure;
+  using Xunit;
+
+  public class KalonianTwingrove
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void GroveAndTwinAre66()
+      {
+        Hand(P1, "Kalonian Twingrove");
+        Battlefield(P1, "Forest", "Forest", "Forest", "Forest", "Forest", "Forest");
+        RunGame(1);
+
+        Equal(2, P1.Battlefield.Creatures.Count(x => x.Power == 6 && x.Toughness == 6));
+      }
+    }
+  }
+}
