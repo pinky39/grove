8a44b8db4c1d859f92f9ea2b7fe9e1d5ad8f2907
pinky <pinky@lab.org>
2015-06-05 09:17:45 +0200

    Test Chord of Calling
    A card with non-trivial new semantics should be tested.

---
+++ tests/Grove.Tests/Cards/ChordOfCalling.cs
@@ -0,0 +1,29 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class ChordOfCalling
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void SearchForDragon()
+      {
+        var dragon = C("Shivan Dragon");
+        
+        Hand(P1, "Chord of Calling");
+        Library(P1, "Mountain", dragon);
+        
+        Battlefield(P1, "Grizzly Bears", "Grizzly Bears", "Grizzly Bears", "Grizzly Bears", 
+          "Forest", "Mountain", "Forest", "Forest", "Mountain");
+
+        Battlefield(P2, "Shivan Dragon");
+
+        RunGame(2);
+
+        Equal(Zone.Battlefield, C(dragon).Zone);
+      }
+    }
+  }
+}
