f302399f91226cb964ba70da856387b67a0b1103
pinky <pinky@lab.org>
2015-06-05 10:17:52 +0200

    Test Hornet Nest

---
+++ tests/Grove.Tests/Cards/HornetNest.cs
@@ -0,0 +1,29 @@
+﻿namespace Grove.Tests.Cards
+{
+  using System.Linq;
+  using Infrastructure;
+  using Xunit;
+
+  public class HornetNest
+  {
+    public class PredefinedAi : PredefinedAiScenario
+    {
+      [Fact]
+      public void Get3Tokens()
+      {
+        var hornets = C("Hornet Nest");
+        var bolt = C("Lightning Bolt");
+
+        Hand(P1, bolt);
+        Battlefield(P2, hornets);
+
+        Exec(
+          At(Step.FirstMain)
+            .Cast(bolt, target: hornets),
+          At(Step.SecondMain)
+            .Verify(() => Equal(3, P2.Battlefield.Creatures.Count())));
+
+      }
+    }
+  }
+}
