bffd4caa242f1c9f778a131e5a42dff0d27cd8d8
pinky <pinky@lab.org>
2015-06-15 14:07:41 +0200

    Test Hot Soup

---
+++ tests/Grove.Tests/Cards/HotSoup.cs
@@ -0,0 +1,27 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class HotSoup
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void DestroyedEquipedCreature()
+      {
+        var bears = C("Grizzly Bears");
+
+        Battlefield(P1, "Llanowar Elves");        
+        Battlefield(P2, bears.IsEquipedWith("Hot Soup"));
+        
+        P1.Life = 2;
+        P2.Life = 1;
+        
+        RunGame(2);
+
+        Equal(Zone.Graveyard, C(bears).Zone);
+      }
+    }
+  }
+}
