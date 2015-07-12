f6aea83995cbe820ebccdaebd7e6bc18193db79c
Sandmann123 <norfirill@gmail.com>
2015-07-12 15:20:36 +0700

    Added test to catch a bug related with Master of Predicaments
    * the card does not support the playing sorcery and instant cards

--- tests/Grove.Tests/Cards/MasterOfPredicaments.cs
+++ tests/Grove.Tests/Cards/MasterOfPredicaments.cs
@@ -39,6 +39,23 @@
 
         RunGame(1);
       }
+
+      [Fact]
+      public void CastRestockWithoutPayingMana()
+      {
+        var restock = C("Restock");
+        var bear1 = C("Grizzly Bears");
+        var bear2 = C("Grizzly Bears");
+        Hand(P1, restock);
+        Graveyard(P1, bear1, bear2);
+        Battlefield(P1, "Master of Predicaments");
+
+        RunGame(1);
+
+        Equal(Zone.Exile, C(restock).Zone);
+        Equal(Zone.Hand, C(bear1).Zone);
+        Equal(Zone.Hand, C(bear2).Zone);
+      }
     }
   }
 }
