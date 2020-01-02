97e16a902cd7707970df60a7e96aa39800aa6c77
pinky <pinky@lab.org>
2020-01-02 17:54:03 +0100

    Add tests for AbzanFalconer

---
+++ tests/Grove.Tests/Cards/AbzanFalconer.cs
@@ -0,0 +1,24 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class AbzanFalconer
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void GiveFlyingToCreaturesWithCounters()
+      {
+        var anthroplasm = C("Anthroplasm");
+        Hand(P1, anthroplasm);
+        Battlefield(P1, "Abzan Falconer", "Island", "Island", "Island", "Island");
+        Battlefield(P2, "Grizzly Bears", "Grizzly Bears");        
+
+        RunGame(1);
+
+        Assert.True(anthroplasm.Card.Has().Flying);        
+      }
+    }
+  }
+}
