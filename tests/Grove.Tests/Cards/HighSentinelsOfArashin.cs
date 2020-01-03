6c723ff4c8e29bed3ef4eca4e6f857d4c3d4438c
pinky <pinky@lab.org>
2020-01-03 15:03:57 +0100

    Add tests for HighSentinelsOfArashin

---
+++ tests/Grove.Tests/Cards/HighSentinelsOfArashin.cs
@@ -0,0 +1,22 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class HighSentinelsOfArashin
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void PumpThenAttack()
+      {
+        Battlefield(P1, "High Sentinels of Arashin", "Plains", "Plains", "Plains", "Plains", "Grizzly Bears");
+        P2.Life = 7;
+
+        RunGame(1);
+
+        Assert.Equal(0, P2.Life);
+      }
+    }
+  }
+}
