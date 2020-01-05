ae1986c522a85ad890c7d683130c69311d5a8ad7
pinky <pinky@lab.org>
2020-01-05 15:17:34 +0100

    Add tests for AbzanCharm

---
+++ tests/Grove.Tests/Cards/AbzanCharm.cs
@@ -0,0 +1,24 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class AbzanCharm
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void Give2CountersToBear()
+      {
+        var charm = C("Abzan Charm");
+        Hand(P1, charm);
+        Battlefield(P1, "Plains", "Swamp", "Forest", "Grizzly Bears");
+        P2.Life = 4;
+
+        RunGame(1);
+
+        Assert.Equal(0, P2.Life);
+      }
+    }
+  }
+}
