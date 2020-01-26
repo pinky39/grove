ad1e90937ce02a5bc15de42efd13d0c548244049
pinky <pinky@lab.org>
2020-01-26 17:16:13 +0100

    Test AbzanSkycaptain

---
+++ tests/Grove.Tests/Cards/AbzanSkycaptain.cs
@@ -0,0 +1,28 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class AbzanSkycaptain
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void Put2CountersOnBear()
+      {
+        var bear = C("Grizzly Bears");
+        
+        Battlefield(P1, "Abzan Skycaptain", bear);
+        Battlefield(P2, "Mountain", "Grizzly Bears");
+        
+        Hand(P2, "Shock");
+
+        P2.Life = 2;
+
+        RunGame(1);
+
+        Equal(4, C(bear).Toughness);                
+      }
+    }
+  }
+}
