b47147c3b06deecdfbeafbbd5c8fdbf0ff31ed11
pinky <pinky@lab.org>
2020-01-26 20:25:08 +0100

    Test ActOfTreason

---
+++ tests/Grove.Tests/Cards/ActOfTreason.cs
@@ -0,0 +1,28 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class ActOfTreason
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void GainControlOfDragon()
+      {
+        var dragon = C("Shivan Dragon");
+
+        Hand(P1, "Act of Treason");
+        Battlefield(P1, "Mountain", "Mountain", "Mountain", "Mountain");
+        Battlefield(P2, dragon.Tap());
+
+        P2.Life = 7;
+
+        RunGame(2);
+
+        Equal(1, P2.Life);
+        Equal(P2, C(dragon).Controller);
+      }
+    }
+  }
+}
