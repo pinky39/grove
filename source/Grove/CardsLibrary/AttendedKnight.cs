b7f1f1b22c4181f470308c05511a31f34760846c
pinky <pinky@lab.org>
2020-02-01 18:44:29 +0100

    Remove support for 'choosable' Triggered abilities
    - Remove cards AvenSurveyor, HoodedAssassin, SandsteppeOutcast
    - Add AttendedKnight as replacement card for SandsteppeOutcast
    
    Replace removed cards in decks with similar cards.

---
+++ source/Grove/CardsLibrary/AttendedKnight.cs
@@ -0,0 +1,38 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+  using Effects;
+  using Modifiers;
+  using Triggers;
+
+  public class AttendedKnight : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Attended Knight")
+        .ManaCost("{2}{W}")
+        .Type("Creature — Human Knight")
+        .Text("{First strike}{EOL}When Attended Knight enters the battlefield, create a 1/1 white Soldier creature token.")
+        .Power(2)
+        .Toughness(2)
+        .SimpleAbilities(Static.FirstStrike)
+        .TriggeredAbility(p =>
+        {
+          p.Text =
+            "When Attended Knight enters the battlefield, create a 1/1 white Soldier creature token.";
+
+          p.Trigger(new OnZoneChanged(to: Zone.Battlefield));
+
+          p.Effect = () => new CreateTokens(
+            count: 1,
+            token: Card
+              .Named("Soldier")
+              .Power(1)
+              .Toughness(1)
+              .Type("Token Creature - Soldier")
+              .Colors(CardColor.White));
+        });
+    }
+  }
+}
