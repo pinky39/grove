97c9cd9e21fc6a6943237b09d6ff914461f5c9e8
pinky <pinky@lab.org>
2020-02-02 19:58:51 +0100

    Test BattleBrawler

---
+++ tests/Grove.Tests/Cards/BattleBrawler.cs
@@ -0,0 +1,36 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class BattleBrawler
+  {
+    public class Predefined : PredefinedScenario
+    {
+      [Fact]
+      public void Gets10AndFirstStrike()
+      {
+        var brawler = C("Battle Brawler");
+        var alarm = C("Raise The Alarm");
+
+        Hand(P1, alarm);
+        Battlefield(P1, brawler);
+
+        Exec(
+          At(Step.Upkeep)
+            .Verify(() =>
+            {
+              Equal(2, C(brawler).Power);
+              False(C(brawler).Has().FirstStrike);
+            }),
+          At(Step.FirstMain)
+            .Cast(alarm)
+            .Verify(() =>
+            {
+              Equal(3, C(brawler).Power);
+              True(C(brawler).Has().FirstStrike);
+            }));
+      }
+    }
+  }
+}
