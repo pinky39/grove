e5da53b78f048070772b8243f2a027452e3ec428
pinky <pinky@lab.org>
2020-02-13 18:04:08 +0100

    Test DragonStyleTwins

---
+++ tests/Grove.Tests/Cards/DragonStyleTwins.cs
@@ -0,0 +1,24 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class DragonStyleTwins
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void Prowess()
+      {
+        Hand(P1, "Shock");
+        Battlefield(P1, "Dragon-Style Twins", "Mountain");
+        Battlefield(P2, "Grizzly Bears");
+        P2.Life = 8;
+
+        RunGame(1);
+
+        Equal(0, P2.Life);
+      }
+    }
+  }
+}
