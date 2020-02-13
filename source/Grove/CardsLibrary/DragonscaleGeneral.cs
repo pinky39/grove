404769f70b205dc9b74b4c4284bb537c2e39e017
pinky <pinky@lab.org>
2020-02-13 18:23:56 +0100

    Fix DragonscaleGeneral

--- source/Grove/CardsLibrary/DragonscaleGeneral.cs
+++ source/Grove/CardsLibrary/DragonscaleGeneral.cs
@@ -23,8 +23,13 @@
           p.Text = "At the beginning of your end step, bolster X, where X is the number of tapped creatures you control.";
           p.Trigger(new OnStepStart(Step.EndOfTurn));
           p.Effect = () => new Put11CountersOnTargets(P(e => e.Controller.Battlefield.Creatures.Count(x => x.IsTapped)));
+          
           p.TargetSelector.AddEffect(
-            trg => trg.Is.Card(c => c.Is().Creature && c.Controller.Battlefield.Creatures.All(x => x.Toughness >= c.Toughness), ControlledBy.SpellOwner).On.Battlefield());
+            trg => trg.Is.Card(c =>
+              c.Is().Creature && c.Controller.Battlefield.Creatures.All(x => x.Toughness >= c.Toughness),
+              ControlledBy.SpellOwner).On.Battlefield(),
+            trg => trg.MustBeTargetable = false);
+
           p.TargetingRule(new EffectOrCostRankBy(rank: c => -c.Score, controlledBy: ControlledBy.SpellOwner));
         });
     }
