ce538cbebc640edcf7877c90bc211f1202ef59c3
pinky <pinky@lab.org>
2020-02-25 20:27:34 +0100

    Test Goblinslide

---
+++ tests/Grove.Tests/Cards/Goblinslide.cs
@@ -0,0 +1,37 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class Goblinslide
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void PutGoblinIntoPlay()
+      {
+        Hand(P1, "Volcanic Hammer");
+        Battlefield(P1, "Goblinslide", "Mountain", "Mountain", "Mountain");
+
+        P2.Life = 4;
+
+        RunGame(1);
+
+        Equal(0, P2.Life);
+      }
+
+      [Fact]
+      public void DontPutGoblinIntoPlay()
+      {
+        Hand(P1, "Volcanic Hammer");
+        Battlefield(P1, "Goblinslide", "Mountain", "Mountain");
+
+        P2.Life = 4;
+
+        RunGame(1);
+
+        Equal(1, P2.Life);
+      }
+    }
+  }
+}
