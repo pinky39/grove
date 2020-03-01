abc921f729651cc8bad24257bb65a5f723ff5d7e
pinky <pinky@lab.org>
2020-03-01 11:25:23 +0100

    Test KheruBloodsucker

---
+++ tests/Grove.Tests/Cards/KheruBloodsucker.cs
@@ -0,0 +1,28 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class KheruBloodsucker
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void SacAndPump()
+      {
+        var kheru = C("Kheru Bloodsucker");
+        var bears = C("Grizzly Bears");
+
+        Battlefield(P1, kheru, "Wall of Blossoms", "Swamp", "Swamp", "Swamp");        
+        Battlefield(P2, bears);
+
+        P2.Life = 5;
+        RunGame(1);
+
+        Equal(3, P2.Life);
+        Equal(Zone.Graveyard, C(bears).Zone);
+        Equal(3, C(kheru).Power);
+      }
+    }
+  }
+}
