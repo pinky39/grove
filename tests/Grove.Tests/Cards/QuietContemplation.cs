3f051a45d1c9436e4d38e052f644da54d2e9d24e
pinky <pinky@lab.org>
2020-03-06 20:29:45 +0100

    Test QuietContemplation

---
+++ tests/Grove.Tests/Cards/QuietContemplation.cs
@@ -0,0 +1,25 @@
+﻿namespace Grove.Tests.Cards
+{
+  using Infrastructure;
+  using Xunit;
+
+  public class QuietContemplation
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void ShockAndTap()
+      {
+        Hand(P1, "Shock");
+        Battlefield(P1, "Grizzly Bears", "Grizzly Bears", "Quiet Contemplation", "Mountain", "Mountain");
+        Battlefield(P2, "Grizzly Bears", "Grizzly Bears");
+
+        P2.Life = 8;
+
+        RunGame(3);
+
+        Equal(0, P2.Life);
+      }
+    }
+  }
+}
