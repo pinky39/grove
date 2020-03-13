6282dd5e27b1ad4eda588f08b4a56a92467a7ea0
pinky <pinky@lab.org>
2020-03-13 22:26:15 +0100

    Fix TemurBanner

--- source/Grove/CardsLibrary/TemurBanner.cs
+++ source/Grove/CardsLibrary/TemurBanner.cs
@@ -3,6 +3,8 @@
   using System.Collections.Generic;
   using Costs;
   using Effects;
+  using Grove.AI;
+  using Grove.AI.TimingRules;
 
   public class TemurBanner : CardTemplateSource
   {
@@ -22,12 +24,20 @@
         .ActivatedAbility(p =>
         {
           p.Text = "{G}{U}{R}, {T}, Sacrifice Temur Banner: Draw a card.";
+          
           p.Cost = new AggregateCost(
             new PayMana("{G}{U}{R}".Parse()),
             new Tap(),
             new Sacrifice());
+          
           p.Effect = () => new DrawCards(1);
-        });
+
+          p.TimingRule(new Any(
+             new WhenOwningCardWillBeDestroyed(),
+             new OnEndOfOpponentsTurn()));
+        })
+        // TODO scoring should depend on number of lands on battlefield
+        .OverrideScore(p => p.Battlefield = Scores.ManaCostToScore[2]);
     }
   }
 }
