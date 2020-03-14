7d51e16518b0eb15c6a80639aff556ff095f1336
pinky <pinky@lab.org>
2020-03-14 21:27:48 +0100

    Test WhiskAway

---
+++ tests/Grove.Tests/Cards/WhiskAway.cs
@@ -0,0 +1,29 @@
+﻿namespace Grove.Tests.Cards
+{
+  using System.Linq;
+  using Infrastructure;
+  using Xunit;
+
+  public class WhiskAway
+  {
+    public class Ai : AiScenario
+    {
+      [Fact]
+      public void PutDragonOnTopOfLibrary()
+      {
+        var dragon = C("Shivan Dragon");
+
+        Hand(P1, "Whisk Away");
+        Battlefield(P1, "Shivan Dragon", "Shivan Dragon", "Island", "Island", "Island");
+        
+        Battlefield(P2, dragon);
+
+        P2.Life = 10;
+
+        Equals(5, P2.Life);
+        Equals(Zone.Library, C(dragon).Zone);
+        Equals(2, P1.Battlefield.Count(x => x.Name == "Shivan Dragon"));
+      }
+    }
+  }
+}
