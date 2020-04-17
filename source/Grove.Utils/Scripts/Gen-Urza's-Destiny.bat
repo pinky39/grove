2b15476705cc4333bb92aecfa059f9a6cc594028
pinky <pinky@lab.org>
2020-04-17 20:27:26 +0200

    Improve Urza's Destiny limited ratings

--- source/Grove.Utils/Scripts/Gen-Urza's-Destiny.bat
+++ source/Grove.Utils/Scripts/Gen-Urza's-Destiny.bat
@@ -1,1 +1,2 @@
-ugrove gen count=1000 "s=Urza's Saga" "b1=Urza's Destiny" "b2=Urza's Destiny" "b3=Urza's Destiny"
+ugrove gen count=100 "s=Urza's Saga" "b1=Urza's Destiny" "b2=Urza's Destiny" "b3=Urza's Destiny"
+ugrove gen count=100 "s=Urza's Destiny" "b1=Urza's Destiny" "b2=Urza's Destiny" "b3=Urza's Destiny"
