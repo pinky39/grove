4f864fdedec4f6fce613a743c5e9e2768eadbee7
pinky <pinky@lab.org>
2020-08-31 19:52:15 +0200

    Add Kalonian Tusker

---
+++ source/Grove/CardsLibrary/KalonianTusker.cs
@@ -0,0 +1,19 @@
+﻿namespace Grove.CardsLibrary
+{
+  using System.Collections.Generic;
+
+  public class KalonianTusker : CardTemplateSource
+  {
+    public override IEnumerable<CardTemplate> GetCards()
+    {
+      yield return Card
+        .Named("Kalonian Tusker")
+        .ManaCost("{G}{G}")
+        .Type("Creature - Beast")
+        .FlavorText(
+          "'And all this time I thought we were tracking it.'—Juruk, Kalonian tracker")
+        .Power(3)
+        .Toughness(3);
+    }
+  }
+}
